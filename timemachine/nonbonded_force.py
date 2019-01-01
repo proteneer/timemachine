645f3677ae6d2e1a82dff9b1b75413a93e73d8a5
Yutong Zhao <proteneer@gmail.com>
2019-01-01 14:23:16 -0500

    Implemented LJ612 forces.

--- timemachine/nonbonded_force.py
+++ timemachine/nonbonded_force.py
@@ -2,12 +2,79 @@ import tensorflow as tf
 from timemachine.force import ConservativeForce
 from timemachine.constants import ONE_4PI_EPS0
 
+
+# (TODO): generalize exclusions so we can support fudge factors.
+def generate_exclusion_masks(exclusions):
+    ones = tf.ones_like(exclusions, dtype=tf.int32)
+    mask_a = tf.matrix_band_part(ones, 0, -1) # Upper triangular matrix of 0s and 1s
+    mask_b = tf.matrix_band_part(ones, 0, 0) # Diagonal matrix
+
+    # only use upper triangular part
+    exclusions = tf.cast(tf.matrix_band_part(exclusions, 0, -1), dtype=tf.int32)
+    return (mask_a - mask_b) - exclusions
+
+
+class LJ612Force(ConservativeForce):
+
+
+    def __init__(self, params, param_idxs, exclusions):
+        """
+        Implements a non-periodic LJ612 potential using the Jorgensen
+        combining rules for sigma/epsilon.
+
+        Parameters:
+        -----------
+        params: tf.float64 variable
+            list of parameters for Ai and Ci used in the OPLS combining
+            rules.
+
+        param_idxs: tf.int32 (N,2)
+            each tuple (Ai, Ci) is used as part of the combining rules
+
+        exclusions: tf.bool (N, N)
+            boolean mask denoting if interaction e[i,j] should be
+            excluded or not. If e[i,j] is 1 then the interaction
+            is excluded, 0 implies it is kept. Note that only the upper
+            right triangular portion of this is used.
+
+        """
+        self.params = params
+        self.param_idxs = param_idxs
+        self.keep_mask = generate_exclusion_masks(exclusions)
+
+    def energy(self, conf):
+
+        A = tf.gather(self.params, self.param_idxs[:, 0])
+        C = tf.gather(self.params, self.param_idxs[:, 1])
+
+        Ai = tf.expand_dims(A, 0)
+        Aj = tf.expand_dims(A, 1)
+        Aij = tf.sqrt(tf.multiply(Ai, Aj))
+
+        Ci = tf.expand_dims(C, 0)
+        Cj = tf.expand_dims(C, 1)
+        Cij = tf.sqrt(tf.multiply(Ci, Cj))
+
+        ri = tf.expand_dims(conf, 0)
+        rj = tf.expand_dims(conf, 1)
+        d2ij = tf.reduce_sum(tf.pow(ri-rj, 2), axis=-1)
+
+        Aij_mask = tf.boolean_mask(Aij, self.keep_mask)
+        Cij_mask = tf.boolean_mask(Cij, self.keep_mask)
+        d2ij_mask = tf.boolean_mask(d2ij, self.keep_mask)       
+        d6ij_mask = d2ij_mask*d2ij_mask*d2ij_mask
+        d12ij_mask = d6ij_mask*d6ij_mask
+
+        energy = Aij_mask/d12ij_mask - Cij_mask/d6ij_mask
+        return tf.reduce_sum(energy, axis=-1)
+
+
 class ElectrostaticForce(ConservativeForce):
 
 
     def __init__(self, params, param_idxs, exclusions):
         """
-        Implements a non-periodic point charge electrostatic potential: sum qiqj
+        Implements a non-periodic point charge electrostatic potential.
 
         Parameters:
         -----------
@@ -27,17 +94,7 @@ class ElectrostaticForce(ConservativeForce):
         # (ytz) TODO: implement Ewald/PME
         self.params = params
         self.param_idxs = param_idxs
-
-        # pre-build mask
-        num_atoms = self.param_idxs.shape[0]
-
-        ones = tf.ones(shape=(num_atoms, num_atoms), dtype=tf.int32) # avoid ones like
-        mask_a = tf.matrix_band_part(ones, 0, -1) # Upper triangular matrix of 0s and 1s
-        mask_b = tf.matrix_band_part(ones, 0, 0) # Diagonal matrix
-
-        # only use upper triangular part
-        exclusions = tf.cast(tf.matrix_band_part(exclusions, 0, -1), dtype=tf.int32)
-        self.keep_mask = (mask_a - mask_b) - exclusions
+        self.keep_mask = generate_exclusion_masks(exclusions)
 
 
     def energy(self, conf):
--- timemachine/test_nonbonded_force.py
+++ timemachine/test_nonbonded_force.py
@@ -1,13 +1,83 @@
 import unittest
 import numpy as np
 import tensorflow as tf
-from timemachine.nonbonded_force import ElectrostaticForce
+from timemachine.nonbonded_force import ElectrostaticForce, LJ612Force
 from timemachine.constants import ONE_4PI_EPS0
 
 class TestNonbondedForce(unittest.TestCase):
 
     def tearDown(self):
         tf.reset_default_graph()
+
+    def test_lj612(self):
+        """
+        Testing non-periodic lj 612 forces.
+        """
+        x0 = np.array([
+            [ 0.0637,   0.0126,   0.2203],
+            [ 1.0573,  -0.2011,   1.2864],
+            [ 2.3928,   1.2209,  -0.2230],
+            [-0.6891,   1.6983,   0.0780],
+            [-0.6312,  -1.6261,  -0.2601]
+        ], dtype=np.float64)
+
+        x_ph = tf.placeholder(shape=(5, 3), dtype=np.float64)
+
+        params_np = np.array([3.0, 2.0, 1.0, 1.4], dtype=np.float64)
+        params = tf.convert_to_tensor(params_np)
+        # Ai, Ci
+        param_idxs = np.array([
+            [0, 3],
+            [1, 2],
+            [1, 2],
+            [1, 2],
+            [1, 2]], dtype=np.int32)
+
+        exclusions = np.array([
+            [0,0,1,0,0],
+            [0,0,0,1,1],
+            [1,0,0,0,0],
+            [0,1,0,0,1],
+            [0,1,0,1,0],
+            ], dtype=np.bool)
+
+        ef = LJ612Force(params, param_idxs, exclusions)
+
+        sess = tf.Session()
+        sess.run(tf.initializers.global_variables())
+
+        num_atoms = 5
+        ref_nrg = 0
+
+        for i in range(num_atoms):
+            Ai = params_np[param_idxs[i, 0]]
+            Ci = params_np[param_idxs[i, 1]]
+            for j in range(i+1, num_atoms):
+                if not exclusions[i, j]:
+                    Aj = params_np[param_idxs[j, 0]]
+                    Cj = params_np[param_idxs[j, 1]]
+                    dij = np.linalg.norm(x0[i] - x0[j])
+                    ref_nrg += np.sqrt(Ai*Aj)/np.power(dij, 12) - np.sqrt(Ci*Cj)/np.power(dij, 6)
+
+        # (ytz) TODO: add a test for forces, for now we rely on
+        # autograd to be analytically correct.
+        test_nrg_op = ef.energy(x_ph)
+
+        test_nrg = sess.run(test_nrg_op, feed_dict={x_ph: x0})
+
+        np.testing.assert_almost_equal(ref_nrg, test_nrg, decimal=15)
+
+        test_grads_op = ef.gradients(x_ph)
+        test_grads = sess.run(test_grads_op, feed_dict={x_ph: x0})
+
+        net_force = np.sum(test_grads, axis=0)
+        # this also checks that there are no NaNs
+        np.testing.assert_almost_equal(net_force, [0,0,0], decimal=14)
+
+        assert not np.any(np.isnan(sess.run(ef.hessians(x_ph), feed_dict={x_ph: x0})))
+        assert not np.any(np.isnan(sess.run(ef.mixed_partials(x_ph), feed_dict={x_ph: x0})))
+
+
 
     def test_electrostatics(self):
         """
@@ -23,7 +93,8 @@ class TestNonbondedForce(unittest.TestCase):
 
         x_ph = tf.placeholder(shape=(5, 3), dtype=np.float64)
 
-        params = np.array([1.3, 0.3], dtype=np.float64)
+        params_np = np.array([1.3, 0.3], dtype=np.float64)
+        params = tf.convert_to_tensor(params_np)
         param_idxs = np.array([0, 1, 1, 1, 1], dtype=np.int32)
         exclusions = np.array([
             [0,0,1,0,0],
@@ -42,10 +113,10 @@ class TestNonbondedForce(unittest.TestCase):
         ref_nrg = 0
 
         for i in range(num_atoms):
-            qi = params[param_idxs[i]]
+            qi = params_np[param_idxs[i]]
             for j in range(i+1, num_atoms):
                 if not exclusions[i, j]:
-                    qj = params[param_idxs[j]]
+                    qj = params_np[param_idxs[j]]
                     qij = qi * qj
                     dij = np.linalg.norm(x0[i] - x0[j])
                     ref_nrg += qij/dij
@@ -57,7 +128,7 @@ class TestNonbondedForce(unittest.TestCase):
         test_nrg_op = ef.energy(x_ph)
 
         test_nrg = sess.run(test_nrg_op, feed_dict={x_ph: x0})
-        np.testing.assert_almost_equal(ref_nrg, test_nrg)
+        np.testing.assert_almost_equal(ref_nrg, test_nrg, decimal=13)
 
         test_grads_op = ef.gradients(x_ph)
         test_grads = sess.run(test_grads_op, feed_dict={x_ph: x0})
@@ -65,6 +136,9 @@ class TestNonbondedForce(unittest.TestCase):
         net_force = np.sum(test_grads, axis=0)
         # this also checks that there are no NaNs
         np.testing.assert_almost_equal(net_force, [0,0,0], decimal=14)
+
+        assert not np.any(np.isnan(sess.run(ef.hessians(x_ph), feed_dict={x_ph: x0})))
+        assert not np.any(np.isnan(sess.run(ef.mixed_partials(x_ph), feed_dict={x_ph: x0})))
 
 
 
