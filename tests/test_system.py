Fix gradients

tests/test_system.py
@@ -26,17 +26,6 @@ def addExclusionsToSet(bonded12, exclusions, baseParticle, fromParticle, current
         if currentLevel > 0:
             addExclusionsToSet(bonded12, exclusions, baseParticle, i, currentLevel-1)
 
-
-# void NonbondedForce::addExclusionsToSet(const vector<set<int> >& bonded12, set<int>& exclusions, int baseParticle, int fromParticle, int currentLevel) const {
-#     for (int i : bonded12[fromParticle]) {
-#         if (i != baseParticle)
-#             exclusions.insert(i);
-#         if (currentLevel > 0)
-#             addExclusionsToSet(bonded12, exclusions, baseParticle, i, currentLevel-1);
-#     }
-# }
-
-
 def deserialize_system(xml_file):
     """
     Deserialize an openmm XML file into a set of functional forms
@@ -48,10 +37,6 @@ def deserialize_system(xml_file):
     masses = []
 
     all_nrgs = []
-
-    # all_bonds = []
-    # all_angles = []
-    # all_torsions = []
 
     for child in root:
         if child.tag == 'Particles':
@@ -300,8 +285,6 @@ def deserialize_state(xml_file):
     for child in root:
         if child.tag == 'Energies':
             pot_nrg = np.float64(child.attrib['PotentialEnergy'])
-            # for subchild in child:
-                # masses.append(np.float64(subchild.attrib['mass']))
         elif child.tag == 'Positions':
             for subchild in child:
                 x, y, z = np.float64(subchild.attrib['x']), np.float64(subchild.attrib['y']), np.float64(subchild.attrib['z'])
@@ -363,6 +346,8 @@ class TestAlaAlaAla(unittest.TestCase):
         lj_nrg_val, lj_grad_val, es_nrg_val, es_grad_val = sess.run([lj_nrg_op, lj_grad_op, es_nrg_op, es_grad_op], feed_dict={x_ph: x0})
         tot_e = lj_nrg_val + es_nrg_val
         np.testing.assert_almost_equal(ref_nrg, tot_e)
+        grad_val = lj_grad_val + es_grad_val
+        np.testing.assert_almost_equal(ref_forces, grad_val*-1)
 
         # GBSA
         ref_nrg, x0, velocities, ref_forces = deserialize_state(get_data('state4.xml'))

timemachine/functionals/nonbonded.py
@@ -144,8 +144,12 @@ class Electrostatic(Energy):
         else:
             d2ij = tf.reduce_sum(tf.pow(ri-rj, 2), axis=-1)
 
-        d2ij = tf.where(d2ij != 0.0, d2ij, tf.zeros_like(d2ij))
-        dij_inverse = 1/tf.sqrt(d2ij)
+        ones_mask = tf.ones(shape=[self.num_atoms, self.num_atoms], dtype=tf.int32)
+        on_diag_mask = tf.matrix_band_part(ones_mask, 0, 0)
+
+        # mask = d2ij != 0.0 doesn't work because gradients propagate through the first arg
+        d2ij_where = tf.where(tf.cast(ones_mask - on_diag_mask, dtype=tf.bool), d2ij, tf.zeros_like(d2ij))
+        dij_inverse = 1/tf.sqrt(d2ij_where)
 
         if self.cutoff is not None:
             # apply only to fully non-excepted terms
@@ -159,21 +163,25 @@ class Electrostatic(Energy):
         dij_inverse_mask =  tf.boolean_mask(dij_inverse, direct_mask)
         eij_direct = qij_direct_mask * dij_inverse_mask
 
-        if box is not None:
-            # We adjust direct by the erfc, and adjust the reciprocal space's
-            # exclusionary contribution by the direction space weighted by erf
-            eij_direct *= tf.erfc(self.alphaEwald*r_direct)
-            # exclusions to subtract from reciprocal space
-            qij_exclusion_mask = tf.boolean_mask(qij, self.exclusion_mask)
-            d2ij_exclusion_mask = tf.boolean_mask(d2ij, self.exclusion_mask)
-            r_exclusion = tf.sqrt(d2ij_exclusion_mask)
-            eij_exclusion = qij_exclusion_mask/r_exclusion
-            eij_exclusion *= tf.erf(self.alphaEwald*r_exclusion)
+        # return tf.gradients(eij_direct, d2ij)
 
-            # extra factor of 2 is to deal with the fact that we compute the full matrix as opposed to the upper right
-            return ONE_4PI_EPS0*tf.reduce_sum(eij_direct, axis=-1)/2, ONE_4PI_EPS0*tf.reduce_sum(eij_exclusion, axis=-1)
-        else:
-            return ONE_4PI_EPS0*tf.reduce_sum(eij_direct, axis=-1)/2, None
+        return ONE_4PI_EPS0*tf.reduce_sum(eij_direct, axis=-1)/2, None
+
+        # if box is not None:
+        #     # We adjust direct by the erfc, and adjust the reciprocal space's
+        #     # exclusionary contribution by the direction space weighted by erf
+        #     eij_direct *= tf.erfc(self.alphaEwald*r_direct)
+        #     # exclusions to subtract from reciprocal space
+        #     qij_exclusion_mask = tf.boolean_mask(qij, self.exclusion_mask)
+        #     d2ij_exclusion_mask = tf.boolean_mask(d2ij, self.exclusion_mask)
+        #     r_exclusion = tf.sqrt(d2ij_exclusion_mask)
+        #     eij_exclusion = qij_exclusion_mask/r_exclusion
+        #     eij_exclusion *= tf.erf(self.alphaEwald*r_exclusion)
+
+        #     # extra factor of 2 is to deal with the fact that we compute the full matrix as opposed to the upper right
+        #     return ONE_4PI_EPS0*tf.reduce_sum(eij_direct, axis=-1)/2, ONE_4PI_EPS0*tf.reduce_sum(eij_exclusion, axis=-1)
+        # else:
+        #     return ONE_4PI_EPS0*tf.reduce_sum(eij_direct, axis=-1)/2, None
 
     def reciprocal_energy(self, conf, box):
         assert box is not None