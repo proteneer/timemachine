37a156c2bf080263919f1f7b3625f3da1069455a
Yutong Zhao <proteneer@gmail.com>
2019-02-01 17:32:39 -0500

    CircleCI
    Typo
    
    Add a makefile
    
    Try c++17
    
    Debug gcc version
    
    Remove auto breaking gcc.
    
    Undo skip.
    
    GBSA_WIP
    
    WIP

---
+++ timemachine/cpu_functionals/make.sh
@@ -0,0 +1,7 @@
+PLATFORM_FLAGS="";
+if [ "$(uname)" == "Darwin" ]; then
+	PLATFORM_FLAGS="-undefined dynamic_lookup";
+    # Do something under Mac OS X platform        
+fi
+
+g++ -g -O0 -march=native -Wall -shared -std=c++11 -fPIC $PLATFORM_FLAGS `python3 -m pybind11 --includes` wrappers.cpp -o energy`python3-config --extension-suffix`
