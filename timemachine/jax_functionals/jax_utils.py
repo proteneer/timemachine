2a7a5f9032463d65f3cfb8f8961263751589efd8
Yutong Zhao <proteneer@gmail.com>
2019-05-06 13:00:19 -0400

    WIP barostat

--- timemachine/jax_functionals/jax_utils.py
+++ timemachine/jax_functionals/jax_utils.py
@@ -1,6 +1,24 @@
 import jax.numpy as np
 
 
+def rescale_coordinates(
+    conf,
+    indices,
+    box,
+    scales):
+    
+    mol_sizes = np.expand_dims(onp.bincount(indices), axis=1)
+    mol_centers = jax.ops.segment_sum(coords, indices)/mol_sizes
+
+    new_centers = mol_centers - box[2]*np.floor(np.expand_dims(mol_centers[...,2], axis=-1)/box[2][2])
+    new_centers -= box[1]*np.floor(np.expand_dims(new_centers[...,1], axis=-1)/box[1][1])
+    new_centers -= box[0]*np.floor(np.expand_dims(new_centers[...,0], axis=-1)/box[0][0])
+
+    offset = new_centers - mol_centers
+
+    return conf + offset[indices]
+
+
 def delta_r(ri, rj, box=None):
     diff = ri - rj # this can be either N,N,3 or B,3
     if box is not None:
