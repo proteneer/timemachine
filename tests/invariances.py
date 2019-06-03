3ef6ccf645c2ac99e591d6668c456b8e0ba12e24
Guowei Qi <gqi@relaytx.com>
2019-06-03 17:39:00 -0400

    use relative error instead of decimal accuracy

--- tests/invariances.py
+++ tests/invariances.py
@@ -27,43 +27,43 @@ def assert_potential_invariance(energy_fn, x0, params, box=None):
     mixed = d2Edxdp(x0,params,None)[0][0]
 
     for _ in range(3):
-        trans_vector = np.random.rand(3)
+        trans_vector = np.random.rand(3).astype(dtype=np.float64)
         trans_x = x0 + trans_vector
         trans_energy = energy_fn(trans_x,params,None)
         trans_forces = force_fn(trans_x,params,None)[0]
         trans_dEdp = dEdp(trans_x,params,None)[0]
         trans_mixed = d2Edxdp(trans_x,params,None)[0][0]
-        np.testing.assert_almost_equal(trans_energy,energy,decimal=5)
-        np.testing.assert_array_almost_equal(trans_forces,forces,decimal=5)
-        np.testing.assert_array_almost_equal(trans_dEdp,dparam,decimal=5)
-        np.testing.assert_array_almost_equal(trans_mixed,mixed,decimal=5)
+        np.testing.assert_allclose(trans_energy,energy,rtol=1e-12)
+        np.testing.assert_allclose(trans_forces,forces,rtol=1e-12)
+        np.testing.assert_allclose(trans_dEdp,dparam,rtol=1e-12)
+        np.testing.assert_allclose(trans_mixed,mixed,rtol=1e-12)
          
     for _ in range(3):
-        rot_matrix = special_ortho_group.rvs(3)
+        rot_matrix = special_ortho_group.rvs(3).astype(dtype=np.float64)
         rot_x = np.matmul(x0, rot_matrix)
         rot_energy = energy_fn(rot_x,params,None)
         rot_forces = force_fn(rot_x,params,None)[0]
         rot_dEdp = dEdp(rot_x,params,None)[0]
         rot_mixed = d2Edxdp(rot_x,params,None)[0][0]
-        np.testing.assert_array_almost_equal(rot_energy,energy,decimal=5)
-        np.testing.assert_array_almost_equal(rot_forces,np.matmul(forces,rot_matrix),decimal=5) 
-        np.testing.assert_array_almost_equal(rot_dEdp,dparam,decimal=5)
+        np.testing.assert_allclose(rot_energy,energy,rtol=1e-12)
+        np.testing.assert_allclose(rot_forces,np.matmul(forces,rot_matrix),rtol=1e-12) 
+        np.testing.assert_allclose(rot_dEdp,dparam,rtol=1e-12)
         for i in range(rot_mixed.shape[0]):
-            np.testing.assert_array_almost_equal(rot_mixed[i],np.matmul(mixed[i],rot_matrix),decimal=5) 
+            np.testing.assert_allclose(rot_mixed[i],np.matmul(mixed[i],rot_matrix),rtol=1e-12) 
 
     for _ in range(3):
-        trans_vector = np.random.rand(3)
-        rot_matrix = special_ortho_group.rvs(3)
+        trans_vector = np.random.rand(3).astype(dtype=np.float64)
+        rot_matrix = special_ortho_group.rvs(3).astype(dtype=np.float64)
         comp_x = np.matmul(x0,rot_matrix) + trans_vector
         comp_energy = energy_fn(comp_x,params,None)
         comp_forces = force_fn(comp_x,params,None)[0]
         comp_dEdp = dEdp(comp_x,params,None)[0]
         comp_mixed = d2Edxdp(comp_x,params,None)[0][0]
-        np.testing.assert_array_almost_equal(comp_energy,energy,decimal=5)
-        np.testing.assert_array_almost_equal(comp_forces,np.matmul(forces,rot_matrix),decimal=5) 
-        np.testing.assert_array_almost_equal(comp_dEdp,dparam,decimal=5)
+        np.testing.assert_allclose(comp_energy,energy,rtol=1e-12)
+        np.testing.assert_allclose(comp_forces,np.matmul(forces,rot_matrix),rtol=1e-12) 
+        np.testing.assert_allclose(comp_dEdp,dparam,rtol=1e-12)
         for i in range(comp_mixed.shape[0]):
-            np.testing.assert_array_almost_equal(comp_mixed[i],np.matmul(mixed[i],rot_matrix),decimal=5) 
+            np.testing.assert_allclose(comp_mixed[i],np.matmul(mixed[i],rot_matrix),rtol=1e-12) 
         
     if box is not None:
         energy = energy_fn(x0,params,box)
@@ -75,15 +75,15 @@ def assert_potential_invariance(energy_fn, x0, params, box=None):
         mixed = d2Edxdp(x0,params,box)[0][0]
 
         for _ in range(3):
-            trans_vector = np.random.rand(3)
+            trans_vector = np.random.rand(3).astype(dtype=np.float64)
             trans_x = x0 + trans_vector
             trans_energy = energy_fn(trans_x,params,box)
             trans_forces = force_fn(trans_x,params,box)[0]
             trans_dEdp = dEdp(trans_x,params,box)[0]
             trans_mixed = d2Edxdp(trans_x,params,box)[0][0]
-            np.testing.assert_almost_equal(trans_energy, energy,decimal=5)
-            np.testing.assert_array_almost_equal(trans_forces, forces,decimal=5)
-            np.testing.assert_array_almost_equal(trans_dEdp,dparam,decimal=5)
-            np.testing.assert_array_almost_equal(trans_mixed, mixed,decimal=5)
+            np.testing.assert_allclose(trans_energy, energy,rtol=1e-12)
+            np.testing.assert_allclose(trans_forces, forces,rtol=1e-12)
+            np.testing.assert_allclose(trans_dEdp,dparam,rtol=1e-12)
+            np.testing.assert_allclose(trans_mixed, mixed,rtol=1e-12)
        
        # timemachine does not support testing rotations on periodic systems
