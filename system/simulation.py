c2c7392760eccdcd68abc0746d8b83267b49aa0d
Guowei Qi <gqi@relaytx.com>
2019-08-16 10:31:21 -0400

    implementing RMSD and enthalpy training

--- system/simulation.py
+++ system/simulation.py
@@ -7,6 +7,10 @@ from timemachine.integrator import langevin_coefficients
 
 from timemachine import constants
 
+from simtk.openmm.app import PDBFile, DCDFile, Topology
+
+import scipy
+
 def average_E_and_derivatives(reservoir):
     """
     Compute the average energy and derivatives
@@ -15,8 +19,8 @@ def average_E_and_derivatives(reservoir):
     ----------
     reservoir: list of reservoir
         [
-            [E, dE_dx, dx_dp, dE_dp],
-            [E, dE_dx, dx_dp, dE_dp],
+            [E, dE_dx, dx_dp, dE_dp, x],
+            [E, dE_dx, dx_dp, dE_dp, x],
             ...
         ]
 
@@ -25,6 +29,9 @@ def average_E_and_derivatives(reservoir):
     Average energy, analytic total derivative, and thermodynamic gradient
 
     """
+    if len(reservoir) == 0:
+        return np.nan, 0, 0
+
     running_sum_total_derivs = None
     running_sum_E = 0
     n_reservoir = len(reservoir)
@@ -40,17 +47,21 @@ def average_E_and_derivatives(reservoir):
         if running_sum_EmultdE_dp is None:
             running_sum_EmultdE_dp = np.zeros_like(dE_dp)
 
-        # tensor contract [N,3] with [P, N, 3] and dE_d
-        total_dE_dp = np.einsum('kl,mkl->m', dE_dx, dx_dp) + dE_dp
-        running_sum_total_derivs += total_dE_dp
-        running_sum_E += E
+        if np.isnan(E):
+            n_reservoir -= 1
+        else:
+            # tensor contract [N,3] with [P, N, 3] and add dE_dp for a shape P array
+            total_dE_dp = np.einsum('kl,mkl->m', dE_dx, dx_dp) + dE_dp
+            running_sum_total_derivs += total_dE_dp
+            running_sum_E += E
 
+            running_sum_dE_dp += dE_dp
+            running_sum_EmultdE_dp += E*dE_dp
 
-        running_sum_dE_dp += dE_dp
-        running_sum_EmultdE_dp += E*dE_dp
+        if n_reservoir < 1:
+            return np.nan, 0, 0
 
-    # compute the thermodynamic average:
-    # boltz*(<E><dE/dp> - <E.dE/dp>)
+    # compute the thermodynamic average: boltz*(<E><dE/dp> - <E.dE/dp>)
     thermo_deriv = running_sum_E*running_sum_dE_dp - running_sum_EmultdE_dp
 
     return running_sum_E/n_reservoir, running_sum_total_derivs/n_reservoir, -constants.BOLTZ*(thermo_deriv/n_reservoir)/(100)
@@ -64,61 +75,88 @@ def run_simulation(
     masses,
     dp_idxs,
     n_samples=200,
-    n_steps=1000):
+    start_dt=1e-6,
+    end_dt=1e-2,
+    scale=1.05,
+    convergence_tolerance=10
+    ):
+
+    num_atoms = len(masses)
 
     potentials = forcefield.merge_potentials(potentials)
-        
-    dt = 0.0005
+
+    dt = start_dt
     ca, cb, cc = langevin_coefficients(
         temperature=25.0,
         dt=dt,
-        friction=50,
+        friction=100,
         masses=masses
     )
 
-    m_dt, m_ca, m_cb, m_cc = dt, 0.5, cb, np.zeros_like(masses)
+    # m_dt, m_ca, m_cb, m_cc = dt, 0.5, cb, np.zeros_like(masses)
 
-    opt = custom_ops.LangevinOptimizer_f32(
+    m_dt, m_ca, m_cb, m_cc = dt, 0.9, np.ones_like(cb)/10000, np.zeros_like(masses)
+
+    opt = custom_ops.LangevinOptimizer_f64(
         m_dt,
         m_ca,
-        m_cb.astype(np.float32),
-        m_cc.astype(np.float32)
+        m_cb.astype(np.float64),
+        m_cc.astype(np.float64)
     )
 
     v0 = np.zeros_like(conf)
     dp_idxs = dp_idxs.astype(np.int32)
 
-    ctxt = custom_ops.Context_f32(
+    ctxt = custom_ops.Context_f64(
         potentials,
         opt,
-        params.astype(np.float32),
-        conf.astype(np.float32), # x0
-        v0.astype(np.float32), # v0
+        params.astype(np.float64),
+        conf.astype(np.float64), # x0
+        v0.astype(np.float64), # v0
         dp_idxs
     )
 
-    # Minimize the system and carry the gradient over
-    # call system converged when the delta is .25 kcal)
-    max_iter = 25000
-    window_size = 150
-    minimization_energies = []
+    tolerance = convergence_tolerance
+
+    def mean_norm(conf):
+        norm_x = np.dot(conf.reshape(-1), conf.reshape(-1))/num_atoms
+        return np.sqrt(norm_x)
+
+    # set normalized convergence criteria
+    x_norm = mean_norm(conf)
+    x_norm = np.where(x_norm < 1, 1, x_norm)
+    epsilon = tolerance/x_norm
+
+    max_iter = 10000
     for i in range(max_iter):
+        # adjust dt by a scale factor for each time step up until it reaches a maximum value
+        dt *= scale
+        dt = min(dt, end_dt)
+        opt.set_dt(dt)
         ctxt.step()
-        E = ctxt.get_E()
-        minimization_energies.append(E)
-        if len(minimization_energies) > window_size:
-            window_std = np.std(minimization_energies[-window_size:])
-            if window_std < 1.046/2:
+        if i > 50 and i % 100 == 0:
+            if np.isnan(ctxt.get_E()):
+                final_energy = np.nan
                 break
-        # if i % 1000 == 0:
-            # print("minimization", i, E)
+            dE_dx = ctxt.get_dE_dx()
+            g_norm = mean_norm(dE_dx)
+            x_norm = mean_norm(conf)
+            # minimization converges when the norm of the forces are less than a certain epsilon
+            if g_norm < epsilon:
+                break
 
+    # For training purposes, don't raise an exception when energy is nan or minimization doesn't converge
+    # Unsuccessful minimizations will have derivatives set to 0 and the data points will not be recorded in the loss plot
     if i == max_iter-1:
-        raise Exception("Energy minimization failed to converge in ", i, "steps")
+        final_energy = np.nan
+        print("Energy minimization failed to converge in ", i, "steps")
     else:
-        print("Minimization converged in", i, "steps to", E)
+        final_energy = ctxt.get_E()
+        print("Minimization converged in", i, "steps to", final_energy)
 
-    # #modify integrator to do dynamics
+
+    # IN PROGRESS: dynamics are currently turned off for training
+    # # modify integrator to do dynamics
     # opt.set_dt(dt)
     # opt.set_coeff_a(ca)
     # opt.set_coeff_b(cb)
@@ -129,7 +167,7 @@ def run_simulation(
     # R = []
     # count = 0
 
-    # for count in range(n_steps):
+    # for count in range(10000):
 
     #     # closure around R, and ctxt
     #     def get_reservoir_item(step):
@@ -137,17 +175,24 @@ def run_simulation(
     #         dE_dx = ctxt.get_dE_dx()
     #         dx_dp = ctxt.get_dx_dp()
     #         dE_dp = ctxt.get_dE_dp()
-    #         min_dx = np.amin(dx_dp)
-    #         max_dx = np.amax(dx_dp)
-    #         lhs = np.einsum('kl,mkl->m', dE_dx, dx_dp)
-    #         total_dE_dp = lhs + dE_dp
+
+    #         # min_dx = np.amin(dx_dp)
+    #         # max_dx = np.amax(dx_dp)
+    #         # lhs = np.einsum('kl,mkl->m', dE_dx, dx_dp)
+    #         # total_dE_dp = lhs + dE_dp
 
     #         # print(step, total_dE_dp)
 
-    #         limits = 1e5
+    #         x = ctxt.get_x()
+
+    #         # limits = 1e5
     #         # if min_dx < -limits or max_dx > limits:
     #             # raise Exception("Derivatives blew up:", min_dx, max_dx)
+
     #         return [E, dE_dx, dx_dp, dE_dp, step]
+
+    #     if count % 1000 == 0:
+    #         print(count, ctxt.get_E())
 
     #     if count < k:
     #         R.append(get_reservoir_item(count))
@@ -155,19 +200,16 @@ def run_simulation(
     #         j = random.randint(0, count)
     #         if j < k:
     #             R[j] = get_reservoir_item(count)
-    #             np.set_printoptions(suppress=True)
-
-    #     if count % 5000 == 0:
-    #         print("count", count)
 
     #     ctxt.step()
 
+    # IN PROGRESS: currently not reservoir sampling, just taking the final state
     R = [[
-        ctxt.get_E(),
+        final_energy,
         ctxt.get_dE_dx(),
         ctxt.get_dx_dp(),
         ctxt.get_dE_dp(),
-        0
+        ctxt.get_x()
     ]]
 
     return R
