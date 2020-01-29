d36478336a4c96abe7df3def19e164c331e8d92d
Chuan Tian <tianchuan1015@gmail.com>
2020-01-29 10:14:55 -0500

    add docstring to bar.py

--- fe/bar.py
+++ fe/bar.py
@@ -5,11 +5,42 @@ import pymbar
 import jax
 
 def EXP(w):
+    """
+    Estimate free energy difference using exponential averaging
+
+    Parameters
+    ----------
+    w : np.ndarray, float, (2, N)
+        forward and reverse work for N frames
+
+    Returns
+    ------
+    deltaF : scalar, float
+        free energy difference
+
+    """
     T = float(jnp.size(w))
     deltaF = - (logsumexp(-w) - jnp.log(T))
     return deltaF
 
 def BARzero(w, deltaF):
+    """
+    A function that when zeroed is equivalent to the solution of BAR
+
+    Parameters
+    ---------
+    w : np.ndarray, float, (2, N)
+        forward and reverse work for N frames
+
+    deltaF : scalar, float
+        free energy difference
+
+    Returns
+    ------
+    fzero : scalar, float
+        a variable that is zeroed when deltaF satisfies BAR
+
+    """
     w_F = w[0]
     w_R = w[1]
     T_F = len(w_F)
@@ -19,25 +50,31 @@ def BARzero(w, deltaF):
     exp_arg_F = (M + w_F - deltaF)
     max_arg_F = jnp.where(exp_arg_F < 0, 0.0, exp_arg_F)
 
-    # try:
     log_f_F = - max_arg_F - jnp.log(jnp.exp(-max_arg_F) + jnp.exp(exp_arg_F - max_arg_F))
-    # except:
-    #    return np.nan
     log_numer = logsumexp(log_f_F)
     exp_arg_R = -(M - w_R - deltaF)
     max_arg_R = jnp.where(exp_arg_R < 0, 0.0, exp_arg_R)
 
-    #try:
     log_f_R = - max_arg_R - jnp.log(jnp.exp(-max_arg_R) + jnp.exp(exp_arg_R - max_arg_R))
-    # except:
-    # print("The input data results in overflow in BAR")
-    # return np.nan
 
     log_denom = logsumexp(log_f_R)
     fzero = log_numer - log_denom
     return fzero
 
 def dG_dw(w):
+    """
+    A function that calculates gradient of free energy difference with respect to work
+
+    Parameters
+    ---------
+    w : np.ndarray, float, (2, N)
+
+    Returns
+    ------
+    dG_dw : np.ndarray, float, (2, N)
+        The gradient of free energy difference with respect to work
+
+    """
     dG, _ = pymbar.BAR(w[0], w[1])
     dBAR_dw = jax.grad(BARzero, argnums=(0,))
     dBAR_dA = jax.grad(BARzero, argnums=(1,))
