195e8afc7004151aaa6ebf0b06f37bd1ae17b1c5
Yutong Zhao <proteneer@gmail.com>
2020-05-08 21:10:20 -0400

    WIP

--- fe/parameterize_rbfe_ti.py
+++ fe/parameterize_rbfe_ti.py
@@ -209,7 +209,7 @@ if __name__ == "__main__":
         ti_lambdas = np.linspace(0, 1, args.num_windows)
         # ti_lambdas = np.ones(args.num_windows)*0.1
         # ti_lambdas = np.array([0.2, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2])
-        # ti_lambdas = np.array([0.17, 0.17, 0.17, 0.17, 0.17, 0.17, 0.17])
+        # ti_lambdas = np.array([0.07, 0.07, 0.07, 0.07, 0.07, 0.07, 0.07])
         # all_du_dls = []
 
         all_processes = []
@@ -247,7 +247,7 @@ if __name__ == "__main__":
             # zero-out
             # if args.n_frames is 0:
                 # writer = None
-            writer = None
+            # writer = None
 
             host_conf = []
             for x,y,z in host_pdb.positions:
