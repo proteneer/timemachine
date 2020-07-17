71e2e379d43c8290cef872d0b4c9a64d539e8fc4
Yutong Zhao <proteneer@gmail.com>
2020-07-17 01:07:00 -0400

    WIP

--- training/abfe.py
+++ training/abfe.py
@@ -76,10 +76,6 @@ if __name__ == "__main__":
 
     # process the host first
     host_pdbfile = general_cfg['protein_pdb']
-    from simtk.openmm.app import PDBFile
-    host_pdb = PDBFile(host_pdbfile)
-
-    # debug
 
     # (tbd): set to MCS if this is None
     stage_dGs = []
