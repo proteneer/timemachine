924bd2ff4f93012fbeb5abaad27ecb10ed8880e2
Yutong Zhao <proteneer@gmail.com>
2020-10-06 14:33:33 -0400

    WIP

--- training/worker.py
+++ training/worker.py
@@ -68,8 +68,12 @@ class Worker(service_pb2_grpc.WorkerServicer):
         )
 
 
-        for p in min_bps:
-            print(p, p.execute(simulation.x, simulation.box, 1.0))
+        for op, p in zip(simulation.potentials, min_bps):
+            if isinstance(op, timemachine.lib.potentials.Nonbonded):
+                print(op.params)
+                force, du_dl, u = p.execute(simulation.x, simulation.box, 1.0)
+                print("host forces", force[:request.num_host_atoms])
+                print("ligand forces", force[request.num_host_atoms:])
 
         print("starting_geometry", min_ctxt.get_x_t())
 
