c154077316b0f8bc28dca167635aef08ba3fb1b0
Yutong Zhao <proteneer@gmail.com>
2020-10-14 17:04:06 -0400

    Update pose docking script for schmolly

--- docking/dock_setup.py
+++ docking/dock_setup.py
@@ -2,181 +2,157 @@ import numpy as np
 
 from simtk.openmm import app
 
-from fe.utils import to_md_units
 from ff.handlers import bonded, nonbonded, openmm_deserializer
 
-def create_system(
+from timemachine.lib import potentials
+
+def combine_parameters(guest_q, guest_lj, host_qlj):
+    guest_qlj = np.concatenate([
+        np.reshape(guest_q, (-1, 1)),
+        np.reshape(guest_lj, (-1, 2))
+    ], axis=1)
+
+    return np.concatenate([host_qlj, guest_qlj])
+
+def combine_potentials(
+    ff_handlers,
     guest_mol,
-    host_pdb,
-    handlers):
+    host_system,
+    precision):
     """
-    Initialize a self-encompassing System object that we can serialize and simulate.
+    This function is responsible for figuring out how to take two separate hamiltonians
+    and combining them into one sensible alchemical system.
 
     Parameters
     ----------
 
-    guest_mol: rdkit.ROMol
-        guest molecule
-        
-    host_pdb: openmm.PDBFile
-        host system from OpenMM
+    ff_handlers: list of forcefield handlers
+        Small molecule forcefield handlers
 
-    handlers: list of timemachine.ops.Gradients
-        forcefield handlers used to parameterize the small molecule
- 
+    guest_mol: Chem.ROMol
+        RDKit molecule
+
+    host_system: openmm.System
+        Host system to be deserialized
+
+    precision: np.float32 or np.float64
+        Numerical precision of the functional form
+
     Returns
     -------
-    3-tuple
-        x0, combined_masses, final_gradients
+    tuple
+        Returns a list of lib.potentials objects, combined masses, and a list of
+        their corresponding vjp_fns back into the forcefield
 
     """
 
-    guest_masses = np.array([a.GetMass() for a in guest_mol.GetAtoms()], dtype=np.float64)
-
-    amber_ff = app.ForceField('amber99sbildn.xml', 'amber99_obc.xml')
-    host_system = amber_ff.createSystem(
-        host_pdb.topology,
-        nonbondedMethod=app.NoCutoff,
-        constraints=None,
-        rigidWater=False
+    host_potentials, host_masses = openmm_deserializer.deserialize_system(
+        host_system,
+        precision,
+        cutoff=1.0
     )
 
-    host_fns, host_masses = openmm_deserializer.deserialize_system(host_system)
+    host_nb_bp = None
 
-    num_host_atoms = len(host_masses)
-    num_guest_atoms = guest_mol.GetNumAtoms()
+    combined_potentials = []
+    
 
-    # Name, Args, vjp_fn
-    final_gradients = []
-
-    for item in host_fns: 
-
-        if item[0] == 'LennardJones':
-            host_lj_params = item[1]
-        elif item[0] == 'Charges':
-            host_charge_params = item[1]
-        elif item[0] == 'GBSA':
-            host_gb_params = item[1][0]
-            host_gb_props = item[1][1:]
-        elif item[0] == 'Exclusions':
-            host_exclusions = item[1]
+    for bp in host_potentials:
+        if isinstance(bp, potentials.Nonbonded):
+            # (ytz): hack to ensure we only have one nonbonded term
+            assert host_nb_bp is None
+            host_nb_bp = bp
         else:
-            final_gradients.append((item[0], item[1]))
+            combined_potentials.append(bp)
+            
 
-    guest_exclusion_idxs, guest_scales = nonbonded.generate_exclusion_idxs(
+    guest_masses = np.array([a.GetMass() for a in guest_mol.GetAtoms()], dtype=np.float64)
+
+    num_guest_atoms = len(guest_masses)
+    num_host_atoms = len(host_masses)
+
+    combined_masses = np.concatenate([host_masses, guest_masses])
+
+
+
+    for handle in ff_handlers:
+        results = handle.parameterize(guest_mol)
+        if isinstance(handle, bonded.HarmonicBondHandler):
+            bond_idxs, (bond_params, vjp_fn) = results
+            bond_idxs += num_host_atoms
+            combined_potentials.append(potentials.HarmonicBond(bond_idxs, precision=precision).bind(bond_params))
+            
+        elif isinstance(handle, bonded.HarmonicAngleHandler):
+            angle_idxs, (angle_params, vjp_fn) = results
+            angle_idxs += num_host_atoms
+            combined_potentials.append(potentials.HarmonicAngle(angle_idxs, precision=precision).bind(angle_params))
+            
+        elif isinstance(handle, bonded.ProperTorsionHandler):
+            torsion_idxs, (torsion_params, vjp_fn) = results
+            torsion_idxs += num_host_atoms
+            combined_potentials.append(potentials.PeriodicTorsion(torsion_idxs, precision=precision).bind(torsion_params))
+            
+        elif isinstance(handle, bonded.ImproperTorsionHandler):
+            torsion_idxs, (torsion_params, vjp_fn) = results
+            torsion_idxs += num_host_atoms
+            combined_potentials.append(potentials.PeriodicTorsion(torsion_idxs, precision=precision).bind(torsion_params))
+            
+        elif isinstance(handle, nonbonded.AM1CCCHandler):
+            charge_handle = handle
+            guest_charge_params, guest_charge_vjp_fn = results
+        elif isinstance(handle, nonbonded.LennardJonesHandler):
+            guest_lj_params, guest_lj_vjp_fn = results
+            lj_handle = handle
+        else:
+            print("Warning: skipping handler", handle)
+            pass
+
+    # process nonbonded terms
+    combined_nb_params = combine_parameters(guest_charge_params, guest_lj_params, host_nb_bp.params)
+
+    # these vjp_fns take in adjoints of combined_params and returns derivatives
+    # appropriate to the underlying handler
+    
+
+    # tbd change scale 14 for electrostatics
+    guest_exclusion_idxs, guest_scale_factors = nonbonded.generate_exclusion_idxs(
         guest_mol,
         scale12=1.0,
         scale13=1.0,
         scale14=0.5
     )
 
-    guest_exclusion_idxs += num_host_atoms
-    guest_lj_exclusion_scales = guest_scales
-    guest_charge_exclusion_scales = guest_scales
+    # allow the ligand to be alchemically decoupled
+    # a value of one indicates that we allow the atom to be adjusted by the lambda value
+    guest_lambda_offset_idxs = np.ones(len(guest_masses), dtype=np.int32) 
 
-    host_exclusion_idxs = host_exclusions[0]
-    host_lj_exclusion_scales = host_exclusions[1]
-    host_charge_exclusion_scales = host_exclusions[2]
+    # use same scale factors until we modify 1-4s for electrostatics
+    guest_scale_factors = np.stack([guest_scale_factors, guest_scale_factors], axis=1)
 
-    combined_exclusion_idxs = np.concatenate([host_exclusion_idxs, guest_exclusion_idxs])
-    combined_lj_exclusion_scales = np.concatenate([host_lj_exclusion_scales, guest_lj_exclusion_scales])
-    combined_charge_exclusion_scales = np.concatenate([host_charge_exclusion_scales, guest_charge_exclusion_scales])
+    combined_lambda_offset_idxs = np.concatenate([host_nb_bp.get_lambda_offset_idxs(), guest_lambda_offset_idxs])
+    combined_exclusion_idxs = np.concatenate([host_nb_bp.get_exclusion_idxs(), guest_exclusion_idxs + num_host_atoms])
 
-    for handle in handlers:
-        results = handle.parameterize(guest_mol)
-
-        if isinstance(handle, bonded.HarmonicBondHandler):
-            bond_idxs, (bond_params, _) = results
-            bond_idxs += num_host_atoms
-            final_gradients.append(("HarmonicBond", (bond_idxs, bond_params)))
-        elif isinstance(handle, bonded.HarmonicAngleHandler):
-            angle_idxs, (angle_params, _) = results
-            angle_idxs += num_host_atoms
-            final_gradients.append(("HarmonicAngle", (angle_idxs, angle_params)))
-        elif isinstance(handle, bonded.ProperTorsionHandler):
-            torsion_idxs, (torsion_params, _) = results
-            torsion_idxs += num_host_atoms
-            final_gradients.append(("PeriodicTorsion", (torsion_idxs, torsion_params)))
-        elif isinstance(handle, bonded.ImproperTorsionHandler):
-            torsion_idxs, (torsion_params, _) = results
-            torsion_idxs += num_host_atoms
-            final_gradients.append(("PeriodicTorsion", (torsion_idxs, torsion_params)))
-        elif isinstance(handle, nonbonded.LennardJonesHandler):
-            guest_lj_params, _ = results
-            combined_lj_params = np.concatenate([host_lj_params, guest_lj_params])
-        elif isinstance(handle, nonbonded.SimpleChargeHandler):
-            guest_charge_params, _ = results
-            combined_charge_params = np.concatenate([host_charge_params, guest_charge_params])
-        elif isinstance(handle, nonbonded.GBSAHandler):
-            guest_gb_params, _ = results
-            combined_gb_params = np.concatenate([host_gb_params, guest_gb_params])
-        elif isinstance(handle, nonbonded.AM1BCCHandler):
-            guest_charge_params, _ = results
-            combined_charge_params = np.concatenate([host_charge_params, guest_charge_params])
-        elif isinstance(handle, nonbonded.AM1CCCHandler):
-            guest_charge_params, _ = results
-            combined_charge_params = np.concatenate([host_charge_params, guest_charge_params])
-        else:
-            raise Exception("Unknown Handler", handle)
-
-    host_conf = []
-    for x,y,z in host_pdb.positions:
-        host_conf.append([to_md_units(x),to_md_units(y),to_md_units(z)])
-    host_conf = np.array(host_conf)
-
-    conformer = guest_mol.GetConformer(0)
-    mol_a_conf = np.array(conformer.GetPositions(), dtype=np.float64)
-    mol_a_conf = mol_a_conf/10 # convert to md_units
-
-    center = np.mean(mol_a_conf, axis=0)
-
-    mol_a_conf -= center
-
-    from scipy.stats import special_ortho_group
-    mol_a_conf = np.matmul(mol_a_conf, special_ortho_group.rvs(3))
-    mol_a_conf += center
-
+    # print("EXCLUSIONS", host_nb_bp.get_exclusion_idxs())
     # assert 0
+    combined_scales = np.concatenate([host_nb_bp.get_scale_factors(), guest_scale_factors])
+    combined_beta = 2.0
 
+    combined_cutoff = 1.0 # nonbonded cutoff
 
+    combined_nb_params = np.asarray(combined_nb_params).copy()
 
-    x0 = np.concatenate([host_conf, mol_a_conf]) # combined geometry
-    v0 = np.zeros_like(x0)
+    # print(combined_nb_params[1758:])
+    # combined_nb_params[:, 0] = 0.0
 
-    N_C = num_host_atoms + num_guest_atoms
-    N_A = num_host_atoms
+    # print(combined_nb_params)
 
-    cutoff = 100000.0
-
-    combined_lambda_plane_idxs = np.zeros(N_C, dtype=np.int32)
-    combined_lambda_offset_idxs = np.zeros(N_C, dtype=np.int32)
-    combined_lambda_offset_idxs[num_host_atoms:] = 1
-
-    final_gradients.append((
-        'Nonbonded', (
-        np.asarray(combined_charge_params),
-        np.asarray(combined_lj_params),
+    combined_potentials.append(potentials.Nonbonded(
         combined_exclusion_idxs,
-        combined_charge_exclusion_scales,
-        combined_lj_exclusion_scales,
-        combined_lambda_plane_idxs,
+        combined_scales,
         combined_lambda_offset_idxs,
-        cutoff
-        )
-    ))
+        combined_beta,
+        combined_cutoff,
+        precision=precision).bind(combined_nb_params))
 
-    final_gradients.append((
-        'GBSA', (
-        np.asarray(combined_charge_params),
-        np.asarray(combined_gb_params),
-        combined_lambda_plane_idxs,
-        combined_lambda_offset_idxs,
-        *host_gb_props,
-        cutoff,
-        cutoff
-        )
-    ))
 
-    combined_masses = np.concatenate([host_masses, guest_masses])
-
-    return x0, combined_masses, final_gradients
+    return combined_potentials, combined_masses
--- docking/pose_dock.py
+++ docking/pose_dock.py
@@ -2,17 +2,25 @@ import numpy as np
 
 from rdkit import Chem
 import simtk.openmm
+from simtk.openmm import app
 from simtk.openmm.app import PDBFile
 from fe.pdb_writer import PDBWriter
 
 from docking import dock_setup
-from ff.handlers.deserialize import deserialize
-from timemachine.lib import ops, custom_ops
+from ff.handlers.deserialize import deserialize_handlers
+from timemachine.lib import LangevinIntegrator
+from timemachine.lib import custom_ops
 from io import StringIO
 
 from fe import system
+from fe.utils import to_md_units
+
 
 from matplotlib import pyplot as plt
+
+
+# from timemachine.lib import LangevinIntegrator
+
 
 if __name__ == "__main__":
 
@@ -31,34 +39,63 @@ if __name__ == "__main__":
         guest_mol
     )
 
-    ff_handlers = deserialize(open('ff/params/smirnoff_1_1_0_ccc.py').read())
+    ff_handlers = deserialize_handlers(open('ff/params/smirnoff_1_1_0_ccc.py').read())
 
-    x0, combined_masses, final_gradients = dock_setup.create_system(
-        guest_mol,
-        host_pdb,
-        ff_handlers
+    amber_ff = app.ForceField('amber99sbildn.xml', 'tip3p.xml')
+    host_system = amber_ff.createSystem(
+        host_pdb.topology,
+        nonbondedMethod=app.NoCutoff,
+        constraints=None,
+        rigidWater=False
     )
 
-    gradients = []
+    bps, masses = dock_setup.combine_potentials(
+        ff_handlers,
+        guest_mol,
+        host_system,
+        np.float32
+    )
 
-    for grad_name, grad_args in final_gradients:
-        op_fn = getattr(ops, grad_name)
-        grad = op_fn(*grad_args, precision=np.float32)
-        gradients.append(grad)
 
-    print(gradients)
+    host_conf = []
+    for x,y,z in host_pdb.positions:
+        host_conf.append([to_md_units(x),to_md_units(y),to_md_units(z)])
+    host_conf = np.array(host_conf)
+    conformer = guest_mol.GetConformer(0)
+    mol_conf = np.array(conformer.GetPositions(), dtype=np.float64)
+    mol_conf = mol_conf/10 # convert to md_units
+
+    # for applying a random rotation
+    # center = np.mean(mol_conf, axis=0)
+    # mol_conf -= center
+    # from scipy.stats import special_ortho_group
+    # mol_conf = np.matmul(mol_conf, special_ortho_group.rvs(3))
+    # mol_conf += center
+
+    x0 = np.concatenate([host_conf, mol_conf]) # combined geometry
+    v0 = np.zeros_like(x0)
+
+    # print(potentia)
 
     n_steps = 20000
 
-    integrator = system.Integrator(
-        steps=n_steps,
-        dt=1.5e-3,
-        temperature=300.0,
-        friction=50,
-        masses=combined_masses,
-        lamb=np.zeros(n_steps),
-        seed=42
-    )
+    # integrator = system.Integrator(
+    #     steps=n_steps,
+    #     dt=1.5e-3,
+    #     temperature=300.0,
+    #     friction=50,
+    #     masses=combined_masses,
+    #     lamb=np.zeros(n_steps),
+    #     seed=42
+    # )
+    seed = 2020
+    intg = LangevinIntegrator(
+        300,
+        1.5e-3,
+        1.0,
+        masses,
+        seed
+    ).impl()
 
     lowering_steps = 10000
 
@@ -68,23 +105,29 @@ if __name__ == "__main__":
     ])
 
 
-    stepper = custom_ops.AlchemicalStepper_f64(
-        gradients,
-        new_lambda_schedule
-        # integrator.lambs
-    )
+    # stepper = custom_ops.AlchemicalStepper_f64(
+    #     gradients,
+    #     new_lambda_schedule
+    #     # integrator.lambs
+    # )
+
+    box = np.eye(3) * 100
 
     v0 = np.zeros_like(x0)
 
-    ctxt = custom_ops.ReversibleContext_f64(
-        stepper,
+
+    impls = []
+    for b in bps:
+        p_impl = b.bound_impl()
+        impls.append(p_impl)
+
+
+    ctxt = custom_ops.Context(
         x0,
         v0,
-        integrator.cas,
-        integrator.cbs,
-        integrator.ccs,
-        integrator.dts,
-        integrator.seed
+        box,
+        intg,
+        impls
     )
 
     combined_pdb_str = StringIO(Chem.MolToPDBBlock(combined_pdb))
@@ -92,30 +135,10 @@ if __name__ == "__main__":
 
     pdb_writer = PDBWriter(combined_pdb_str, out_file)
 
-    # pdb_writer.write_header()
-
-    # frames = ctxt.get_all_coords()
-    # for frame_idx, x in enumerate(frames):
-    #     # if frame_idx % 100 == 0:
-    #     pdb_writer.write(x*10)
-    #     break
-    # pdb_writer.close()
-
-    # assert 0
-
-    ctxt.forward_mode()
-
-    energies = stepper.get_energies()
-
-    print(energies)
-
-    frames = ctxt.get_all_coords()
-
     pdb_writer.write_header()
-    for frame_idx, x in enumerate(frames):
-        if frame_idx % 100 == 0:
-            pdb_writer.write(x*10)
+    for step, lamb in enumerate(np.linspace(1.0, 0.0, 1000)):
+        ctxt.step(lamb)
+        if step % 100 == 0:
+            print("step", step, "nrg", ctxt.get_u_t())
+            pdb_writer.write(ctxt.get_x_t()*10)
     pdb_writer.close()
-
-    plt.plot(energies)
-    plt.show()
