30d883a5ebe6cc3183e0f11c45ef28711a92a36a
Yutong Zhao <proteneer@gmail.com>
2020-11-05 15:46:12 -0500

    Better shape tests

--- timemachine/potentials/shape.py
+++ timemachine/potentials/shape.py
@@ -1,18 +1,43 @@
 import jax.numpy as np
 
-def d2ij(ci, cj):
-    diff = ri - rj
-    return np.dot(diff, axis=-1)
+from timemachine.potentials.jax_utils import distance
+
+def squared_distance(ci, cj):
+    diff = ci - cj
+    return np.sum(diff*diff, axis=-1)
+    # print(diff.shape)
+    # return np.dot(diff, diff)
 
 
-# overlap volume between two sets of points
 def volume(conf_a, params_a, conf_b, params_b):
+    """
+    Given two molecules, compute the second order volume Vij between them. It's
+    important the units in the coordinates conf_x are consistent with alpha
+    coefficients (params_x[:, 0]). This will likely overestimate the true
+    volume as the third and higher order terms are ignored.
 
+    Parameters
+    ----------
+    conf_a: np.array [A, 3]
+        point cloud of coordinates for molecule A
+
+    params_a: np.array [A, 2]
+        parameters where [:, 0] are alphas in Grant's paper, and [:, 1] 
+        are the weights/prefactors outside of the exponential
+
+    conf_b: np.array [B, 3]
+        point cloud of coordinates for molecule B
+
+    params_b: np.array [A, 2]
+        identical to params_a in semantics
+
+    """
     ci = np.expand_dims(conf_a, axis=1) # N, 1, 3
     cj = np.expand_dims(conf_b, axis=0) # 1, N, 3
 
-    dij = distance(ci, cj)
-    d2ij = dij*dij
+    d2ij = squared_distance(ci, cj)
+
+    # print(d2ij[0])
 
     ai = np.expand_dims(params_a[:, 0], axis=1)
     aj = np.expand_dims(params_b[:, 0], axis=0)
@@ -20,35 +45,78 @@ def volume(conf_a, params_a, conf_b, params_b):
     pi = np.expand_dims(params_a[:, 1], axis=1)
     pj = np.expand_dims(params_b[:, 1], axis=0)
 
-    aij = ai*aj
-    kij = np.exp(-(aij*d2ij)/(ai+aj))
-    pij = pi*pj
-    vij = pij*kij*np.power(np.pi/(ai+aj), 3/2)
+    kij = np.exp(-(ai*aj*d2ij)/(ai+aj))
+    vij = pi*pj*kij*np.power(np.pi/(ai+aj), 3/2)
 
     return np.sum(vij)
 
-def overlap(conf, params, a_idxs, b_idxs):
+def normalized_overlap(conf_a, params_a, conf_b, params_b):
     """
-    Compute the 2 body gaussian overlap volume.
+    Compute the normalized overlap volume defined by Grant 1996,
+    equation 20:
+
+    S_AB =  2*int dr rho_A * rho_B
+           ------------------------
+           int dr rho_A^2 + rho_B^2
+
+    Guaranteeing that 0 <= S_AB <= 1. An alternative would be
+    something like the Tanimoto. 
 
     Parameters
     ----------
+    conf_a: np.array [A, 3]
+        point cloud of coordinates for molecule A
 
-    conf: [N, 3]
+    params_a: np.array [A, 2]
+        parameters where [:, 0] are alphas in Grant's paper, and [:, 1] 
+        are the weights/prefactors outside of the exponential
+
+    conf_b: np.array [B, 3]
+        point cloud of coordinates for molecule B
+
+    params_b: np.array [A, 2]
+        identical to params_a in semantics
+
     """
-    conf_a = conf[a_idxs]
-    conf_b = conf[b_idxs]
-    params_a = params[a_idxs]
-    params_b = params[b_idxs]
-
     vij = volume(conf_a, params_a, conf_b, params_b)
     vii = volume(conf_a, params_a, conf_a, params_a)
     vjj = volume(conf_b, params_b, conf_b, params_b)
 
+    print("ref triple", vij, vii, vjj)
+
+    # return vij + vii + vjj
+    # try tanimoto etc. later as well
     return 2*vij/(vii+vjj)
 
-def inverse_overlap(conf, params, box, lamb, a_idxs, b_idxs):
+def harmonic_overlap(conf, params, box, lamb, a_idxs, b_idxs, alphas, weights, k):
+    """
+    Compute the shape potential. The derivative of this function
+    w.r.t. conf generates a non-rigid force.
 
-    V = overlap(conf, params, a_idxs, b_idxs)
-    # much more stable than 1/V variants
-    return 200*(V-1)**2
+    Parameters
+    ----------
+    conf: np.array [N, 3]
+        Conformation of the system
+
+    params: np.array [N, 2]
+        Shape parameters of the system
+
+    a_idxs: np.array [A]
+        Molecule A's indices into the conformation
+
+    b_idxs: np.array [B]
+        Molecule B's indices into the conformation
+
+    """
+
+    conf_a = conf[a_idxs]
+    conf_b = conf[b_idxs]
+
+    params_c = np.stack([alphas, weights], axis=1)
+
+    params_a = params_c[a_idxs]
+    params_b = params_c[b_idxs]
+
+    V = normalized_overlap(conf_a, params_a, conf_b, params_b)
+    # return V
+    return k*(V-1)**2
