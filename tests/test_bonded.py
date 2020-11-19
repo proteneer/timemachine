6f6ec9d5e7b18818baa12ac5af21d27dea8acb61
Josh Fass <jfass@relaytx.com>
2020-11-19 10:22:01 -0500

    Misc. minor adjustments in tests/common, tests/test_bonded, and tests/test_shape (#299)
    * Update common.py
    
    * Reorganization and naming in test_bonded.py
    
    * define relative tolerance at precision in one place
    * add 1-2 line description of each test
    * move some test parameters into function signature
    
    * resolve onp import in tests/test_shape.py
    
    * auto-format tests/test_shape.py
    
    * Type annotations and docstrings in tests/test_shape.py
    
    * Oops, two -> to typo I introduced in docstring
    
    * actually fix onp import issue!
    
    my previous "fix" had broken things
    
    * remove type annotations
    
    * Move kb and b0 into test_centroid_restraint signature
    
    * move relative_tolerance_at_precision definitions back into individual tests
    
    Resolving https://github.com/proteneer/timemachine/pull/299#discussion_r526943766

--- tests/test_bonded.py
+++ tests/test_bonded.py
@@ -1,7 +1,8 @@
 import numpy as np
 import jax
-import jax.numpy as jnp
-from jax.config import config; config.update("jax_enable_x64", True)
+from jax.config import config
+
+config.update("jax_enable_x64", True)
 import functools
 
 from common import GradientTest
@@ -11,21 +12,21 @@ from timemachine.potentials import bonded
 
 class TestBonded(GradientTest):
 
-    def test_centroid_restraint(self):
+    def test_centroid_restraint(self, n_particles=10, n_A=4, n_B=3, kb=5.4, b0=2.3):
+        """Randomly define subsets A and B of a larger collection of particles,
+        generate a centroid restraint between A and B, and then validate the resulting CentroidRestraint force"""
+        box = np.eye(3) * 100
 
-        N = 10
+        # specific to centroid restraint force
+        relative_tolerance_at_precision = {np.float32: 2e-5, np.float64: 1e-9}
 
-        for precision, rtol in [(np.float64, 1e-9), (np.float32, 2e-5)]:
+        for precision, rtol in relative_tolerance_at_precision.items():
+            x_primal = self.get_random_coords(n_particles, 3)
 
-            x_primal = self.get_random_coords(N, 3)
+            gai = np.random.randint(0, n_particles, n_A, dtype=np.int32)
+            gbi = np.random.randint(0, n_particles, n_B, dtype=np.int32)
 
-            gai = np.random.randint(0, N, 4, dtype=np.int32)
-            gbi = np.random.randint(0, N, 3, dtype=np.int32)
-
-            kb = 5.4
-            b0 = 2.3
-
-            masses = np.random.rand(N)
+            masses = np.random.rand(n_particles)
 
             ref_nrg = jax.partial(
                 bonded.centroid_restraint,
@@ -35,8 +36,6 @@ class TestBonded(GradientTest):
                 kb=kb,
                 b0=b0
             )
-
-            box = np.eye(3)*100
 
             # we need to clear the du_dp buffer each time, so we need
             # to instantiate test_nrg inside here
@@ -49,7 +48,7 @@ class TestBonded(GradientTest):
             )
 
             params = np.array([], dtype=np.float64)
-            lamb = 0.3 # doesn't matter
+            lamb = 0.3  # doesn't matter
 
             self.compare_forces(
                 x_primal,
@@ -62,34 +61,33 @@ class TestBonded(GradientTest):
                 precision=precision
             )
 
+    def test_harmonic_bond(self, n_particles=64, n_bonds=35, dim=3):
+        """Randomly connect pairs of particles, then validate the resulting HarmonicBond force"""
+        np.random.seed(125)  # TODO: where should this seed be set?
 
-    def test_harmonic_bond(self):
-        np.random.seed(125)
+        x = self.get_random_coords(n_particles, dim)
 
-        N = 64
-        B = 35
-        D = 3
+        atom_idxs = np.arange(n_particles)
+        params = np.random.rand(n_bonds, 2).astype(np.float64)
 
-        x = self.get_random_coords(N, D)
-
-        atom_idxs = np.arange(N)
-        params = np.random.rand(B, 2).astype(np.float64)
         bond_idxs = []
-        for _ in range(B):
+        for _ in range(n_bonds):
             bond_idxs.append(np.random.choice(atom_idxs, size=2, replace=False))
         bond_idxs = np.array(bond_idxs, dtype=np.int32)
 
         lamb = 0.0
+        box = np.eye(3) * 100
 
-        for precision, rtol in [(np.float32, 4e-5), (np.float64, 1e-9)]:
+        # specific to harmonic bond force
+        relative_tolerance_at_precision = {np.float32: 2e-5, np.float64: 1e-9}
+
+        for precision, rtol in relative_tolerance_at_precision.items():
             test_potential = potentials.HarmonicBond(bond_idxs)
             ref_potential = functools.partial(
                 bonded.harmonic_bond,
                 bond_idxs=bond_idxs
             )
 
-            box = np.eye(3)*100
-
             self.compare_forces(
                 x,
                 params,
@@ -101,30 +99,28 @@ class TestBonded(GradientTest):
                 precision=precision
             )
 
-    def test_harmonic_angle(self):
+    def test_harmonic_angle(self, n_particles=64, n_angles=25, dim=3):
+        """Randomly connect triples of particles, then validate the resulting HarmonicAngle force"""
         np.random.seed(125)
 
-        N = 64
-        A = 25
-        D = 3
+        x = self.get_random_coords(n_particles, dim)
 
-        x = self.get_random_coords(N, D)
-
-        atom_idxs = np.arange(N)
-        params = np.random.rand(A, 2).astype(np.float64)
+        atom_idxs = np.arange(n_particles)
+        params = np.random.rand(n_angles, 2).astype(np.float64)
         angle_idxs = []
-        for _ in range(A):
+        for _ in range(n_angles):
             angle_idxs.append(np.random.choice(atom_idxs, size=3, replace=False))
         angle_idxs = np.array(angle_idxs, dtype=np.int32)
 
         lamb = 0.0
+        box = np.eye(3) * 100
 
-        for precision, rtol in [(np.float64, 1e-9), (np.float32, 2e-5)]:
-            # print(precision, rtol)
+        # specific to harmonic angle force
+        relative_tolerance_at_precision = {np.float32: 2e-5, np.float64: 1e-9}
+
+        for precision, rtol in relative_tolerance_at_precision.items():
             test_potential = potentials.HarmonicAngle(angle_idxs)
             ref_potential = functools.partial(bonded.harmonic_angle, angle_idxs=angle_idxs)
-
-            box = np.eye(3)*100
 
             self.compare_forces(
                 x,
@@ -137,34 +133,31 @@ class TestBonded(GradientTest):
                 precision=precision
             )
 
-
-    def test_periodic_torsion(self):
+    def test_periodic_torsion(self, n_particles=64, n_torsions=25, dim=3):
+        """Randomly connect quadruples of particles, then validate the resulting PeriodicTorsion force"""
         np.random.seed(125)
 
-        N = 64
-        T = 25
-        D = 3
+        x = self.get_random_coords(n_particles, dim)
 
-        x = self.get_random_coords(N, D)
-
-        atom_idxs = np.arange(N)
-        params = np.random.rand(T, 3).astype(np.float64)
+        atom_idxs = np.arange(n_particles)
+        params = np.random.rand(n_torsions, 3).astype(np.float64)
         torsion_idxs = []
-        for _ in range(T):
+        for _ in range(n_torsions):
             torsion_idxs.append(np.random.choice(atom_idxs, size=4, replace=False))
 
         torsion_idxs = np.array(torsion_idxs, dtype=np.int32)
 
         lamb = 0.0
+        box = np.eye(3) * 100
 
-        for precision, rtol in [(np.float32, 2e-5), (np.float64, 1e-9)]:
+        # specific to periodic torsion force
+        relative_tolerance_at_precision = {np.float32: 2e-5, np.float64: 1e-9}
 
+        for precision, rtol in relative_tolerance_at_precision.items():
             test_potential = potentials.PeriodicTorsion(torsion_idxs)
 
             # test the parameter derivatives for correctness.
             ref_potential = functools.partial(bonded.periodic_torsion, torsion_idxs=torsion_idxs)
-
-            box = np.eye(3)*100
 
             self.compare_forces(
                 x,
