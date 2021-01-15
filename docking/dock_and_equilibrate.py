proteneer/timemachine

commit 8bb4da6da45320ac4a50f7a6d1cf5cc1e96788ed

Update note about early termination conditions

docking/dock_and_equilibrate.py
@@ -57,8 +57,8 @@ def dock_and_equilibrate(
 
     Note
     ----
-    If any norm of force per atom exceeds 20000 kJ/(mol*nm), the simulation for that
-    guest will stop and the work will not be calculated.
+    If any norm of force per atom exceeds 20000 kJ/(mol*nm [MAX_NORM_FORCE defined in docking/report.py],
+    the simulation for that guest will stop and the work will not be calculated.
     """
 
     if not os.path.exists(outdir):

docking/pose_dock.py
@@ -62,8 +62,8 @@ def pose_dock(
 
     Note
     ----
-    If any norm of force per atom exceeds 10000 kJ/(mol*nm), the simulation for that
-    guest will stop and the work will not be calculated.
+    If any norm of force per atom exceeds 20000 kJ/(mol*nm) [MAX_NORM_FORCE defined in docking/report.py],
+    the simulation for that guest will stop and the work will not be calculated.
     """
     assert transition_steps <= n_steps
     assert transition_type in ("insertion", "deletion")

commit fcaf39848756939a4d6ec4e6dc5bf8bd7882275c

Fix docstring typo I introduced in https://github.com/proteneer/timemachine/pull/317/commits/8bb4da6da45320ac4a50f7a6d1cf5cc1e96788ed

docking/dock_and_equilibrate.py
@@ -57,7 +57,7 @@ def dock_and_equilibrate(
 
     Note
     ----
-    If any norm of force per atom exceeds 20000 kJ/(mol*nm [MAX_NORM_FORCE defined in docking/report.py],
+    If any norm of force per atom exceeds 20000 kJ/(mol*nm) [MAX_NORM_FORCE defined in docking/report.py],
     the simulation for that guest will stop and the work will not be calculated.
     """
 