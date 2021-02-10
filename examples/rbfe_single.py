fe74cfe6b72b81720b4d39f0365e01f045c8bdb5
Josh Fass <jfass@relaytx.com>
2021-02-09 20:40:11 -0500

    Update rbfe_single.py
    * Add alternative to gradient clipping inspired by https://arxiv.org/abs/1903.08619
    * Flatten parameters and gradients for convenience
    * Add type hints

--- examples/rbfe_single.py
+++ examples/rbfe_single.py
@@ -6,6 +6,7 @@
 import argparse
 import numpy as np
 import jax
+from jax import numpy as jnp
 
 from fe.free_energy import construct_lambda_schedule
 from fe.utils import convert_uIC50_to_kJ_per_mole
@@ -15,11 +16,91 @@ from md import builders
 from testsystems.relative import hif2a_ligand_pair
 
 from ff.handlers.serialize import serialize_handlers
-from ff.handlers import nonbonded
+from ff.handlers.nonbonded import AM1CCCHandler, LennardJonesHandler
 from parallel.client import CUDAPoolClient
 
+from typing import Union, Optional, Iterable, Any, Tuple, Dict
 
-def wrap_method(args, fxn):
+from scipy.optimize import root_scalar
+
+array = Union[np.array, jnp.array]
+Handler = Union[AM1CCCHandler, LennardJonesHandler] # TODO: do these all inherit from a Handler class already?
+
+
+# TODO: move optimizer stuff into timemachine/optimize or so
+def _taylor_first_order(x: array, f_x: float, grad: array) -> callable:
+    """
+
+    Notes:
+        TODO: is it preferable to use jax linearize? https://jax.readthedocs.io/en/latest/jax.html#jax.linearize
+    """
+    def f_prime(y: array) -> float:
+        return f_x + np.dot(grad, y - x)
+
+    return f_prime
+
+
+def _smart_clip(
+        x: array, f_x: float, grad: array,
+        step_size: float=0.1, search_direction: Optional[array]=None,
+        step_lower_bound: float=0.0,
+):
+    """ Motivated by https://arxiv.org/abs/1903.08619 , use knowledge of a lower-bound on f_x
+    to prevent from taking a step too large
+
+    TODO: consider further damping?
+
+    TODO: rather than truncating at absolute global bound on loss,
+        consider truncating at relative bound, like, don't take a step that
+        you predict would decrease the loss by more than
+            X % ?
+            X absolute increment?
+
+            some combination of these?
+    TODO: generalize to use local surrogates other than first-order Taylor expansions
+
+    Note:
+    * search_direction not assumed normalized. for example, it could be the raw gradient
+    """
+
+    # default search direction: SGD
+    if search_direction is None:
+        search_direction = - grad
+
+    # default local surrogate model: linear
+    f_prime = _taylor_first_order(x, f_x, grad)
+
+    # default step: step_size * search_direction
+    x_next = x + step_size * search_direction
+
+    # if this is too optimistic, according to local surrogate f_prime
+    if f_prime(x_next) < step_lower_bound: # TODO: replace f_prime bound with something more configurable
+        x_proposed = x_next
+
+        line_search_fxn = lambda alpha: f_prime(x + alpha * search_direction) - step_lower_bound
+
+        result = root_scalar(line_search_fxn, x0=0, x1=step_size)
+        alpha = result.root
+
+        x_next = x + alpha * search_direction
+
+        message = f"""
+        f_prime(x_proposed) = {f_prime(x_proposed):.5f}
+        using default step size {step_size:.5f}
+        is lower than step_lower_bound = {step_lower_bound:.5f}
+            
+        truncating step size to {alpha:.5f}, 
+        so that the predicted f_prime(x_next) = {f_prime(x_next):.5f}"""
+        print(message)
+
+    ## TODO: replace epsilon fudge with proper np assert almost greater than
+    #eps = 1e-3
+    #assert (f_prime(x_next) >= step_lower_bound - eps)
+
+    return x_next
+
+
+def wrap_method(args: Iterable[Any], fxn: callable):
     # TODO: is there a more functools-y approach to make
     #   a function accept tuple instead of positional arguments?
     return fxn(*args)
@@ -120,31 +201,98 @@ if __name__ == "__main__":
     ordered_params = forcefield.get_ordered_params()
     ordered_handles = forcefield.get_ordered_handles()
 
-    gradient_clip_thresholds = {
-        nonbonded.AM1CCCHandler: 0.05,
-        nonbonded.LennardJonesHandler: np.array([0.001, 0])
-    }
+    handle_types_being_optimized = [AM1CCCHandler, LennardJonesHandler]
 
-    for epoch in range(100):
+    def flatten(params) -> Tuple[array, callable]:
+        """Turn params dict into flat array, with an accompanying unflatten function
+
+        TODO: note that the result is going to be in the order given by ordered_handles (filtered by presence in hrandle_types)
+            rather than in the order they appear in handle_types_being_optimized
+
+        TODO: maybe leave out the reference to handle_types_being optimized altogether
+
+        TODO: does Jax have a pytree-based flatten / unflatten utility?
+        """
+
+
+        theta_list = []
+        _shapes = dict()
+        _handle_types = []
+
+        for param, handle in zip(params, ordered_handles):
+            assert handle.params.shape == param.shape
+            key = type(handle)
+
+            if key in handle_types_being_optimized:
+                theta_list.append(param.flatten())
+                _shapes[key] = param.shape
+                _handle_types.append(key)
+
+        theta = np.hstack(theta_list)
+
+        def unflatten(theta: array) -> Dict[Handler, array]:
+            params = dict()
+            i = 0
+            for key in _handle_types:
+                shape = _shapes[key]
+                num_params = int(np.prod(shape))
+                params[key] = np.array(theta[i : i + num_params]).reshape(shape)
+                i += num_params
+            return params
+
+        return theta, unflatten
+
+
+
+    # in each optimization step, don't step so far that you think you're jumping to
+    #   loss_next = relative_improvement_bound * loss_current
+    relative_improvement_bound = 0.95
+
+    flat_theta_traj = []
+    flat_grad_traj = []
+    loss_traj = []
+
+    for epoch in range(1000):
         epoch_params = serialize_handlers(ordered_handles)
         loss, loss_grad = vg_fn(ordered_params, mol_a, mol_b, core, label_ddG)
 
         print("epoch", epoch, "loss", loss)
 
-        for loss_grad, handle in zip(loss_grad, ordered_handles):
-            assert handle.params.shape == loss_grad.shape
+        # note: unflatten_grad and unflatten_theta have identical definitions for now
+        flat_loss_grad, unflatten_grad = flatten(loss_grad)
+        flat_theta, unflatten_theta = flatten(ordered_params)
 
-            if type(handle) in gradient_clip_thresholds:
-                bounds = gradient_clip_thresholds[type(handle)]
-                loss_grad = np.clip(loss_grad, -bounds, bounds)
-                print("adjust handle", handle, "by", - loss_grad)
-                handle.params -= loss_grad
+        step_lower_bound = loss * relative_improvement_bound
+        next_theta = _smart_clip(flat_theta, loss, flat_loss_grad, step_lower_bound=step_lower_bound)
+        param_updates = unflatten_theta(next_theta)
+
+        # for any parameter handler types being updated, update in place
+        for handle in ordered_handles:
+            handle_type = type(handle)
+            if handle_type in param_updates:
+                handle.params = param_updates[handle_type]
 
                 # useful for debugging to dump out the grads
                 # for smirks, dp in zip(handle.smirks, loss_grad):
                     # if np.any(dp) > 0:
                         # print(smirks, dp)
 
+        # checkpoint results to npz (overwrite
+        flat_theta_traj.append(flat_theta)
+        flat_grad_traj.append(flat_loss_grad)
+        loss_traj.append(loss)
+
+        path_to_npz = 'results_checkpoint.npz'
+        print(f'saving theta, grad, loss trajs to {path_to_npz}')
+        np.savez(
+            path_to_npz,
+            theta_traj=np.array(flat_theta_traj),
+            grad_traj=np.array(flat_grad_traj),
+            loss_traj=np.array(loss_traj)
+        )
+
         # write ff parameters after each epoch
-        with open("checkpoint_" + str(epoch) + ".py", 'w') as fh:
+        path_to_ff_checkpoint = f"checkpoint_{epoch}.py"
+        print(f'saving force field parameter checkpoint to {path_to_ff_checkpoint}')
+        with open(path_to_ff_checkpoint, 'w') as fh:
             fh.write(epoch_params)
