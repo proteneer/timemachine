fefb8771f1d04a4e9a71c1c3145c572584943a02
Josh Fass <jfass@relaytx.com>
2021-02-11 20:02:10 -0500

    f-string typo

--- examples/hif2a/fit_to_multiple_rbfes.py
+++ examples/hif2a/fit_to_multiple_rbfes.py
@@ -276,7 +276,7 @@ if __name__ == "__main__":
                 update_mask = np.any(increment != 0, axis=0)
 
                 # TODO: replace with a function that knows what to report about each handle type
-                print(f'updated {sum(update_mask)} params by between {min(increment)} and {max(increment[])}')
+                print(f'updated {sum(update_mask)} params by between {min(increment)} and {max(increment)}')
 
         # Note: for certain kinds of method-validation tests, these labels could also be synthetic
         t1 = time()
