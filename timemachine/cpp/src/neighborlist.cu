a82e66b62bf6aa3e7af89937a190e74580ac0aa3
Forrest York <forrest.york@gmail.com>
2021-04-26 10:56:52 -0600

    Remove unnecessary shuffle sync
    * Appears to reduce time by about 60 microseconds
      per call.

--- timemachine/cpp/src/neighborlist.cu
+++ timemachine/cpp/src/neighborlist.cu
@@ -87,7 +87,7 @@ void Neighborlist<RealType>::compute_block_bounds_host(
         d_box,
         static_cast<cudaStream_t>(0)
     );
-
+    // Does this need to peek at the last error?
     cudaDeviceSynchronize();
 
     gpuErrchk(cudaMemcpy(h_bb_ctrs, d_block_bounds_ctr_, this->B()*3*sizeof(*d_block_bounds_ctr_), cudaMemcpyDeviceToHost));
@@ -116,9 +116,7 @@ std::vector<std::vector<int> > Neighborlist<RealType>::get_nblist_host(
     );
 
     cudaDeviceSynchronize();
-
     const int B = this->B(); //(N+32-1)/32;
-    const int Y = this->Y(); //(B+32-1)/32;
 
     unsigned long long MAX_TILE_BUFFER = B*B;
     unsigned long long MAX_ATOM_BUFFER = B*B*32;
