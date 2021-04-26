d604200579747051844e9f3ba9fa4bb4ed9c6c90
Josh Fass <jfass@relaytx.com>
2021-04-26 17:07:05 -0400

    fs -> free_energies
    Addressing https://github.com/proteneer/timemachine/pull/399#discussion_r620554550

--- fe/cycles.py
+++ fe/cycles.py
@@ -16,7 +16,7 @@ from typing import Optional
 def construct_mle_layer(n_nodes: int,
                         rbfe_inds: np.array, rbfe_sigmas: Optional[np.array] = None,
                         abfe_inds: Optional[np.array] = None, abfe_sigmas: Optional[np.array] = None) -> callable:
-    """Construct a differentiable function predict_fs(simulated_rbfes, simulated_abfes) -> fs
+    """Construct a differentiable function predict_free_energies(simulated_rbfes, simulated_abfes) -> free_energies
 
     Parameters
     ----------
@@ -26,25 +26,25 @@ def construct_mle_layer(n_nodes: int,
     rbfe_inds : int array, shape (n_rbfes, 2)
     rbfe_sigmas : optional float array, shape (n_rbfes,)
         assume input will be an array of n_rbfes simulated_rbfes, with
-        simulated_rbfes = [fs[b] - fs[a] + Normal(0, rbfe_sigmas[i]) for i, (a, b) in enumerate(rbfe_inds)]
+        simulated_rbfes = [free_energies[b] - free_energies[a] + Normal(0, rbfe_sigmas[i]) for i, (a, b) in enumerate(rbfe_inds)]
 
         if rbfe_sigmas not provided, will be assumed = 1.0 for all comparisons
 
     abfe_inds : optional int array, shape (n_absolute,)
     abfe_sigmas : optional float array, shape (n_absolute,)
         assume input will be an array of n_abfes simulated_abfes, with
-        simulated_abfes = [fs[i] + Normal(0, abfe_sigmas[i]) for i in abfe_inds]
+        simulated_abfes = [free_energies[i] + Normal(0, abfe_sigmas[i]) for i in abfe_inds]
 
-        if abfe_inds not provided, will add one dummy ABFE "measurement" that fs[0] = 0
+        if abfe_inds not provided, will add one dummy ABFE "measurement" that free_energies[0] = 0
         if abfe_sigmas not provided, will be assumed = 1.0 for all ABFEs
 
     TODO: refactor (rbfe_inds, rbfe_sigmas) into an "RBFEInfo" object, (abfe_inds, abfe_sigmas) into an "ABFEInfo" object?
 
     Returns
     -------
-    predict_fs : callable
+    predict_free_energies : callable
 
-        predict_fs(simulated_rbfes, Optional[simulated_abfes]) -> fs
+        predict_free_energies(simulated_rbfes, Optional[simulated_abfes]) -> free_energies
 
         maps (an array containing n_rbfes *relative* calculations, and optionally
               an array containing n_absolute *absolute* calculations)
@@ -59,27 +59,27 @@ def construct_mle_layer(n_nodes: int,
     >>> rbfe_inds = np.array([[0,1], [1,2], [2,0]])
     >>> simulated_rbfes = np.array([-1.0, -1.0, +2.0])
     >>> rbfe_sigmas = np.ones(len(rbfe_inds))
-    >>> predict_fs = construct_mle_layer(n_nodes, rbfe_inds, rbfe_sigmas)
-    >>> fs = predict_fs(simulated_rbfes, np.array([0.0]))
-    >>> reconstructed_diffs = onp.array([fs[j] - fs[i] for (i, j) in rbfe_inds])
+    >>> predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds, rbfe_sigmas)
+    >>> free_energies = predict_free_energies(simulated_rbfes, np.array([0.0]))
+    >>> reconstructed_diffs = onp.array([free_energies[j] - free_energies[i] for (i, j) in rbfe_inds])
     >>> onp.isclose(reconstructed_diffs, simulated_rbfes).all()
     True
-    >>> loss = lambda x : np.sum(predict_fs(x)**2) # random loss defined in terms of cycle-corrected estimates
+    >>> loss = lambda x : np.sum(predict_free_energies(x)**2) # random loss defined in terms of cycle-corrected estimates
     >>> grad(loss)(simulated_rbfes) # can take gradients of this loss w.r.t. simulated_rbfes
     DeviceArray([-2.66666667, -0.66666667,  3.33333333], dtype=float64)
 
     Notes
     -----
     * Here we used an independent Gaussian noise model for the likelihood of simulated_rbfe(i, j) given
-        true underlying value of fs[j] - fs[i].
+        true underlying value of free_energies[j] - free_energies[i].
         Other noise models could be plugged in here (e.g. ones that allow heavy-tailed noise or correlated errors),
         as long as log_likelihood still permits a cvxpy-friendly expression.
-    * Here we used no prior information about plausible values of fs.
+    * Here we used no prior information about plausible values of free_energies.
         Rather than returning a maximum likelihood estimate
-            argmax_{trial_fs} log_likelihood(trial_fs),
+            argmax_{trial_free_energies} log_likelihood(trial_free_energies),
         we could just as well compute a maximum a posteriori estimate
-            argmax_{trial_fs} log_prior(trial_fs) + log_likelihood(trial_fs),
-        where log_prior(trial_fs) could be informed by a cheminformatics model or similar.
+            argmax_{trial_free_energies} log_prior(trial_free_energies) + log_likelihood(trial_free_energies),
+        where log_prior(trial_free_energies) could be informed by a cheminformatics model or similar.
 
     References
     ----------
@@ -117,39 +117,40 @@ def construct_mle_layer(n_nodes: int,
     simulated_abfes = cp.Parameter(n_absolute)
 
     # the optimization variable is a collection of trial absolute free energies
-    trial_fs = cp.Variable(n_nodes)
+    trial_free_energies = cp.Variable(n_nodes)
 
     # express the expected values of rbfe calculations in terms of differences of underlying trial abfe values
-    implied_rbfes = trial_fs[inds_r] - trial_fs[inds_l]
-    implied_abfes = trial_fs[abfe_inds]
+    implied_rbfes = trial_free_energies[inds_r] - trial_free_energies[inds_l]
+    implied_abfes = trial_free_energies[abfe_inds]
 
-    # gaussian log likelihood of simulated_rbfes, compared with the relative free energies implied by trial_fs
+    # gaussian log likelihood of simulated_rbfes, compared with the relative free energies implied by trial_free_energies
     rbfe_residuals = implied_rbfes - simulated_rbfes
     log_likelihood_rbfes = cp.sum(- (rbfe_residuals / rbfe_sigmas) ** 2 - np.log(rbfe_sigmas * np.sqrt(2 * np.pi)))
 
-    # gaussian log likelihood of simulated_abfes, compared with the absolute free energies implied by trial_fs
+    # gaussian log likelihood of simulated_abfes, compared with the absolute free energies implied by trial_free_energies
     abfe_residuals = implied_abfes - simulated_abfes
     log_likelihood_abfes = cp.sum(- (abfe_residuals / abfe_sigmas) ** 2 - np.log(abfe_sigmas * np.sqrt(2 * np.pi)))
 
     # likelihood of rbfes and abfes jointly
     log_likelihood = log_likelihood_rbfes + log_likelihood_abfes
 
-    # predicted fs are obtained by varying trial_fs to maximize log_likelihood of simulated_rbfes and simulated_abfes
+    # predicted free_energies are obtained by varying trial_free_energies to maximize log_likelihood of
+    #   simulated_rbfes and simulated_abfes
     objective = cp.Minimize(- log_likelihood)
     problem = cp.Problem(objective)
     assert problem.is_dpp()
 
-    # return value of the cvxpylayer_fxn callable is a 1-tuple containing a jax array, (fs,)
-    cvxpylayer_fxn = CvxpyLayer(problem, parameters=problem.parameters(), variables=[trial_fs])
+    # return value of the cvxpylayer_fxn callable is a 1-tuple containing a jax array, (free_energies,)
+    cvxpylayer_fxn = CvxpyLayer(problem, parameters=problem.parameters(), variables=[trial_free_energies])
 
     # for convenience, return the jax array rather than a 1-tuple, and make simulated_abfes argument optional
     #   if no ABFE inds were specified
     if no_abfe:
-        def predict_fs(simulated_rbfes, simulated_abfes=None):
+        def predict_free_energies(simulated_rbfes, simulated_abfes=None):
             return cvxpylayer_fxn(simulated_rbfes, np.zeros(1))[0]
     else:
-        def predict_fs(simulated_rbfes, simulated_abfes):
+        def predict_free_energies(simulated_rbfes, simulated_abfes):
             return cvxpylayer_fxn(simulated_rbfes, simulated_abfes)[0]
 
-    # return callable function, predict_fs(simulated_rbfes, Optional[simulated_abfes]) -> fs
-    return predict_fs
+    # return callable function, predict_free_energies(simulated_rbfes, Optional[simulated_abfes]) -> free_energies
+    return predict_free_energies
--- tests/test_cycle_closure.py
+++ tests/test_cycle_closure.py
@@ -12,39 +12,40 @@ onp.random.seed(2021)
 
 
 def test_cycle_closure_consistency_triangle():
-    """Generate a 3-cycle with random true_fs on the nodes, and assert we can recover predicted fs == true_fs"""
+    """Generate a 3-cycle with random true_free_energies on the nodes,
+    and assert we can recover predicted free_energies == true_free_energies"""
 
     n_nodes = 3
-    true_fs = onp.random.randn(n_nodes)
-    true_fs -= true_fs[0]
+    true_free_energies = onp.random.randn(n_nodes)
+    true_free_energies -= true_free_energies[0]
 
     rbfe_inds = np.array([[0, 1], [1, 2], [2, 0]])
     inds_l, inds_r = rbfe_inds.T
-    simulated_rbfes = true_fs[inds_r] - true_fs[inds_l]
+    simulated_rbfes = true_free_energies[inds_r] - true_free_energies[inds_l]
 
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds)
 
-    fs = predict_fs(simulated_rbfes)
+    free_energies = predict_free_energies(simulated_rbfes)
 
-    assert (np.isclose(true_fs, fs).all())
+    assert (np.isclose(true_free_energies, free_energies).all())
 
 
 def test_cycle_closure_consistency_dense(n_nodes=100):
-    """Generate a large dense comparison network with random true_fs on the nodes, and assert we can recover
-    predicted fs == true_fs"""
+    """Generate a large dense comparison network with random true_free_energies on the nodes, and assert we can recover
+    predicted free_energies == true_free_energies"""
 
-    true_fs = onp.random.randn(n_nodes)
-    true_fs -= true_fs[0]
+    true_free_energies = onp.random.randn(n_nodes)
+    true_free_energies -= true_free_energies[0]
 
     inds_l, inds_r = np.triu_indices(n_nodes, k=1)
     rbfe_inds = np.stack([inds_l, inds_r]).T
 
-    simulated_rbfes = true_fs[inds_r] - true_fs[inds_l]
+    simulated_rbfes = true_free_energies[inds_r] - true_free_energies[inds_l]
 
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds)
-    fs = predict_fs(simulated_rbfes)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds)
+    free_energies = predict_free_energies(simulated_rbfes)
 
-    assert (np.isclose(true_fs, fs).all())
+    assert (np.isclose(true_free_energies, free_energies).all())
 
 
 def test_deadlock_triangle(verbose=True):
@@ -57,16 +58,16 @@ def test_deadlock_triangle(verbose=True):
     rbfe_inds = np.array([[0, 1], [1, 2], [2, 0]])
     inds_l, inds_r = rbfe_inds.T
 
-    true_fs = np.array([0, 2, -1], dtype=np.float64)
-    true_rbfes = true_fs[inds_r] - true_fs[inds_l]
+    true_free_energies = np.array([0, 2, -1], dtype=np.float64)
+    true_rbfes = true_free_energies[inds_r] - true_free_energies[inds_l]
     simulated_rbfes = np.array([4, -1, 5], dtype=np.float64)
 
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds)
 
     def apply_cycle_correction_to_rbfes(simulated_rbfes):
-        """estimate mle_fs, then return [mle_fs[j] - mle_fs[i] for (i,j) in rbfe_inds]"""
-        fs = predict_fs(simulated_rbfes)
-        cycle_corrected_rbfes = fs[inds_r] - fs[inds_l]
+        """estimate mle_free_energies, then return [mle_free_energies[j] - mle_free_energies[i] for (i,j) in rbfe_inds]"""
+        free_energies = predict_free_energies(simulated_rbfes)
+        cycle_corrected_rbfes = free_energies[inds_r] - free_energies[inds_l]
 
         return cycle_corrected_rbfes
 
@@ -95,24 +96,24 @@ def test_deadlock_triangle(verbose=True):
 
 def test_optimization_with_cycle_closure(n_nodes=10, verbose=True):
     """Optimize a collection of dense pairwise simulated_rbfes values so that the cycle-closure-corrected estimates they
-    imply will equal some known realizable set of true_fs."""
+    imply will equal some known realizable set of true_free_energies."""
 
-    true_fs = onp.random.randn(n_nodes)
-    true_fs -= true_fs[0]
+    true_free_energies = onp.random.randn(n_nodes)
+    true_free_energies -= true_free_energies[0]
 
     inds_l, inds_r = np.triu_indices(n_nodes, k=1)
     rbfe_inds = np.stack([inds_l, inds_r]).T
     n_rbfes = len(rbfe_inds)
 
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds)
 
     def L(simulated_rbfes):
-        """sum((fs - true_fs)^2)"""
+        """sum((free_energies - true_free_energies)^2)"""
         assert (len(simulated_rbfes) == n_rbfes)
-        fs = predict_fs(simulated_rbfes)
-        assert (len(fs) == n_nodes)
+        free_energies = predict_free_energies(simulated_rbfes)
+        assert (len(free_energies) == n_nodes)
 
-        return np.sum((fs - true_fs) ** 2)
+        return np.sum((free_energies - true_free_energies) ** 2)
 
     def fun(x):
         l, g = value_and_grad(L)(x)
@@ -120,12 +121,12 @@ def test_optimization_with_cycle_closure(n_nodes=10, verbose=True):
 
     x0 = onp.random.randn(n_rbfes)
     if verbose:
-        print(f'sum((fs - true_fs)^2) before optimization: {L(x0):.3f}')
+        print(f'sum((free_energies - true_free_energies)^2) before optimization: {L(x0):.3f}')
     assert L(x0) > 1
 
     result = minimize(fun, x0=x0, jac=True, tol=0.0)
     if verbose:
-        print(f'sum((fs - true_fs)^2) after optimization: {L(result.x):.20f}')
+        print(f'sum((free_energies - true_free_energies)^2) after optimization: {L(result.x):.20f}')
 
     assert result.fun < 1e-16
 
@@ -138,23 +139,23 @@ def test_grad_cycle_closure(n_nodes=5, tol=1e-3, verbose=True):
     Also check that grad(loss_fxn)(the_right_answer) = zeros.
     """
 
-    true_fs = np.array(onp.random.randn(n_nodes))
-    true_fs -= true_fs[0]
+    true_free_energies = np.array(onp.random.randn(n_nodes))
+    true_free_energies -= true_free_energies[0]
 
     inds_l, inds_r = np.triu_indices(n_nodes, k=1)
     rbfe_inds = np.stack([inds_l, inds_r]).T
     n_rbfes = len(rbfe_inds)
 
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds)
 
     def loss_fxn(simulated_rbfes):
-        """sum((fs - true_fs)^2)"""
+        """sum((free_energies - true_free_energies)^2)"""
         assert (len(simulated_rbfes) == n_rbfes)
-        fs = predict_fs(simulated_rbfes)
-        fs -= fs[0]
-        assert (len(fs) == n_nodes)
+        free_energies = predict_free_energies(simulated_rbfes)
+        free_energies -= free_energies[0]
+        assert (len(free_energies) == n_nodes)
 
-        return np.sum((fs - true_fs) ** 2)
+        return np.sum((free_energies - true_free_energies) ** 2)
 
     f = lambda x: float(loss_fxn(x))
     g = lambda x: onp.array(grad(loss_fxn)(x))
@@ -169,34 +170,34 @@ def test_grad_cycle_closure(n_nodes=5, tol=1e-3, verbose=True):
         assert c < tol
 
     # check on exact rbfes
-    exact_rbfes = true_fs[inds_r] - true_fs[inds_l]
+    exact_rbfes = true_free_energies[inds_r] - true_free_energies[inds_l]
     assert np.isclose(f(exact_rbfes), 0)
     assert np.isclose(g(exact_rbfes), 0).all()
 
 
 def test_absolute(n_nodes=10, n_absolute=5, n_rbfes=20):
-    """Given a collection of relative and absolute measurements derived from a set of random ground-truth fs,
-    assert that we can exactly reconstruct these fs, including their absolute offset."""
+    """Given a collection of relative and absolute measurements derived from a set of random ground-truth free_energies,
+    assert that we can exactly reconstruct these free_energies, including their absolute offset."""
 
-    # random ground-truth, with true_fs[0] not necessarily = 0
-    true_fs = np.array(onp.random.randn(n_nodes))
+    # random ground-truth, with true_free_energies[0] not necessarily = 0
+    true_free_energies = np.array(onp.random.randn(n_nodes))
 
     # absolute measurements for subset of nodes
     # TODO: randomize abfe_inds by shuffling?
     abfe_inds = onp.arange(n_nodes)[:n_absolute]
-    exact_partial_abfes = true_fs[abfe_inds]
+    exact_partial_abfes = true_free_energies[abfe_inds]
 
     # relative measurements for subset of pairs
     # TODO: randomize inds_l, inds_r, rbfe_inds by shuffling?
     all_inds_l, all_inds_r = np.triu_indices(n_nodes, k=1)
     inds_l, inds_r = all_inds_l[:n_rbfes], all_inds_r[:n_rbfes]
     rbfe_inds = np.stack([inds_l, inds_r]).T[:n_rbfes]
-    exact_partial_rbfes = true_fs[inds_r] - true_fs[inds_l]
+    exact_partial_rbfes = true_free_energies[inds_r] - true_free_energies[inds_l]
 
     # all rbfes and abfes get equal weight
-    predict_fs = construct_mle_layer(n_nodes, rbfe_inds=rbfe_inds, abfe_inds=abfe_inds)
+    predict_free_energies = construct_mle_layer(n_nodes, rbfe_inds=rbfe_inds, abfe_inds=abfe_inds)
 
-    # reconstruct fs from these partial views -- should be able to exactly recover true_fs
-    reconstructed_fs = predict_fs(exact_partial_rbfes, exact_partial_abfes)
+    # reconstruct free_energies from these partial views -- should be able to exactly recover true_free_energies
+    reconstructed_free_energies = predict_free_energies(exact_partial_rbfes, exact_partial_abfes)
 
-    assert np.isclose(reconstructed_fs, true_fs).all()
+    assert np.isclose(reconstructed_free_energies, true_free_energies).all()
