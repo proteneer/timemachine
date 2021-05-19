b0bc00963d844eec5f93e0e2859d93b478374355
Josh Fass <jfass@relaytx.com>
2021-05-19 14:45:07 -0700

    Non-unitted

--- tests/test_ensembles.py
+++ tests/test_ensembles.py
@@ -22,11 +22,11 @@ def _compute_reduced_potential(potential_energy, temperature, volume, pressure):
 def test_nvt():
     npt = NVTEnsemble(potential_energy=None, temperature=300 * kelvin)
 
-    U = -100 * kilojoule_per_mole
+    U = (-100 * kilojoule_per_mole).value_in_unit(ENERGY_UNIT)
     u_0 = npt.reduce(U)
 
     # check that reduced potential increases with increasing U
-    assert npt.reduce(U + 10 * kilojoule_per_mole) > u_0
+    assert npt.reduce(U + 10) > u_0
 
     # check that we agree with reference computation on some specific instances...
     n_trials = 100
@@ -47,13 +47,13 @@ def test_nvt():
 def test_npt():
     npt = NPTEnsemble(potential_energy=None, temperature=300 * kelvin, pressure=1 * atmosphere)
 
-    U = -100 * kilojoule_per_mole
-    volume = 4 * nanometer ** 3
+    U = (-100 * kilojoule_per_mole).value_in_unit(ENERGY_UNIT)
+    volume = (4 * nanometer ** 3).value_in_unit(DISTANCE_UNIT**3)
     u_0 = npt.reduce(U, volume)
 
     # check that reduced potential increases with increasing U or volume
-    assert npt.reduce(U + 10 * kilojoule_per_mole, volume) > u_0
-    assert npt.reduce(U, volume + 1 * nanometer ** 3) > u_0
+    assert npt.reduce(U + 10, volume) > u_0
+    assert npt.reduce(U, volume + 1) > u_0
 
     # check that we agree with reference computation on some specific instances...
     n_trials = 100
