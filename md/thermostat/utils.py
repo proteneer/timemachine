f62ee956e8617f05af6225975357076216aed76c
Josh Fass <jfass@relaytx.com>
2021-05-19 13:31:30 -0700

    Better unit imports

--- md/thermostat/utils.py
+++ md/thermostat/utils.py
@@ -1,9 +1,9 @@
 import numpy as np
 from timemachine.constants import BOLTZ
-from simtk import unit
+from simtk.unit import Quantity, kelvin
 
 
-def sample_velocities(masses: unit.Quantity, temperature: unit.Quantity) -> np.array:
+def sample_velocities(masses: Quantity, temperature: Quantity) -> np.array:
     """Sample Maxwell-Boltzmann velocities ~ N(0, sqrt(kB T / m)"""
     n_particles = len(masses)
     spatial_dim = 3
@@ -11,7 +11,7 @@ def sample_velocities(masses: unit.Quantity, temperature: unit.Quantity) -> np.a
     v_unscaled = np.random.randn(n_particles, spatial_dim)
 
     # intended to be consistent with timemachine.integrator:langevin_coefficients
-    sigma = np.sqrt(BOLTZ * temperature.value_in_unit(unit.kelvin)) * np.sqrt(1 / masses)
+    sigma = np.sqrt(BOLTZ * temperature.value_in_unit(kelvin)) * np.sqrt(1 / masses)
     v_scaled = v_unscaled * np.expand_dims(sigma, axis=1)
 
     assert v_scaled.shape == (n_particles, spatial_dim)
