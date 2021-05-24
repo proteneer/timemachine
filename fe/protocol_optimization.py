939c097082c232317f207dd0955f3495d07b845f
Josh Fass <jfass@relaytx.com>
2021-05-24 16:00:44 -0700

    Update protocol_optimization.py

--- fe/protocol_optimization.py
+++ fe/protocol_optimization.py
@@ -16,7 +16,7 @@ def basis_dot(lam: float, params: np.array) -> np.array:
 
 # normalizing protocols to start and end at preset locations
 
-def normalize_01(f_0, f_1, f_lam):
+def normalize(f_0, f_1, f_lam):
     """given evaluations of a function f_0 = f(0), f_1 = f(1), f_lam = f(lam)
     return an evaluation of a new function g(lam) = (f(lam) - f(0)) / (f(1) - f(0))
 
@@ -28,20 +28,8 @@ def normalize_01(f_0, f_1, f_lam):
     return (f_lam - f_0) / (f_1 - f_0)
 
 
-def normalize_00(f_0, f_1, f_lam, lam):
-    """start at zeros, end at zeros, so this can be layered on top of something else"""
-    return lam * (f_lam - f_1) + (1 - lam) * (f_lam - f_0)
-
-
 def parameterized_protocol(lam: float, params: np.array) -> np.array:
     """start at zeros, end at ones"""
 
     f = partial(basis_dot, params=params)
-    return normalize_01(f(0), f(1), f(lam))
-
-
-def parameterized_perturbation(lam: float, params: np.array) -> np.array:
-    """start at zeros, end at zeros, so this can be layered on top of something else"""
-
-    f = partial(basis_dot, params=params)
-    return normalize_00(f(0), f(1), f(lam), lam)
+    return normalize(f(0), f(1), f(lam))
