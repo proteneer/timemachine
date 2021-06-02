a63b7af6facf523f79c04db0e22964c13ec3ff9d
Josh Fass <jfass@relaytx.com>
2021-06-01 17:20:30 -0700

    Remove some legends

--- scripts/noneq_adaptation/plot.py
+++ scripts/noneq_adaptation/plot.py
@@ -7,7 +7,7 @@ from deploy import interpolate_lambda_schedule
 hist_kwargs = dict(density=True, alpha=0.5)
 
 
-# TODO: reduce duplication of titles and legends
+# TODO: reduce duplication of titles and axis labels
 
 def plot_lambda_schedules(n_md_steps, optimized_lam_traj):
     x = np.arange(n_md_steps)
@@ -21,7 +21,7 @@ def plot_lambda_schedules(n_md_steps, optimized_lam_traj):
     plt.plot(x, default, label='manual')
     plt.plot(x, optimized, label='optimized')
 
-    plt.legend(title='protocol')
+    # plt.legend(title='protocol')
 
 
 def plot_work_accumulation_stddev(du_dl_trajs_default, du_dl_trajs_optimized, optimized_lam_traj):
@@ -38,7 +38,7 @@ def plot_work_accumulation_stddev(du_dl_trajs_default, du_dl_trajs_optimized, op
     plt.plot(x[1:], du_dl_trajs_default.std(0)[1:] * np.diff(default), label='manual')
     plt.plot(x[1:], du_dl_trajs_optimized.std(0)[1:] * np.diff(optimized), label='optimized')
 
-    plt.legend(title='protocol')
+    # plt.legend(title='protocol')
 
 
 def plot_work_histograms(works_default, works_optimized):
@@ -51,7 +51,7 @@ def plot_work_histograms(works_default, works_optimized):
     plt.hist(works_default, label='manual', **hist_kwargs)
     plt.hist(works_optimized, label='optimized', **hist_kwargs)
 
-    plt.legend(title='protocol')
+    # plt.legend(title='protocol')
 
 
 if __name__ == '__main__':
@@ -74,6 +74,8 @@ if __name__ == '__main__':
         n_md_steps = results['total_md_step_range'][i]
         ax = plt.subplot(3, n_conditions, i + 1, sharey=ax)
         plot_lambda_schedules(n_md_steps, optimized_lam_trajs[str(i)])
+        if i == 0:
+            plt.legend(title='protocol')
 
     # row 2: plot work accumulation over course of protocol...
     ax = None
@@ -87,6 +89,8 @@ if __name__ == '__main__':
             du_dl_trajs_optimized[str(n_md_steps)],
             optimized_lam_trajs[str(i)]
         )
+        if i == 0:
+            plt.legend(title='protocol')
 
     # row 3: plot final work histograms
     the_big_zipper = zip(results['total_md_step_range'], results['works_default'], results['works_optimized'])
@@ -95,6 +99,8 @@ if __name__ == '__main__':
         plot_ind = (2 * n_conditions) + i + 1
         ax = plt.subplot(3, n_conditions, plot_ind, sharex=ax, sharey=ax)
         plot_work_histograms(w_default, w_optimized)
+        if i == 0:
+            plt.legend(title='protocol')
 
     plt.tight_layout()
 
