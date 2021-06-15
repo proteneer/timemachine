767c295cb45efd0e3ca01cc35d94ad67fede7914
Josh Fass <josh.fass@choderalab.org>
2021-06-14 21:13:13 -0700

    Update test_jax_utils.py

--- tests/test_jax_utils.py
+++ tests/test_jax_utils.py
@@ -8,12 +8,26 @@ onp.random.seed(2021)
 from timemachine.potentials.jax_utils import delta_r
 
 
-def test_jitted_delta_r_symmetric():
-    """assert jit(delta_r)(ri, rj, box) == - jit(delta_r)(rj, ri, box)"""
-    ri, rj = onp.random.randn(2, 1000, 3)
-    box = np.eye(3)
+def test_delta_r():
+    """assert that
+    * delta_r(ri, rj, box) == - delta_r(rj, ri, box)
+    * and that delta_r agrees with jit(delta_r)
+    on a few random inputs of varying size
+    """
 
-    dr_ij = jit(delta_r)(ri, rj, box)
-    dr_ji = jit(delta_r)(rj, ri, box)
+    for _ in range(5):
+        n_atoms = onp.random.randint(50, 1000)
+        dim = onp.random.randint(3, 5)
+        ri, rj = onp.random.randn(2,  n_atoms, dim)
+        box = np.eye(dim)
 
-    onp.testing.assert_allclose(dr_ij, -dr_ji)
+        dr_ij_1 = delta_r(ri, rj, box)
+        dr_ji_1 = delta_r(rj, ri, box)
+
+        onp.testing.assert_allclose(dr_ij_1, -dr_ji_1)
+
+        dr_ij_2 = jit(delta_r)(ri, rj, box)
+        dr_ji_2 = jit(delta_r)(rj, ri, box)
+
+        onp.testing.assert_allclose(dr_ij_1, dr_ij_2)
+        onp.testing.assert_allclose(dr_ji_1, dr_ji_2)
