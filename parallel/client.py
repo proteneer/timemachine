f7add2ba6ea8bc4170bd56ee81d3573a4ef1431f
Yutong Zhao <proteneer@gmail.com>
2021-07-28 11:50:37 -0400

    Increase max size to 1024

--- parallel/client.py
+++ parallel/client.py
@@ -186,8 +186,8 @@ class GRPCClient(AbstractClient):
         self.stubs = []
         if options is None:
             options = [
-                ('grpc.max_send_message_length', 500 * 1024 * 1024),
-                ('grpc.max_receive_message_length', 500 * 1024 * 1024)
+                ('grpc.max_send_message_length', 1024 * 1024 * 1024),
+                ('grpc.max_receive_message_length', 1024 * 1024 * 1024)
             ]
         for host in self.hosts:
             channel = grpc.insecure_channel(
