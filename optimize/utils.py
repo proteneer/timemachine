9d258e34c667640f7afc3fed38d52aca24c0a6bf
Josh Fass <jfass@relaytx.com>
2021-08-02 16:08:52 -0400

    Generalize to handle float-valued leaves
    Addressing https://github.com/proteneer/timemachine/pull/474#discussion_r681187907

--- optimize/utils.py
+++ optimize/utils.py
@@ -3,6 +3,37 @@ import numpy as onp
 from typing import Tuple, Callable
 
 
+def get_shape(x):
+    """safe to call on floats and arrays
+    get_shape(1) -> None, get_shape(ones(2)) -> (2,)"""
+
+    try:
+        return x.shape
+    except AttributeError:
+        try:
+            _ = float(x)
+            return None
+        except:
+            raise ValueError("x doesn't appear to be an array or a float")
+
+
+def reshape(x, shape):
+    """assume x is scalar if shape is None"""
+    if shape is None:
+        return float(x)
+    else:
+        return x.reshape(shape)
+
+
+def num_elements(shape):
+    """floats (indicated by None) have 1 element"""
+    if shape is None:
+        return 1
+    else:
+        # note: length-zero arrays are fine in this branch: onp.prod(()) == 1.0
+        return int(onp.prod(shape))
+
+
 def flatten_and_unflatten(input_tree) -> Tuple[Callable, Callable]:
     """Make a pair of functions flatten(tree) -> x, unflatten(x) -> tree
 
@@ -19,11 +50,12 @@ def flatten_and_unflatten(input_tree) -> Tuple[Callable, Callable]:
     """
 
     tree_structure = tree_util.tree_structure(input_tree)
-    leaf_shapes = [leaf.shape for leaf in tree_util.tree_leaves(input_tree)]
+
+    leaf_shapes = [get_shape(leaf) for leaf in tree_util.tree_leaves(input_tree)]
 
     def flatten(tree):
         leaves = tree_util.tree_leaves(tree)
-        flattened_leaves = [leaf.flatten() for leaf in leaves]
+        flattened_leaves = [reshape(leaf, num_elements(get_shape(leaf))) for leaf in leaves]
         x = np.hstack(flattened_leaves)
         assert len(x.shape) == 1
         return x
@@ -32,8 +64,7 @@ def flatten_and_unflatten(input_tree) -> Tuple[Callable, Callable]:
         leaves = []
         i = 0
         for shape in leaf_shapes:
-            num_elements = int(onp.prod(shape))
-            leaves.append((x[i:i + num_elements].reshape(shape)))
+            leaves.append(reshape(x[i:i + num_elements(shape)], shape))
             i += num_elements
         tree = tree_util.tree_unflatten(tree_structure, leaves)
         return tree
--- tests/test_optimize.py
+++ tests/test_optimize.py
@@ -44,8 +44,8 @@ def test_flatten_and_unflatten_dict():
                 onp.random.randn(80, 4),
             ]
         ),
-        Bias=onp.array([1.2]),
-        Multiplier=onp.array([2.5]),
+        Bias=1.2,
+        Multiplier=onp.array(2.5),
     )
 
     check_flatten_and_unflatten_roundtrip(example_pytree)
