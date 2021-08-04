56e363d47c04b45abf4e0a143aea446757af465c
Yutong Zhao <proteneer@gmail.com>
2021-08-04 11:30:35 -0400

    Fast U computation (#475)
    * Fast U computation
    
    * Update docstrings
    
    * Add tests for coords
    
    * Update timemachine/cpp/src/wrap_kernels.cpp
    
    Co-authored-by: Josh Fass <jfass@relaytx.com>
    
    * Use step as opposed to internal step_
    
    Co-authored-by: Josh Fass <jfass@relaytx.com>

--- timemachine/cpp/src/context.cu
+++ timemachine/cpp/src/context.cu
@@ -29,6 +29,7 @@ Context::Context(
     d_box_t_ = gpuErrchkCudaMallocAndCopy(box_0, 3*3);
     gpuErrchk(cudaMalloc(&d_du_dx_t_, N*3*sizeof(*d_du_dx_t_)));
     gpuErrchk(cudaMalloc(&d_du_dl_buffer_, N*sizeof(*d_du_dl_buffer_)));
+    gpuErrchk(cudaMalloc(&d_u_buffer_, N*sizeof(*d_u_buffer_)));
 
     unsigned long long *d_in_tmp = nullptr; // dummy
     unsigned long long *d_out_tmp = nullptr; // dummy
@@ -53,6 +54,7 @@ Context::~Context() {
     gpuErrchk(cudaFree(d_box_t_));
     gpuErrchk(cudaFree(d_du_dx_t_));
     gpuErrchk(cudaFree(d_du_dl_buffer_));
+    gpuErrchk(cudaFree(d_u_buffer_));
     gpuErrchk(cudaFree(d_sum_storage_));
 
     // for(int i=0; i < streams_.size(); i++) {
@@ -135,6 +137,159 @@ std::array<std::vector<double>, 3> Context::multiple_steps(
     } catch(...) {
         gpuErrchk(cudaFree(d_du_dl_buffer));
         gpuErrchk(cudaFree(d_box_buffer));
+        throw;
+    }
+
+}
+
+std::array<std::vector<double>, 3> Context::multiple_steps_U(
+    const double lambda, // which lambda window we run the integrator over
+    const int n_steps,
+    const std::vector<double> &lambda_windows, // which lambda windows we wish to evaluate U at
+    int store_u_interval,
+    int store_x_interval) {
+
+    unsigned long long *d_u_traj = nullptr;
+    double *d_box_traj = nullptr;
+
+    // try catch block is to deal with leaks in d_u_buffer
+    if(store_u_interval <= 0) {
+        throw std::runtime_error("store_u_interval <= 0");
+    }
+
+    if(store_x_interval <= 0) {
+        throw std::runtime_error("store_x_interval <= 0");
+    }
+
+    int n_windows = lambda_windows.size();
+    int u_traj_size = ((n_steps + store_u_interval - 1) / store_u_interval)*n_windows;
+    int x_traj_size = (n_steps + store_x_interval - 1) / store_x_interval;
+    int box_traj_size = x_traj_size*3*3;
+
+    std::vector<double> h_x_traj(x_traj_size*N_*3);
+
+    try {
+        gpuErrchk(cudaMalloc(&d_box_traj, box_traj_size*sizeof(*d_box_traj)));
+        gpuErrchk(cudaMalloc(&d_u_traj, u_traj_size*sizeof(*d_u_traj)));
+        gpuErrchk(cudaMemset(d_u_traj, 0, u_traj_size*sizeof(*d_u_traj)));
+
+        for(int step=0; step < n_steps; step++) {
+
+            if(step % store_x_interval == 0) {
+                gpuErrchk(cudaMemcpy(
+                    &h_x_traj[0] + (step / store_x_interval)*N_*3,
+                    d_x_t_,
+                    N_*3*sizeof(double),
+                    cudaMemcpyDeviceToHost)
+                );
+                gpuErrchk(cudaMemcpy(&d_box_traj[0] + (step / store_x_interval)*3*3, d_box_t_, 3*3*sizeof(*d_box_traj), cudaMemcpyDeviceToDevice));
+            }
+
+            cudaStream_t stream = static_cast<cudaStream_t>(0);
+
+            for(int i=0; i < observables_.size(); i++) {
+                observables_[i]->observe(
+                    step,
+                    N_,
+                    d_x_t_,
+                    d_box_t_,
+                    lambda
+                );
+            }
+
+            gpuErrchk(cudaMemsetAsync(d_du_dx_t_, 0, N_*3*sizeof(*d_du_dx_t_), stream));
+
+            // first pass generate the forces
+            for(int i=0; i < bps_.size(); i++) {
+                bps_[i]->execute_device(
+                    N_,
+                    d_x_t_,
+                    d_box_t_,
+                    lambda,
+                    d_du_dx_t_, // we only need the forces
+                    nullptr,
+                    nullptr,
+                    nullptr,
+                    stream
+                );
+            }
+
+            // we need to compute aggregate energies on this step
+            if(step % store_u_interval == 0) {
+                unsigned long long *u_ptr = d_u_traj + (step / store_u_interval)*n_windows;
+                for(int w=0; w < n_windows; w++) {
+                    // reset buffers on each pass.
+                    gpuErrchk(cudaMemsetAsync(d_u_buffer_, 0, N_*sizeof(*d_u_buffer_), stream));
+                    for(int i=0; i < bps_.size(); i++) {
+                        bps_[i]->execute_device(
+                            N_,
+                            d_x_t_,
+                            d_box_t_,
+                            lambda_windows[w],
+                            nullptr,
+                            nullptr,
+                            nullptr,
+                            d_u_buffer_,
+                            stream
+                        );
+                    }
+                    cub::DeviceReduce::Sum(
+                        d_sum_storage_,
+                        d_sum_storage_bytes_,
+                        d_u_buffer_,
+                        u_ptr+w,
+                        N_,
+                        stream
+                    );
+                    gpuErrchk(cudaPeekAtLastError());
+                }
+
+            }
+
+            intg_->step_fwd(
+                d_x_t_,
+                d_v_t_,
+                d_du_dx_t_,
+                d_box_t_,
+                stream
+            );
+
+            if(barostat_) {
+                // May modify coords, du_dx and box size
+                barostat_->inplace_move(
+                    d_x_t_,
+                    d_box_t_,
+                    lambda,
+                    stream
+                );
+            }
+
+        }
+
+        cudaDeviceSynchronize();
+
+        std::vector<unsigned long long> h_u_traj_ull(u_traj_size);
+        gpuErrchk(cudaMemcpy(
+            &h_u_traj_ull[0],
+            d_u_traj,
+            u_traj_size*sizeof(*d_u_traj),
+            cudaMemcpyDeviceToHost)
+        );
+
+        std::vector<double> h_u_traj_double(u_traj_size);
+        for(int i=0; i < h_u_traj_ull.size(); i++) {
+            h_u_traj_double[i] = FIXED_TO_FLOAT<double>(h_u_traj_ull[i]);
+        }
+        std::vector<double> h_box_traj(box_traj_size);
+        gpuErrchk(cudaMemcpy(&h_box_traj[0], d_box_traj, box_traj_size*sizeof(*d_box_traj), cudaMemcpyDeviceToHost));
+
+        gpuErrchk(cudaFree(d_u_traj));
+        gpuErrchk(cudaFree(d_box_traj));
+        return std::array<std::vector<double>, 3>({h_u_traj_double, h_x_traj, h_box_traj});
+
+    } catch(...) {
+        gpuErrchk(cudaFree(d_u_traj));
+        gpuErrchk(cudaFree(d_box_traj));
         throw;
     }
 
