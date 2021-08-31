d8917a7ac48ddf229e3f1c112b189333818cdb52
Forrest York <forrest.york@gmail.com>
2021-08-31 14:00:51 -0600

    Adds test for setup_relative_restraints_using_smarts

--- tests/test_free_energy_rabfe.py
+++ tests/test_free_energy_rabfe.py
@@ -1,6 +1,14 @@
 import io
 from contextlib import redirect_stdout
-from fe.free_energy_rabfe import RABFEResult
+from fe.free_energy_rabfe import (
+    RABFEResult,
+    setup_relative_restraints_by_distance,
+    setup_relative_restraints_using_smarts
+)
+from fe.atom_mapping import CompareDistNonterminal
+
+from rdkit import Chem
+from rdkit.Chem import AllChem, rdFMCS
 
 
 def capture_print(closure):
@@ -23,3 +31,27 @@ def test_rabfe_result_to_from_log():
 
     reconstructed = RABFEResult.from_log(first_line)
     assert result == reconstructed
+
+def test_setting_up_restraints_using_smarts():
+    seed = 814
+
+    mcs_params = rdFMCS.MCSParameters()
+    mcs_params.AtomTyper = CompareDistNonterminal()
+    mcs_params.BondTyper = rdFMCS.BondCompare.CompareAny
+
+    smi_a = "CCCONNN"
+    smi_b = "CCCNNN"
+    mol_a = Chem.MolFromSmiles(smi_a)
+    mol_a = Chem.AddHs(mol_a)
+    mol_b = Chem.MolFromSmiles(smi_b)
+    mol_b = Chem.AddHs(mol_b)
+    for mol in [mol_a, mol_b]:
+        AllChem.EmbedMolecule(mol, randomSeed=seed)
+
+    result = rdFMCS.FindMCS(
+        [mol_a, mol_b],
+        mcs_params
+    )
+
+    core = setup_relative_restraints_using_smarts(mol_a, mol_b, result.smartsString)
+    assert core.shape == (2, 2)
