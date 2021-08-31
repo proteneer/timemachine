0111ce4431bd1022418b6d7cc49a91874ea16e3c
Forrest York <forrest.york@gmail.com>
2021-08-31 14:02:25 -0600

    Adds method to compute restraints using distance
    * As RABFE doesn't require a bijection between blocker and compound
      atoms, can simply check distances

--- tests/test_free_energy_rabfe.py
+++ tests/test_free_energy_rabfe.py
@@ -1,4 +1,5 @@
 import io
+import numpy as np
 from contextlib import redirect_stdout
 from fe.free_energy_rabfe import (
     RABFEResult,
@@ -6,6 +7,7 @@ from fe.free_energy_rabfe import (
     setup_relative_restraints_using_smarts
 )
 from fe.atom_mapping import CompareDistNonterminal
+from fe.utils import get_romol_conf
 
 from rdkit import Chem
 from rdkit.Chem import AllChem, rdFMCS
@@ -31,6 +33,38 @@ def test_rabfe_result_to_from_log():
 
     reconstructed = RABFEResult.from_log(first_line)
     assert result == reconstructed
+
+def test_setting_up_restraints_using_distance():
+    seed = 814
+    smi_a = "CCCONNN"
+    smi_b = "CCCNNN"
+    mol_a = Chem.MolFromSmiles(smi_a)
+    mol_a = Chem.AddHs(mol_a)
+
+    mol_b = Chem.MolFromSmiles(smi_b)
+    mol_b = Chem.AddHs(mol_b)
+    for mol in [mol_a, mol_b]:
+        AllChem.EmbedMolecule(mol, randomSeed=seed)
+
+    mol_a_coords = get_romol_conf(mol_a)
+    mol_b_coords = get_romol_conf(mol_b)
+
+    core = setup_relative_restraints_by_distance(mol_a, mol_b)
+    assert core.shape == (5, 2)
+
+    # If we have a 0 cutoff, expect nothing to overlap
+    core = setup_relative_restraints_by_distance(mol_a, mol_b, cutoff=0.0)
+    assert core.shape == (0,)
+
+    for cutoff in [0.08, 0.1, 0.2, 1.0]:
+        core = setup_relative_restraints_by_distance(mol_a, mol_b, cutoff=cutoff)
+        assert core.size > 0
+        for a, b in core.tolist():
+            assert np.linalg.norm(mol_a_coords[a] - mol_b_coords[b]) < cutoff
+
+    # Adds seven hydrogen (terminal) atoms if allow terminal matches
+    core = setup_relative_restraints_by_distance(mol_a, mol_b, terminal=True)
+    assert core.shape == (12, 2)
 
 def test_setting_up_restraints_using_smarts():
     seed = 814
