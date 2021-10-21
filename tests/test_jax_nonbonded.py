f25d28a1e5984b3c4a1ba0e4462616f19d7cee13
Josh Fass <jfass@relaytx.com>
2021-10-21 10:12:51 -0400

    Improve Jax nonbonded test case (#514)
    * Remove "sparsely populated" box from test
    * Add clash-free "crowded" box to test
    * Refactor test to allow randomization of each argument to be toggled independently (default behavior is still to randomize all arguments)
    * Generate symmetric random rescale_masks in test
    * Make arbitrary changes to distributions of some test arguments (e.g. sig ~ Unif([0,1]) -> sig ~ Unif([0, 0.1]))
    * Remove an == comparison (use assert_almost_equal instead)
    * Add waterbox to test

--- tests/test_jax_nonbonded.py
+++ tests/test_jax_nonbonded.py
@@ -5,11 +5,16 @@ import numpy as onp
 from numpy.random import randn, rand, randint, seed
 seed(2021)
 
+from scipy.optimize import minimize
+
 from jax import numpy as np, value_and_grad, jit, vmap
 
 from jax.ops import index_update, index
 from timemachine.potentials.nonbonded import nonbonded_v3, nonbonded_v3_on_specific_pairs
-from timemachine.potentials.jax_utils import convert_to_4d, get_all_pairs_indices, get_group_group_indices
+from timemachine.potentials.jax_utils import convert_to_4d, get_all_pairs_indices, get_group_group_indices, distance
+from md import builders
+from ff.handlers import openmm_deserializer
+from simtk import unit
 
 from functools import partial
 from typing import Tuple, Callable
@@ -22,38 +27,148 @@ NonbondedArgs = Tuple[nonbonded_args]
 NonbondedFxn = Callable[[*nonbonded_args], Energy]
 
 
-def generate_random_inputs(n_atoms: int, dim: int = 3) -> NonbondedArgs:
-    # distribute somewhat sparsely within box by:
-    #   * generating evenly spaced points along each axis
-    #   * shuffling independently along each axis
-    #   * adding small Gaussian noise
-    # so that it's unlikely that two points will be right on top of each other
-    offsets = onp.array([onp.arange(n_atoms)] * dim).T
-    assert (offsets.shape == (n_atoms, dim))
-    for i in range(dim):
-        onp.random.shuffle(offsets[:, i])
-    conf = (0.5 * randn(n_atoms, dim) + offsets)
-    conf += conf.min()
-    params = rand(n_atoms, 3)
-    params[:, 0] -= np.mean(params[:, 0])
+def resolve_clashes(x0, box0, min_dist=0.1):
+    def urt(x, box):
+        distance_matrix = distance(x, box)
+        i, j = np.triu_indices(len(distance_matrix), k=1)
+        return distance_matrix[i, j]
 
-    box = np.diag(np.max(conf, axis=0)) + 0.1
+    dij = urt(x0, box0)
+    x_shape = x0.shape
+    box_shape = box0.shape
+
+    if np.min(dij) < min_dist:
+        # print('some distances too small')
+        print(f'before optimization: min(dij) = {np.min(dij)} < min_dist threshold ({min_dist})')
+        # print('smallest few distances', sorted(dij)[:10])
+
+        def unflatten(xbox):
+            n = x_shape[0] * x_shape[1]
+            x = xbox[:n].reshape(x_shape)
+            box = xbox[n:].reshape(box_shape)
+            return x, box
+
+        def U_repulse(xbox):
+            x, box = unflatten(xbox)
+            dij = urt(x, box)
+            return np.sum(np.where(dij < min_dist, (dij - min_dist) ** 2, 0))
+
+        def fun(xbox):
+            v, g = value_and_grad(U_repulse)(xbox)
+            return float(v), onp.array(g, onp.float64)
+
+        initial_state = np.hstack([x0.flatten(), box0.flatten()])
+        # print(f'penalty before: {U_repulse(initial_state)}')
+        result = minimize(fun, initial_state, jac=True, method='L-BFGS-B')
+        # print(f'penalty after minimization: {U_repulse(result.x)}')
+
+        x, box = unflatten(result.x)
+        dij = urt(x, box)
+
+        print(f'after optimization: min(dij) = {np.min(dij)}')
+
+        return x, box
+
+    else:
+        return x0, box0
+
+
+easy_instance_flags = dict(
+    trigger_pbc=False,
+    randomize_box=False,
+    randomize_charges=False,
+    randomize_sigma=False,
+    randomize_epsilon=False,
+    randomize_lamb=False,
+    randomize_charge_rescale_mask=False,
+    randomize_lj_rescale_mask=False,
+    randomize_lambda_plane_idxs=False,
+    randomize_lambda_offset_idxs=False,
+    randomize_beta=False,
+    randomize_cutoff=False,
+)
+
+difficult_instance_flags = {key: True for key in easy_instance_flags}
+
+def generate_waterbox_nb_args() -> NonbondedArgs:
+
+    system, positions, box, _ = builders.build_water_system(3.0)
+    bps, masses = openmm_deserializer.deserialize_system(system, cutoff=1.2)
+    nb = bps[-1]
+    params = nb.params
+
+    conf = positions.value_in_unit(unit.nanometer)
+
+    N = conf.shape[0]
+    beta = nb.get_beta()
+    cutoff = nb.get_cutoff()
+
+    lamb = 0.0
+    charge_rescale_mask = onp.ones((N, N))
+    lj_rescale_mask = onp.ones((N, N))
+    lambda_plane_idxs = np.zeros(N, dtype=int)
+    lambda_offset_idxs = np.zeros(N, dtype=int)
+
+    args = conf, params, box, lamb, charge_rescale_mask, lj_rescale_mask, beta, cutoff, lambda_plane_idxs, lambda_offset_idxs
+
+    return args
+
+
+def generate_random_inputs(n_atoms, dim, instance_flags=difficult_instance_flags) -> NonbondedArgs:
+    """Can toggle randomization of each argument using instance_flags"""
+    box = np.eye(dim)
+    if instance_flags['randomize_box']:
+        box += np.diag(rand(dim))
     assert box.shape == (dim, dim)
 
-    lamb = rand()
+    conf = rand(n_atoms, dim)
+    if instance_flags['trigger_pbc']:
+        conf *= 5
+        conf -= 2.5
 
+    min_dist = 0.1
+    conf, box = resolve_clashes(conf, box, min_dist=min_dist)
+
+    charges = np.zeros(n_atoms)
+    sig = min_dist * np.ones(n_atoms)
+    eps = np.ones(n_atoms)
+    if instance_flags['randomize_charges']:
+        charges = randn(n_atoms)
+    if instance_flags['randomize_sigma']:
+        sig = min_dist * rand(n_atoms)
+    if instance_flags['randomize_epsilon']:
+        eps = rand(n_atoms)
+
+    params = np.array([charges, sig, eps]).T
+
+    lamb = 0.0
+    if instance_flags['randomize_lamb']:
+        lamb = rand()
     charge_rescale_mask = onp.ones((n_atoms, n_atoms))
     lj_rescale_mask = onp.ones((n_atoms, n_atoms))
+
     for _ in range(n_atoms):
         i, j = randint(n_atoms, size=2)
-        charge_rescale_mask[i, j] = 0.0
-        lj_rescale_mask[i, j] = 0.0
+        if instance_flags['randomize_charge_rescale_mask']:
+            charge_rescale_mask[i, j] = charge_rescale_mask[j, i] = 0.0
+        if instance_flags['randomize_lj_rescale_mask']:
+            lj_rescale_mask[i, j] = lj_rescale_mask[j, i] = 0.0
 
-    beta = rand() + 1
-    cutoff = rand() + 0.5
+    beta = 2.0
+    if instance_flags['randomize_beta']:
+        beta += rand()
+    cutoff = 1.2
+    if instance_flags['randomize_cutoff']:
+        cutoff += rand()
 
-    lambda_plane_idxs = randint(low=-2, high=2, size=n_atoms)
-    lambda_offset_idxs = randint(low=-2, high=2, size=n_atoms)
+    lambda_plane_idxs = np.zeros(n_atoms, dtype=int)
+    lambda_offset_idxs = np.zeros(n_atoms, dtype=int)
+
+    if instance_flags['randomize_lambda_plane_idxs']:
+        lambda_plane_idxs = randint(low=-2, high=2, size=n_atoms)
+
+    if instance_flags['randomize_lambda_offset_idxs']:
+        lambda_offset_idxs = randint(low=-2, high=2, size=n_atoms)
 
     args = conf, params, box, lamb, charge_rescale_mask, lj_rescale_mask, beta, cutoff, lambda_plane_idxs, lambda_offset_idxs
 
@@ -66,7 +181,6 @@ def compare_two_potentials(u_a: NonbondedFxn, u_b: NonbondedFxn, args: Nonbonded
     energy_a, gradients_a = value_and_grads(u_a)(*args)
     energy_b, gradients_b = value_and_grads(u_b)(*args)
 
-    assert energy_a == energy_b
     onp.testing.assert_almost_equal(energy_a, energy_b)
     for (g_a, g_b) in zip(gradients_a, gradients_b):
         onp.testing.assert_allclose(g_a, g_b)
@@ -112,13 +226,21 @@ def _nonbonded_v3_clone(
 
     lj, coulomb = nonbonded_v3_on_specific_pairs(conf, params, box, inds_i, inds_j, beta, cutoff)
 
+    # keep only eps > 0
+    eps = params[:, 2]
+    lj = np.where(eps[inds_i] > 0, lj, 0)
+    lj = np.where(eps[inds_j] > 0, lj, 0)
+
     eij_total = lj * lj_rescale_mask[inds_i, inds_j] + coulomb * charge_rescale_mask[inds_i, inds_j]
 
     return np.sum(eij_total)
 
 
-def test_jax_nonbonded(n_instances=10):
-    """Assert that nonbonded_v3 and _nonbonded_v3 agree on several random instances"""
+def run_randomized_tests_of_jax_nonbonded(instance_generator, n_instances=10):
+    """Assert that nonbonded_v3 and _nonbonded_v3 agree on several random instances
+
+    instance_generator(n_atoms, dim) -> NonbondedArgs
+    """
     jittable_nonbonded_v3 = partial(nonbonded_v3, runtime_validate=False)
     u_a, u_b = jit(jittable_nonbonded_v3), jit(_nonbonded_v3_clone)
 
@@ -128,8 +250,25 @@ def test_jax_nonbonded(n_instances=10):
     dims = onp.random.randint(3, 5, n_instances)
 
     for n_atoms, dim in zip(random_sizes, dims):
-        args = generate_random_inputs(n_atoms, dim)
+        args = instance_generator(n_atoms, dim)
         compare_two_potentials(u_a, u_b, args)
+
+
+def test_jax_nonbonded_waterbox():
+    jittable_nonbonded_v3 = partial(nonbonded_v3, runtime_validate=False)
+    u_a, u_b = jit(jittable_nonbonded_v3), jit(_nonbonded_v3_clone)
+    compare_two_potentials(u_a, u_b, generate_waterbox_nb_args())
+
+
+def test_jax_nonbonded_easy(n_instances=10):
+    instance_generator = partial(generate_random_inputs, instance_flags=easy_instance_flags)
+    run_randomized_tests_of_jax_nonbonded(instance_generator, n_instances)
+
+
+def test_jax_nonbonded(n_instances=10):
+    instance_generator = partial(generate_random_inputs, instance_flags=difficult_instance_flags)
+    run_randomized_tests_of_jax_nonbonded(instance_generator, n_instances)
+
 
 def test_vmap():
     """Can call jit(vmap(nonbonded_v3_on_specific_pairs))"""
@@ -148,7 +287,7 @@ def test_vmap():
     # signature: conf -> ljs, coulombs, where ljs.shape == (n_interactions, )
     u_pairs = partial(nonbonded_v3_on_specific_pairs, **fixed_kwargs)
     ljs, coulombs = u_pairs(conf)
-    assert ljs.shape == (n_interactions, )
+    assert ljs.shape == (n_interactions,)
 
     def u(conf):
         ljs, coulombs = u_pairs(conf)
@@ -159,4 +298,4 @@ def test_vmap():
     n_snapshots = 100
     confs = onp.random.randn(n_snapshots, n_total, 3)
     us = vmapped(confs)
-    assert us.shape == (n_snapshots, )
+    assert us.shape == (n_snapshots,)
