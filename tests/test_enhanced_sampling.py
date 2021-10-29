dce794e49e6d5e03032408132fee369995992717
Yutong Zhao <proteneer@gmail.com>
2021-10-29 15:17:26 -0400

    Gas Phase Enhanced Sampling (#518)
    * Add gas-phase enhanced sampling protocol and tests

---
+++ tests/test_enhanced_sampling.py
@@ -0,0 +1,94 @@
+# set environment variable when running this file.
+import os
+
+# (ytz): not pretty, but this is needed to get XLA to be less stupid
+# see https://github.com/google/jax/issues/1408 for more information
+# needs to be set before xla/jax is initialized, and is set to a number
+# suitable for running on CI
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=6"
+
+from jax.config import config
+
+config.update("jax_enable_x64", True)
+import jax
+
+
+from timemachine.potentials import bonded
+import numpy as np
+from md import enhanced
+from tests import test_ligands
+
+from ff import Forcefield
+from ff.handlers.deserialize import deserialize_handlers
+
+
+def get_ff():
+    ff_handlers = deserialize_handlers(open("ff/params/smirnoff_1_1_0_sc.py").read())
+    ff = Forcefield(ff_handlers)
+    return ff
+
+
+def test_gas_phase_importance_sampling():
+    """
+    This tests importance sampling in the gas-phase, where samples generated
+    from a proposal distribution p_easy are reweighted into the target p_decharged.
+
+    The intuition is that we can easily overcome various intramolecular barriers
+    such as torsions, steric clashes, by turning off specific torsions and nonbonded
+    terms.
+    """
+
+    mol = test_ligands.get_benzene()
+    ff = get_ff()
+    torsion_idxs = np.array([5, 6, 7, 8])
+    temperature = 300
+
+    state = enhanced.VacuumState(mol, ff)
+
+    # samples are weighted by counts
+    weighted_samples, log_weights = enhanced.generate_log_weighted_samples(
+        mol,
+        temperature,
+        state.U_easy,
+        state.U_decharged,
+    )
+
+    enhanced_samples = enhanced.sample_from_log_weights(weighted_samples, log_weights, 100000)
+
+    @jax.jit
+    def get_torsion(x_t):
+        cijkl = x_t[torsion_idxs]
+        return bonded.signed_torsion_angle(*cijkl)
+
+    batch_torsion_fn = jax.vmap(get_torsion)
+    enhanced_torsions = batch_torsion_fn(enhanced_samples)
+
+    num_negative = np.sum(enhanced_torsions < 0)
+    num_positive = np.sum(enhanced_torsions >= 0)
+
+    # should be roughly 50/50
+    assert np.abs(num_negative / (num_negative + num_positive) - 0.5) < 0.05
+
+    # check that the distributions on the lhs look roughly identical
+
+    enhanced_torsions_lhs, _ = np.histogram(enhanced_torsions, bins=50, range=(-np.pi, 0), density=True)
+    enhanced_torsions_rhs, _ = np.histogram(enhanced_torsions, bins=50, range=(0, np.pi), density=True)
+
+    # check for symmetry about theta=0
+    assert np.mean((enhanced_torsions_lhs - enhanced_torsions_rhs[::-1]) ** 2) < 5e-2
+
+    weighted_samples, log_weights = enhanced.generate_log_weighted_samples(
+        mol,
+        temperature,
+        state.U_decharged,
+        state.U_decharged,
+        num_batches=5000,  # don't need as many batches since we don't have to prune
+    )
+
+    vanilla_samples = enhanced.sample_from_log_weights(weighted_samples, log_weights, 100000)
+
+    vanilla_torsions = batch_torsion_fn(vanilla_samples)
+    vanilla_samples_lhs, _ = np.histogram(vanilla_torsions, bins=50, range=(-np.pi, 0), density=True)
+
+    # check for consistency with vanilla samples
+    assert np.mean((enhanced_torsions_lhs - vanilla_samples_lhs) ** 2) < 1e-2
--- timemachine/integrator.py
+++ timemachine/integrator.py
@@ -1,5 +1,8 @@
 from timemachine.constants import BOLTZ
 import numpy as np
+import jax
+from jax import random as jrandom
+import time
 
 
 def langevin_coefficients(temperature, dt, friction, masses):
@@ -80,3 +83,86 @@ class LangevinIntegrator(Integrator):
         new_x = x + 0.5 * self.dt * (v_mid + new_v)
 
         return new_x, new_v
+
+
+def _fori_steps(x0, v0, key0, grad_fn, num_steps, dt, ca, cbs, ccs):
+    def body_fn(_, val):
+        # BAOAB integrator
+        x_t, v_t, key = val
+        du_dx = grad_fn(x_t)[0]
+        v_mid = v_t + cbs * du_dx
+        noise = jrandom.normal(key, v_t.shape)
+        _, sub_key = jrandom.split(key)
+        v_t = ca * v_mid + ccs * noise
+        x_t += 0.5 * dt * (v_mid + v_t)
+        return x_t, v_t, sub_key
+
+    return jax.lax.fori_loop(0, num_steps, body_fn, (x0, v0, key0))
+
+
+def simulate(x0, U_fn, temperature, masses, steps_per_batch, num_batches, num_workers, seed=None):
+    """
+    Simulate a gas-phase system using a reference jax implementation.
+
+    Parameters
+    ----------
+
+    x0: (N,3) np.ndarray
+        initial coordinates
+
+    U_fn: function
+        Potential energy function
+
+    temperature: float
+        Temperature in Kelvin
+
+    steps_per_batch: int
+        number of steps we run for each batch
+
+    num_batches: int
+        number of batches we run
+
+    num_workers: int
+        How many jobs to run in parallel
+
+    seed: int
+        used for the random number generated
+
+    Returns
+    -------
+
+    """
+    dt = 1.5e-3
+    friction = 1.0
+    ca, cbs, ccs = langevin_coefficients(temperature, dt, friction, masses)
+    cbs = np.expand_dims(cbs * -1, axis=-1)
+    ccs = np.expand_dims(ccs, axis=-1)
+
+    grad_fn = jax.jit(jax.grad(U_fn, argnums=(0,)))
+    U_fn = jax.jit(U_fn)
+
+    if seed is None:
+        seed = int(time.time())
+
+    @jax.jit
+    def multiple_steps(x0, v0, key0):
+        return _fori_steps(x0, v0, key0, grad_fn, steps_per_batch, dt, ca, cbs, ccs)
+
+    v0 = np.zeros_like(x0)
+
+    # jitting a pmap will result in a warning about inefficient data movement
+    batched_multiple_steps_fn = jax.pmap(multiple_steps)
+
+    xs_t = np.array([x0] * num_workers)
+    vs_t = np.array([v0] * num_workers)
+    keys_t = np.array([jrandom.PRNGKey(seed + idx) for idx in range(num_workers)])
+
+    all_xs = []
+
+    for batch_step in range(num_batches):
+        #                                             [B,N,3][B,N,3][B,2]
+        xs_t, vs_t, keys_t = batched_multiple_steps_fn(xs_t, vs_t, keys_t)
+        all_xs.append(xs_t)
+
+    # result has shape [num_workers, num_batches, num_atoms, num_dimensions]
+    return np.transpose(np.array(all_xs), axes=[1, 0, 2, 3])
