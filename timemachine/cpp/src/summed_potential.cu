8f6533495cda339a4e83517211bcbcc02666185c
Matt Wittmann <mwittman@relaytx.com>
2021-11-18 12:06:26 -0800

    Don't require replicated input coordinates

--- timemachine/cpp/src/summed_potential.cu
+++ timemachine/cpp/src/summed_potential.cu
@@ -13,9 +13,9 @@ SummedPotential::SummedPotential(Potential &u_a, Potential &u_b, const int P_a) 
 void SummedPotential::execute_device(
     const int N,
     const int P,
-    const double *d_x,   // 2 * (N * 3)
+    const double *d_x,   // N * 3
     const double *d_p,   // P_a + P_b
-    const double *d_box, // 2 * (3 * 3)
+    const double *d_box, // 3 * 3
     const double lambda,
     unsigned long long *d_du_dx, // 2 * (N * 3)
     double *d_du_dp,             // P_a + P_b
@@ -39,9 +39,9 @@ void SummedPotential::execute_device(
     u_b.execute_device(
         N,
         P - P_a, // number of parameters for second potential
-        d_x + N * 3,
+        d_x,
         d_p + P_a,
-        d_box + 3 * 3,
+        d_box,
         lambda,
         d_du_dx + N * 3,
         d_du_dp + P_a,
