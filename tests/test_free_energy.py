7094161c5662eaa7c558451736c614acfe04c60d
Matt Wittmann <mwittman@relaytx.com>
2021-11-29 09:16:21 -0800

    Revert "Use plain assertions for scalars"
    This reverts commit f7bee69401dc49a4a7a417a25adb12175771a62f.

--- tests/test_free_energy.py
+++ tests/test_free_energy.py
@@ -389,8 +389,7 @@ def test_construct_differentiable_interface_fast():
         U_ref = construct_differentiable_interface(unbound_potentials, precision)
         U = construct_differentiable_interface_fast(unbound_potentials, sys_params, precision)
         args = (coords, sys_params, box, lam)
-
-        assert U(*args) == U_ref(*args)
+        np.testing.assert_array_equal(U(*args), U_ref(*args))
 
         argnums = (0, 1, 3)
         grad_U_ref = grad(U_ref, argnums=argnums)(*args)
@@ -401,4 +400,4 @@ def test_construct_differentiable_interface_fast():
         for dU_dp, dU_dp_ref in zip(grad_U[1], grad_U_ref[1]):
             np.testing.assert_array_equal(dU_dp, dU_dp_ref)
 
-        assert grad_U[2] == grad_U_ref[2]
+        np.testing.assert_array_equal(grad_U[2], grad_U_ref[2])
