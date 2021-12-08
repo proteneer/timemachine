5509fb30e3ec3ea52bcdd7c56d241a8e7ab64d72
Josh Fass <jfass@relaytx.com>
2021-12-07 21:12:44 -0500

    Add v_t, box to test_set_and_get

--- tests/test_md.py
+++ tests/test_md.py
@@ -64,9 +64,17 @@ class TestContext(unittest.TestCase):
         np.testing.assert_equal(ctxt.get_box(), box)
 
         new_x = np.random.rand(N, 3)
+        new_v = np.random.rand(N, 3)
+        new_box = np.random.rand(3, 3)
         ctxt.set_x_t(new_x)
+        ctxt.set_v_t(new_v)
+        ctxt.set_box(new_box)
+
 
         np.testing.assert_equal(ctxt.get_x_t(), new_x)
+        np.testing.assert_equal(ctxt.get_v_t(), new_v)
+        np.testing.assert_equal(ctxt.get_box(), new_box)
+
 
     def test_fwd_mode(self):
         """
