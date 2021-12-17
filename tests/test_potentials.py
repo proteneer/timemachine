67ae854961c0a836d2e0d086e0c9c31d50dc3262
Matt Wittmann <mwittman@relaytx.com>
2021-12-17 10:53:21 -0800

    Add test

--- tests/test_potentials.py
+++ tests/test_potentials.py
@@ -1,11 +1,25 @@
 import pytest
+import numpy as np
 
-import timemachine.lib.potentials as ps
+from timemachine.lib import custom_ops, potentials
 
 
-def test_summed_potential_raises_on_inconsistent_lengths():
+@pytest.fixture
+def harmonic_bond():
+    return potentials.HarmonicBond(np.array([[0, 1], [0, 2]], dtype=np.int32)).bind([[1, 0], [1, 0]])
+
+
+def test_summed_potential_raises_on_inconsistent_lengths(harmonic_bond):
 
     with pytest.raises(ValueError) as excinfo:
-        ps.SummedPotential([ps.HarmonicBond()], [])
+        potentials.SummedPotential([harmonic_bond], [])
 
     assert str(excinfo.value) == "number of potentials != number of parameter arrays"
+
+
+def test_bound_potential_get_potential(harmonic_bond):
+
+    unbound_impl = harmonic_bond.unbound_impl(np.float32)
+    bound_impl = custom_ops.BoundPotential(unbound_impl, harmonic_bond.params)
+
+    assert unbound_impl is bound_impl.get_potential()
