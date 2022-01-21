5cc6dd58c10d1b2c4b383f959230bb927b131411
Matt Wittmann <mwittmann@relaytx.com>
2022-01-21 10:54:10 -0800

    Collect garbage before calling cudaDeviceReset()
    Otherwise destructors can be called after reset, resulting in
    attempts to free unallocated memory

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 # This file defines fixtures shared across multiple test modules.
 # https://docs.pytest.org/en/latest/how-to/fixtures.html#scope-sharing-fixtures-across-classes-modules-packages-or-session
 
+import gc
 import pytest
 from timemachine.lib import custom_ops
 
@@ -8,6 +9,12 @@ from timemachine.lib import custom_ops
 @pytest.fixture(autouse=True)
 def reset_cuda_device_after_test():
     """Calls cudaDeviceReset() after each test.
+
     This is needed for 'cuda-memcheck --leak-check full' to catch leaks"""
+
     yield
+
+    # ensure that destructors are called before cudaDeviceReset()
+    gc.collect()
+
     custom_ops.cuda_device_reset()
