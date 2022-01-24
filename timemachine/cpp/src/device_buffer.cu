e75ab43db35862fc36f94663dc662c0cd6ff8768
Matt Wittmann <mwittmann@relaytx.com>
2022-01-24 10:37:46 -0800

    Improve safety of allocate
    Shouldn't be able to allocate a size that's not a multiple of sizeof(T)

--- timemachine/cpp/src/device_buffer.cu
+++ timemachine/cpp/src/device_buffer.cu
@@ -4,14 +4,14 @@
 
 namespace timemachine {
 
-template <typename T> T *allocate(const std::size_t size) {
+template <typename T> T *allocate(const std::size_t length) {
     T *buffer;
-    gpuErrchk(cudaMalloc(&buffer, size));
+    gpuErrchk(cudaMalloc(&buffer, length * sizeof(T)));
     return buffer;
 }
 
 template <typename T>
-DeviceBuffer<T>::DeviceBuffer(const std::size_t length) : size(length * sizeof(T)), data(allocate<T>(size)) {}
+DeviceBuffer<T>::DeviceBuffer(const std::size_t length) : size(length * sizeof(T)), data(allocate<T>(length)) {}
 
 template <typename T> DeviceBuffer<T>::~DeviceBuffer() { gpuErrchk(cudaFree(data)); }
 
