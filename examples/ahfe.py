c3ce8cfce359c30b75369e1d06146b1d20144bcc
Josh Fass <jfass@relaytx.com>
2022-01-26 14:16:04 -0800

    Format

--- examples/ahfe.py
+++ examples/ahfe.py
@@ -1,13 +1,14 @@
 # absolute hydration free energy
 
 import numpy as np
+from pathlib import Path
+from tqdm import tqdm
 
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
 import timemachine
 from timemachine.constants import BOLTZ
-
 
 from ff import Forcefield
 from ff.handlers import openmm_deserializer
@@ -22,13 +23,14 @@ from md.moves import NPTMove
 from md.states import CoordsVelBox
 
 from pymbar import BAR
+
 from typing import Dict, Tuple, List
 from numpy.typing import ArrayLike
 
-from pathlib import Path
-
-from tqdm import tqdm
-
+# type aliases
+Array = ArrayLike
+Traj = List[CoordsVelBox]
+Diagnostics = Dict
 
 # force field
 tm_path = Path(timemachine.__path__[0]).parent
@@ -38,26 +40,24 @@ with open(path_to_ff, "r") as f:
 ff = Forcefield(ff_handlers)
 
 # ligand
-romol = Chem.AddHs(Chem.MolFromSmiles("CC(=O)OC1=CC=CC=C1C(=O)O"))
-ligand_masses = [a.GetMass() for a in romol.GetAtoms()]
-AllChem.EmbedMolecule(romol)
-ligand_coords = get_romol_conf(romol)
+mol = Chem.AddHs(Chem.MolFromSmiles("CC(=O)OC1=CC=CC=C1C(=O)O"))
+ligand_masses = [a.GetMass() for a in mol.GetAtoms()]
+AllChem.EmbedMolecule(mol)
+ligand_coords = get_romol_conf(mol)
 
 # water box
 host_system, host_coords, box, omm_topology = builders.build_water_system(4.0)
 host_bps, host_masses = openmm_deserializer.deserialize_system(host_system, cutoff=1.2)
 num_host_atoms = host_coords.shape[0]
 
-# minimize
-min_host_coords = minimizer.minimize_host_4d([romol], host_system, host_coords, ff, box)
 # water box + ligand
+min_host_coords = minimizer.minimize_host_4d([mol], host_system, host_coords, ff, box)
 masses = np.concatenate([host_masses, ligand_masses])
 coords = np.concatenate([min_host_coords, ligand_coords])
 
 # alchemical topologies / potentials
 final_potentials = []
-
-gbt = topology.BaseTopology(romol, ff)
+gbt = topology.BaseTopology(mol, ff)
 hgt = topology.HostGuestTopology(host_bps, gbt)
 
 fn_handle_tuples = [
@@ -73,6 +73,7 @@ for fn, handles in fn_handle_tuples:
 
 bound_impls = [p.bound_impl(np.float32) for p in final_potentials]
 
+# reduced potential
 temperature = 300
 pressure = 1.0
 
@@ -91,10 +92,7 @@ n_equil_moves = 50
 
 dt = 1.5e-3
 
-seed = 2022
-
-npt = NPTMove(final_potentials, 1.0, masses, temperature, pressure, n_steps_per_move, seed)
-
+npt = NPTMove(final_potentials, None, masses, temperature, pressure, n_steps_per_move, seed=2022)
 
 # collect samples from p(x | lam) for lam in lambda_schedule
 lambda_schedule = construct_lambda_schedule(32)
@@ -105,8 +103,8 @@ for lam in lambda_schedule:
     print("lam = ", lam)
     npt.lamb = lam
 
+    # equilibration
     x_equil = CoordsVelBox(coords, np.zeros_like(coords), box)
-
     trange = tqdm(range(n_equil_moves), desc="equilibration")
     for _ in trange:
         x_equil = npt.move(x_equil)
@@ -120,12 +118,6 @@ for lam in lambda_schedule:
     trajs.append(traj)
 
 # analyze
-# TODO: move this into fe module, if it's not there already
-Array = ArrayLike
-Traj = List[CoordsVelBox]
-Diagnostics = Dict
-
-
 def vec_u(xs: Traj, lam: float) -> Array:
     return np.array([u(x, lam) for x in xs])
 
@@ -134,6 +126,7 @@ def work(xs: Traj, from_lam: float, to_lam: float) -> Array:
     return vec_u(xs, to_lam) - vec_u(xs, from_lam)
 
 
+# TODO: move this into fe module, if it's not there already?
 def pair_bar(trajs: List[Traj], lambda_schedule: Array) -> Tuple[float, Diagnostics]:
     assert len(trajs) == len(lambda_schedule)
 
@@ -152,6 +145,7 @@ def pair_bar(trajs: List[Traj], lambda_schedule: Array) -> Tuple[float, Diagnost
     return delta_f, dict(works=works, estimates=estimates)
 
 
+# print
 delta_f_decouple, diagnostics = pair_bar(trajs, lambda_schedule)
 delta_f_hydration = -delta_f_decouple  # reduced
 delta_F_hydration = delta_f_hydration * (BOLTZ * temperature)  # in kJ/mol
