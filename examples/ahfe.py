adfeaa07c7a294d4e77558ccf8c57a177e355543
Josh Fass <jfass@relaytx.com>
2022-01-26 13:46:30 -0800

    Add pair bar
    Also use minimizer.minimize_host_4d

--- examples/ahfe.py
+++ examples/ahfe.py
@@ -18,15 +18,25 @@ from md import builders
 from md.moves import NPTMove
 from md.states import CoordsVelBox
 
+from pymbar import BAR
+from typing import Dict, Tuple, List
+from numpy.typing import ArrayLike
+
 from pathlib import Path
 
 from tqdm import tqdm
+
+from timemachine.constants import BOLTZ
+
+from md import minimizer
+
 
 # force field
 tm_path = Path(timemachine.__path__[0]).parent
 path_to_ff = tm_path / "ff/params/smirnoff_1_1_0_ccc.py"
 with open(path_to_ff, "r") as f:
     ff_handlers = deserialize_handlers(f.read())
+ff = Forcefield(ff_handlers)
 
 # ligand
 romol = Chem.AddHs(Chem.MolFromSmiles("CC(=O)OC1=CC=CC=C1C(=O)O"))
@@ -35,17 +45,19 @@ AllChem.EmbedMolecule(romol)
 ligand_coords = get_romol_conf(romol)
 
 # water box
-system, host_coords, box, omm_topology = builders.build_water_system(4.0)
-host_bps, host_masses = openmm_deserializer.deserialize_system(system, cutoff=1.2)
+host_system, host_coords, box, omm_topology = builders.build_water_system(4.0)
+host_bps, host_masses = openmm_deserializer.deserialize_system(host_system, cutoff=1.2)
 num_host_atoms = host_coords.shape[0]
 
+# minimize
+min_host_coords = minimizer.minimize_host_4d([romol], host_system, host_coords, ff, box)
 # water box + ligand
 masses = np.concatenate([host_masses, ligand_masses])
-coords = np.concatenate([host_coords, ligand_coords])
+coords = np.concatenate([min_host_coords, ligand_coords])
 
 # alchemical topologies / potentials
 final_potentials = []
-ff = Forcefield(ff_handlers)
+
 gbt = topology.BaseTopology(romol, ff)
 hgt = topology.HostGuestTopology(host_bps, gbt)
 
@@ -60,15 +72,27 @@ for fn, handles in fn_handle_tuples:
     params, potential = fn(*[h.params for h in handles])
     final_potentials.append(potential.bind(params))
 
+bound_impls = [p.bound_impl(np.float32) for p in final_potentials]
+
+temperature = 300
+pressure = 1.0
+
+
+def u(x: CoordsVelBox, lam: float) -> float:
+    # TODO: replace with SummedPotential
+    U_s = np.array([bp.execute(x.coords, x.box, lam)[-1] for bp in bound_impls])
+    u_s = U_s / (BOLTZ * temperature)
+    return np.sum(u_s)
+
+
 # MD
 n_steps_per_move = 50
 n_prod_samples = 100
 n_equil_moves = 50
 
-temperature = 300
 dt = 1.5e-3
 friction = 10
-pressure = 1.0
+
 seed = 2022
 
 
@@ -83,24 +107,14 @@ trajs = []
 for lam in lambda_schedule:
     print("lam = ", lam)
 
-    x0 = CoordsVelBox(coords, np.zeros_like(coords), box)
+    x_equil = CoordsVelBox(coords, np.zeros_like(coords), box)
 
-    # initial insertion step to remove clashes
-    # TODO: this is currently very slow, since NPTMove doesn't allow lambda to be set on the fly
-    #   --> modify NPTMove so that this can be fast
-    x_relaxed = x0
-    declash_schedule = np.linspace(1.0, lam, 2)
-    trange = tqdm(declash_schedule, desc="resolving clashes")
-    for noneq_lam in trange:
-        npt = construct_npt_move(noneq_lam, friction=np.inf)
-        x_relaxed = npt.move(x_relaxed)
-
-    # equilibration
-    x_equil = x_relaxed
+    npt = construct_npt_move(lam)
     assert npt.lamb == lam
     trange = tqdm(range(n_equil_moves), desc="equilibration")
     for _ in trange:
         x_equil = npt.move(x_equil)
+    print("u(x_equil)", u(x_equil, lam))
 
     # production
     traj = [x_equil]
@@ -110,6 +124,40 @@ for lam in lambda_schedule:
     trajs.append(traj)
 
 # analyze
-# TODO: define u(x, lam)
-# TODO: define work(xs, from_lam, to_lam)
-# TODO: \sum_i BAR(w_F=work(xs[i], lam[i], lam[i+1]), w_R=work(xs[i+1], lam[i+1], lam[i]))
+# TODO: move this into fe module, if it's not there already
+Array = ArrayLike
+Traj = List[CoordsVelBox]
+Diagnostics = Dict
+
+
+def vec_u(xs: Traj, lam: float) -> Array:
+    return np.array([u(x, lam) for x in xs])
+
+
+def work(xs: Traj, from_lam: float, to_lam: float) -> Array:
+    return vec_u(xs, to_lam) - vec_u(xs, from_lam)
+
+
+def pair_bar(trajs: List[Traj], lambda_schedule: Array) -> Tuple[float, Diagnostics]:
+    assert len(trajs) == len(lambda_schedule)
+
+    works = []
+    estimates = []
+    for i in range(len(trajs) - 1):
+        w_F = work(trajs[i], lambda_schedule[i], lambda_schedule[i + 1])
+        w_R = work(trajs[i + 1], lambda_schedule[i + 1], lambda_schedule[i])
+
+        works.append((w_F, w_R))
+        df, ddf = BAR(w_F, w_R)
+        estimates.append((df, ddf))
+
+    delta_f = sum([df for (df, _) in estimates])
+
+    return delta_f, dict(works=works, estimates=estimates)
+
+
+delta_f, diagnostics = pair_bar(trajs, lambda_schedule)
+
+print(f"estimated free energy difference: {delta_f}")
+print("free energy difference estimated for each step:")
+print(diagnostics["estimates"])
