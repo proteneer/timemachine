ed5b99e5faa4e7598804b9b13ccfe894e6b361fd
Josh Fass <jfass@relaytx.com>
2022-01-27 13:12:37 -0800

    Extract cmd_args parse, save less by default

--- examples/run_smc.py
+++ examples/run_smc.py
@@ -7,64 +7,66 @@ import numpy as np
 
 from fe.free_energy_rabfe import construct_pre_optimized_absolute_lambda_schedule_solvent
 from md.smc import simple_smc, conditional_multinomial_resample
-
 from testsystems.biphenyl import construct_biphenyl_test_system
 
-if __name__ == "__main__":
+
+def parse_options():
     parser = argparse.ArgumentParser()
     parser.add_argument("--n_walkers", type=int, help="number of walkers", default=100)
     parser.add_argument("--n_windows", type=int, help="number of lambda windows", default=100)
     parser.add_argument("--n_md_steps", type=int, help="number of MD steps per move", default=1000)
     parser.add_argument("--resample_thresh", type=float, help="resample when fractional ESS < thresh", default=0.6)
+    parser.add_argument("--debug_mode", type=bool, help="save full trajectories", default=False)
+
     cmd_args = parser.parse_args()
-    print(cmd_args)
 
-    # SMC set up
-    n_walkers = cmd_args.n_walkers
-    n_windows = cmd_args.n_windows
-    resample_thresh = cmd_args.resample_thresh
-    n_md_steps = cmd_args.n_md_steps
-    config = dict(n_walkers=n_walkers, n_windows=n_windows, resample_thresh=resample_thresh, n_md_steps=n_md_steps)
+    if cmd_args.debug_mode:
+        n_frames = cmd_args.n_walkers * cmd_args.n_windows
+        print(f"Warning! This will take a lot of disk space! ({n_frames} simulation frames)")
 
-    potential_energy_fxn, mover, initial_samples = construct_biphenyl_test_system(n_steps=n_md_steps)
+    return cmd_args
 
-    sample_inds = np.random.choice(np.arange(len(initial_samples)), size=n_walkers)
+
+if __name__ == "__main__":
+    cmd_args = parse_options()
+
+    # SMC setup: define initial samples, lambdas schedule, propagate fxn, log_prob fxn, resample fxn
+    potential_energy_fxn, mover, initial_samples = construct_biphenyl_test_system(n_steps=cmd_args.n_md_steps)
+    sample_inds = np.random.choice(np.arange(len(initial_samples)), size=cmd_args.n_walkers)
     samples = [initial_samples[i] for i in sample_inds]
-    lambdas = construct_pre_optimized_absolute_lambda_schedule_solvent(n_windows)[::-1]
-    resample = partial(conditional_multinomial_resample, thresh=resample_thresh)
 
-    def advance(xlam):
-        x, lam = xlam
-        mover.lamb = lam
-        x_next = mover.move(x)
-
-        return x_next
-
-    def u(xlam):
-        x, lam = xlam
-        return potential_energy_fxn.u(x, lam)
+    lambdas = construct_pre_optimized_absolute_lambda_schedule_solvent(cmd_args.n_windows)[::-1]
 
     def propagate(xs, lam):
-        xlams = [(x, lam) for x in xs]
-        xs_next = map(advance, xlams)
+        mover.lamb = lam
+        xs_next = [mover.move(x, lam) for x in xs]
         return xs_next
 
     def log_prob(xs, lam):
-        xlams = [(x, lam) for x in xs]
-        u_s = np.array(map(u, xlams))
+        u_s = np.array([potential_energy_fxn.u(x, lam) for x in xs])
         return -u_s
 
-    smc_result = simple_smc(
-        samples,
-        lambdas,
-        propagate,
-        log_prob,
-        resample,
-    )
+    resample = partial(conditional_multinomial_resample, thresh=cmd_args.resample_thresh)
 
-    with open(f"smc_result_{datetime.now()}.pkl", "wb") as f:
-        dump((smc_result, config), f)
+    # run simulation
+    smc_result = simple_smc(samples, lambdas, propagate, log_prob, resample)
+
+    # save summary
+    uid = f"{datetime.now()}"
 
     traj, log_weights_traj, ancestry_traj, incremental_log_weights_traj = smc_result
 
-# TODO: use less disk space!
+    # by default, just save the final weighted samples, incremental log weight trajectory (and cmd_args)
+    summary = dict(
+        final_samples=traj[-1],
+        final_log_weights=log_weights_traj[-1],
+        ancestry_traj=ancestry_traj,
+        incremental_log_weights_traj=incremental_log_weights_traj,
+    )
+    with open(f"summary_smc_result_{uid}.pkl", "wb") as f:
+        dump((summary, cmd_args), f)
+
+    # optionally save trajectories
+    if cmd_args.debug_mode:
+        with open(f"full_smc_traj_{uid}.pkl", "wb") as f:
+            dump((traj, cmd_args), f)
