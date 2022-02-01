f1c657826f4853efb7bf43ce604d8a2cd9d8edc1
Josh Fass <jfass@relaytx.com>
2022-02-01 07:27:53 -0800

    Revert "Delete comment block in barostat"
    This reverts commit 40814af4bd89e135cfd5f258e093b111f9aaab4a.

--- timemachine/cpp/src/barostat.hpp
+++ timemachine/cpp/src/barostat.hpp
@@ -8,6 +8,17 @@
 
 namespace timemachine {
 
+//class Barostat {
+//
+//public:
+//
+//    virtual void inplace_move(
+//        double *d_x,
+//        double *d_box,
+//        const double lambda
+//    );
+//
+//};
 
 class MonteCarloBarostat {
 
