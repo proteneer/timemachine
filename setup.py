8e24ff7ec09ce627e3cb0024b0490254ca33fc5b
Matt Wittmann <mwittmann@relaytx.com>
2022-02-01 16:50:35 -0800

    Add initial setup.py

---
+++ setup.py
@@ -0,0 +1,60 @@
+"""setuptools based setup module.
+
+Adapted from https://github.com/pypa/sampleproject/blob/main/setup.py
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name="timemachine",
+    version="0.1.0",
+    description="A high-performance differentiable molecular dynamics, docking and optimization engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/proteneer/timemachine",
+    author="Relay Therapeutics",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="molecular dynamics",
+    packages=find_packages(),
+    python_requires=">=3.7",
+    install_requires=[
+        "bootstrapped",
+        "grpcio",
+        "grpcio-tools",
+        "hilbertcurve",
+        "jax",
+        "jaxlib",
+        "networkx",
+        "numpy",
+        "pymbar",
+        "pyyaml",
+        "scipy",
+    ],
+    extras_require={  # Optional
+        "dev": ["black==21.10b0", "pre-commit"],
+        "test": ["pytest", "pytest-cov"],
+    },
+    # package_data={
+    #     "sample": ["package_data.dat"],
+    # },
+    # entry_points={
+    #     "console_scripts": [
+    #         "sample=sample:main",
+    #     ],
+    # },
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/proteneer/timemachine/issues",
+        "Source": "https://github.com/proteneer/timemachine/",
+    },
+)
