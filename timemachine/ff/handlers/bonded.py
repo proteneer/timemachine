6768aa70fc99a50b4a637a517453125276486a40
Josh Fass <jfass@relaytx.com>
2022-02-08 20:58:48 -0800

    Apply patch for impropertorsion idxs
    https://github.com/proteneer/timemachine/commit/76a353e1dbbdeffcff30da4d8f8f0bc911355880

--- timemachine/ff/handlers/bonded.py
+++ timemachine/ff/handlers/bonded.py
@@ -213,4 +213,12 @@ class ImproperTorsionHandler(SerializableMixIn):
 
         param_idxs = np.array(param_idxs)
 
-        return params[param_idxs], np.array(improper_idxs, dtype=np.int32)
+        # if no matches found, return arrays that can still be concatenated as expected
+        if len(param_idxs) > 0:
+            assigned_params = params[param_idxs]
+            improper_idxs = np.array(improper_idxs, dtype=np.int32)
+        else:
+            assigned_params = np.zeros(shape=(0, params.shape[1]), dtype=params.dtype)
+            improper_idxs = np.zeros(shape=(0, 4), dtype=np.int32)
+
+        return assigned_params, improper_idxs
