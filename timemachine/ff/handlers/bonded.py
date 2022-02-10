a40c67c6bf62bfacd6ea270a517af92a535f47bf
Josh Fass <jfass@relaytx.com>
2022-02-10 11:52:07 -0500

    Fix no matches corner case for proper torsion handler

--- timemachine/ff/handlers/bonded.py
+++ timemachine/ff/handlers/bonded.py
@@ -135,7 +135,15 @@ class ProperTorsionHandler:
 
         scatter_idxs = np.array(scatter_idxs)
 
-        return params[scatter_idxs], np.repeat(torsion_idxs, repeats, axis=0).astype(np.int32)
+        # if no matches found, return arrays that can still be concatenated as expected
+        if len(param_idxs) > 0:
+            assigned_params = params[param_idxs]
+            proper_idxs = np.repeat(torsion_idxs, repeats, axis=0).astype(np.int32)
+        else:
+            assigned_params = params[[False] * len(params)]  # empty slice with same dtype, other dimensions
+            proper_idxs = np.zeros((0, 4), dtype=np.int32)
+
+        return assigned_params, proper_idxs
 
     def serialize(self):
         list_params = []
