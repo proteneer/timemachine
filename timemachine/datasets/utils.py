d6d0983a704052b11fb2458825b2f791efbbd786
Matt Wittmann <mwittmann@relaytx.com>
2022-02-14 11:07:43 -0800

    Fix importlib.resources usage
    importlib.resources.path is not intended to work with directories;
    this works coincidentally in Python 3.7 but will break in future
    versions. See https://bugs.python.org/issue44162

--- timemachine/datasets/utils.py
+++ timemachine/datasets/utils.py
@@ -5,7 +5,6 @@ from rdkit import Chem
 
 
 def fetch_freesolv() -> List[Chem.Mol]:
-    with resources.path("timemachine", "datasets") as datasets_path:
-        freesolv_path = str(datasets_path / "freesolv" / "freesolv.sdf")
-        supplier = Chem.SDMolSupplier(freesolv_path, removeHs=False)
-        return [mol for mol in supplier]
+    freesolv_path = resources.files(__package__) / "freesolv" / "freesolv.sdf"
+    supplier = Chem.SDMolSupplier(str(freesolv_path), removeHs=False)
+    return [mol for mol in supplier]
