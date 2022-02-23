112b4a0467f71110898adc17b72dc1991a9172c9
Matt Wittmann <mwittmann@relaytx.com>
2022-02-23 13:44:56 -0800

    Separate scale parameters for sigma, epsilon in generator

--- tests/nonbonded/parameter_interpolation.py
+++ tests/nonbonded/parameter_interpolation.py
@@ -2,7 +2,7 @@ import numpy as np
 import numpy.typing as npt
 
 
-def gen_params(params_initial: npt.NDArray, rng: np.random.Generator, dcharge=0.01, dloglj=0.1):
+def gen_params(params_initial: npt.NDArray, rng: np.random.Generator, dcharge=0.01, dlogsig=0.1, dlogeps=0.1):
     """Given an initial set of nonbonded parameters, generate random
     final parameters and return the concatenation of the initial and
     final parameters"""
@@ -13,8 +13,8 @@ def gen_params(params_initial: npt.NDArray, rng: np.random.Generator, dcharge=0.
     charge_final = charge_init + rng.normal(0, dcharge, size=(num_atoms,))
 
     # perturb LJ parameters in log space to avoid negative result
-    sig_final = np.where(sig_init, np.exp(np.log(sig_init) + rng.normal(0, dloglj, size=(num_atoms,))), 0)
-    eps_final = np.where(eps_init, np.exp(np.log(eps_init) + rng.normal(0, dloglj, size=(num_atoms,))), 0)
+    sig_final = np.where(sig_init, np.exp(np.log(sig_init) + rng.normal(0, dlogsig, size=(num_atoms,))), 0)
+    eps_final = np.where(eps_init, np.exp(np.log(eps_init) + rng.normal(0, dlogeps, size=(num_atoms,))), 0)
 
     params_final = np.stack((charge_final, sig_final, eps_final), axis=1)
 
