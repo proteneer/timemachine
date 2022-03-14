d8f67ffcf9d7e8feefe2ed7fe33297aeb6f4ba1e
Forrest York <forrest.york@gmail.com>
2022-03-14 10:18:36 -0600

    PR Feedback: Avoid double negative
    * Removes reference to cuda-memcheck as well

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,16 +10,14 @@ from timemachine.lib import custom_ops
 
 @pytest.fixture(autouse=True)
 def reset_cuda_device_after_test(request):
-    """Calls cudaDeviceReset() after each test.
+    """Calls cudaDeviceReset() after each test marked with memcheck.
 
-    This is needed for 'cuda-memcheck --leak-check full' to catch leaks"""
+    This is needed for 'compute-sanitizer --leak-check full' to catch leaks"""
 
     yield
 
     # If the test is not marked for memory tests, no need to reset device
-    if "memcheck" not in request.keywords:
-        return
-    # ensure that destructors are called before cudaDeviceReset()
-    gc.collect()
-
-    custom_ops.cuda_device_reset()
+    if "memcheck" in request.keywords:
+        # ensure that destructors are called before cudaDeviceReset()
+        gc.collect()
+        custom_ops.cuda_device_reset()
