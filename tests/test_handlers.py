proteneer/timemachine

commit 1ac56298ca05f64b612acae0379bae6797ac7a81

Extract bond charge correction functions (#571)
Breaks up AM1CCCHandler.static_parameterize into three pieces, and adds a test for each piece.

* compute_or_load_am1_charges(mol)
* bond_smirks_matches(mol, smirks_list)
* apply_bond_charge_corrections(initial_charges, bond_idxs, deltas)

Suggests that arbitrary other trainable functions could in principle be used to generate the inputs for apply_bond_charge_corrections.

tests/test_handlers.py
@@ -10,6 +10,7 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 from ff.handlers import nonbonded, bonded
 from ff.handlers.deserialize import deserialize_handlers
+from ff.charges import AM1CCC_CHARGES
 
 import functools
 
@@ -560,6 +561,90 @@ def test_am1_differences():
                     print(" ")
 
             assert 0
+
+
+def test_compute_or_load_am1_charges():
+    """Loop over test ligands, asserting that charges are stored in expected property and that the same charges are
+    returned on repeated calls"""
+
+    # get some molecules
+    cache_key = "AM1Cache"
+    suppl = Chem.SDMolSupplier("tests/data/ligands_40.sdf", removeHs=False)
+    all_mols = [mol for mol in suppl]
+    all_mols = all_mols[:5]  # truncate so that whole test is ~ 10 seconds
+
+    # don't expect AM1 cache yet
+    for mol in all_mols:
+        assert not mol.HasProp(cache_key)
+
+    # compute charges once
+    fresh_am1_charges = [nonbonded.compute_or_load_am1_charges(mol) for mol in all_mols]
+
+    # expect each mol to have AM1 cache now
+    for mol in all_mols:
+        assert mol.HasProp(cache_key)
+
+    # expect the same charges as the first time around
+    cached_am1_charges = [nonbonded.compute_or_load_am1_charges(mol) for mol in all_mols]
+    for (fresh, cached) in zip(fresh_am1_charges, cached_am1_charges):
+        np.testing.assert_array_equal(fresh, cached)
+
+
+def test_bond_smirks_matches():
+    """Loop over test ligands, asserting that
+    * returned indices are in bounds
+    * returned bonds are present in the mol"""
+    # get some molecules
+    suppl = Chem.SDMolSupplier("tests/data/ligands_40.sdf", removeHs=False)
+    all_mols = [mol for mol in suppl]
+
+    # get some bond smirks
+    smirks_list = [smirks for (smirks, param) in AM1CCC_CHARGES["patterns"]]
+
+    for mol in all_mols:
+        bond_idxs, type_idxs = nonbonded.bond_smirks_matches(mol, smirks_list)
+
+        # assert indices in bounds
+        assert (bond_idxs.min() >= 0) and (bond_idxs.max() < mol.GetNumAtoms())
+        assert (type_idxs.min() >= 0) and (type_idxs.max() < len(smirks_list))
+
+        # assert that bond_idxs are present in the mol
+        bonds = set()
+        for bond in mol.GetBonds():
+            a, b = bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()
+            bonds.add((a, b))
+            bonds.add((b, a))
+        for bond in bond_idxs:
+            assert tuple(bond) in bonds
+
+
+def test_apply_bond_charge_corrections():
+    """Assert that applying random bond charge corrections does not change net charge"""
+
+    n_random_tests = 100
+    np.random.seed(2022)
+
+    for _ in range(n_random_tests):
+        n_atoms = np.random.randint(10, 50)
+
+        # random initial charges with integral net charge
+        initial_net_charge = np.random.randint(-5, +6)
+        __initial_charges = np.random.randn(n_atoms)
+        _initial_charges = __initial_charges - np.mean(__initial_charges)
+        assert np.isclose(np.sum(_initial_charges), 0)
+        initial_charges = _initial_charges + (initial_net_charge / n_atoms)
+        assert np.isclose(np.sum(initial_charges), initial_net_charge, atol=1e-5), "initial net charge not integral"
+
+        # arbitrary: duplicates okay, reversals okay, symmetry not required
+        n_directed_bonds = np.random.randint(50, 100)
+        bonds = np.random.randint(0, n_atoms, size=(n_directed_bonds, 2))
+        deltas = np.random.randn(n_directed_bonds)
+
+        final_charges = nonbonded.apply_bond_charge_corrections(initial_charges, bonds, deltas)
+        final_net_charge = np.sum(final_charges)
+
+        assert (final_charges != initial_charges).any()
+        np.testing.assert_almost_equal(final_net_charge, initial_net_charge)
 
 
 def test_lennard_jones_handler():

ff/handlers/nonbonded.py
@@ -5,9 +5,11 @@ import jax
 import jax.numpy as jnp
 import networkx as nx
 import pickle
+from collections import Counter
 
 from rdkit import Chem
-from ff.handlers.utils import match_smirks, sort_tuple
+from ff.handlers.utils import sort_tuple, match_smirks as rd_match_smirks
+from ff.handlers.bcc_aromaticity import match_smirks as oe_match_smirks
 from ff.handlers.serialize import SerializableMixIn
 from ff.handlers.bcc_aromaticity import AromaticityModel
 
@@ -150,7 +152,7 @@ def generate_nonbonded_idxs(mol, smirks):
     N = mol.GetNumAtoms()
     param_idxs = np.zeros(N, dtype=np.int32)
     for p_idx, patt in enumerate(smirks):
-        matches = match_smirks(mol, patt)
+        matches = rd_match_smirks(mol, patt)
         for m in matches:
             param_idxs[m[0]] = p_idx
 
@@ -159,6 +161,109 @@ def generate_nonbonded_idxs(mol, smirks):
 
 def parameterize_ligand(params, param_idxs):
     return params[param_idxs]
+
+
+def compute_or_load_am1_charges(mol):
+    """Unless already cached in mol's "AM1Cache" property, use OpenEye to compute AM1 partial charges."""
+
+    # check for cache
+    cache_key = "AM1Cache"
+    if not mol.HasProp(cache_key):
+        # The charges returned by OEQuacPac is not deterministic across OS platforms. It is known
+        # to be an issue that the atom ordering modifies the return values as well. A follow up
+        # with OpenEye is in order
+        # https://github.com/openforcefield/openff-toolkit/issues/983
+        am1_charges = list(oe_assign_charges(mol, "AM1"))
+
+        mol.SetProp(cache_key, base64.b64encode(pickle.dumps(am1_charges)))
+
+    else:
+        am1_charges = pickle.loads(base64.b64decode(mol.GetProp(cache_key)))
+
+    return np.array(am1_charges)
+
+
+def bond_smirks_matches(mol, smirks_list):
+    """Return an array of ordered bonds and an array of their assigned types
+
+    Notes
+    -----
+    * Uses OpenEye for substructure searches
+    * Order within smirks_list matters
+        "First match wins."
+        For example, if bond (a,b) can be matched by smirks_list[2], smirks_list[5], ..., assign type 2
+    * Order within each smirks pattern matters
+        For example, "[#6:1]~[#1:2]" and "[#1:1]~[#6:2]" will match atom pairs in the opposite order
+    """
+    oemol = convert_to_oe(mol)
+    AromaticityModel.assign(oemol)
+
+    bond_idxs = []  # [B, 2]
+    type_idxs = []  # [B]
+
+    for type_idx, smirks in enumerate(smirks_list):
+        matches = oe_match_smirks(smirks, oemol)
+
+        for matched_indices in matches:
+            a, b = matched_indices[0], matched_indices[1]
+            forward_matched_bond = [a, b]
+            reverse_matched_bond = [b, a]
+
+            already_assigned = forward_matched_bond in bond_idxs or reverse_matched_bond in bond_idxs
+
+            if not already_assigned:
+                bond_idxs.append(forward_matched_bond)
+                type_idxs.append(type_idx)
+
+    return np.array(bond_idxs), np.array(type_idxs)
+
+
+def apply_bond_charge_corrections(initial_charges, bond_idxs, deltas):
+    """For an arbitrary collection of ordered bonds and associated increments `(a, b, delta)`,
+    update `charges` by `charges[a] += delta`, `charges[b] -= delta`
+
+    Notes
+    -----
+    * preserves sum(initial_charges) for arbitrary values of bond_idxs or deltas
+    * order within each row of bond_idxs is meaningful
+        `(..., bond_idxs, deltas)`
+        means the opposite of
+        `(..., bond_idxs[:, ::-1], deltas)`
+    * order within the first axis of bond_idxs, deltas is not meaningful
+        `(..., bond_idxs[perm], deltas[perm])`
+        means the same thing for any permutation `perm`
+    """
+
+    # apply bond charge corrections
+    incremented = ops.index_add(initial_charges, bond_idxs[:, 0], +deltas)
+    decremented = ops.index_add(incremented, bond_idxs[:, 1], -deltas)
+    final_charges = decremented
+
+    # make some safety assertions
+    assert bond_idxs.shape[1] == 2
+    assert len(deltas) == len(bond_idxs)
+
+    net_charge = jnp.sum(initial_charges)
+    net_charge_is_integral = jnp.isclose(net_charge, jnp.round(net_charge), atol=1e-5)
+
+    final_net_charge = jnp.sum(final_charges)
+    net_charge_is_unchanged = jnp.isclose(final_net_charge, net_charge, atol=1e-5)
+
+    assert net_charge_is_integral
+    assert net_charge_is_unchanged
+
+    # print some safety warnings
+    directed_bonds = Counter([tuple(b) for b in bond_idxs])
+    undirected_bonds = Counter([tuple(sorted(b)) for b in bond_idxs])
+
+    if max(directed_bonds.values()) > 1:
+        duplicates = [bond for (bond, count) in directed_bonds.items() if count > 1]
+        print(UserWarning(f"Duplicate directed bonds! {duplicates}"))
+    elif max(undirected_bonds.values()) > 1:
+        duplicates = [bond for (bond, count) in undirected_bonds.items() if count > 1]
+        print(UserWarning(f"Duplicate undirected bonds! {duplicates}"))
+
+    return final_charges
 
 
 class NonbondedHandler(SerializableMixIn):
@@ -342,74 +447,12 @@ class AM1CCCHandler(SerializableMixIn):
             molecule to be parameterized.
 
         """
-        # imported here for optional dependency
-        from openeye import oechem
+        am1_charges = compute_or_load_am1_charges(mol)
+        bond_idxs, type_idxs = bond_smirks_matches(mol, smirks)
 
-        oemol = convert_to_oe(mol)
-        AromaticityModel.assign(oemol)
+        deltas = params[type_idxs]
+        q_params = apply_bond_charge_corrections(am1_charges, bond_idxs, deltas)
 
-        # check for cache
-        cache_key = "AM1Cache"
-        if not mol.HasProp(cache_key):
-            # The charges returned by OEQuacPac is not deterministic across OS platforms. It is known
-            # to be an issue that the atom ordering modifies the return values as well. A follow up
-            # with OpenEye is in order
-            # https://github.com/openforcefield/openff-toolkit/issues/983
-            am1_charges = list(oe_assign_charges(mol, "AM1"))
-
-            mol.SetProp(cache_key, base64.b64encode(pickle.dumps(am1_charges)))
-
-        else:
-            am1_charges = pickle.loads(base64.b64decode(mol.GetProp(cache_key)))
-
-        bond_idxs = []
-        bond_idx_params = []
-
-        for index in range(len(smirks)):
-            smirk = smirks[index]
-            param = params[index]
-
-            substructure_search = oechem.OESubSearch(smirk)
-            substructure_search.SetMaxMatches(0)
-
-            matched_bonds = []
-            matches = []
-            for match in substructure_search.Match(oemol):
-
-                matched_indices = {
-                    atom_match.pattern.GetMapIdx() - 1: atom_match.target.GetIdx()
-                    for atom_match in match.GetAtoms()
-                    if atom_match.pattern.GetMapIdx() != 0
-                }
-
-                matches.append(matched_indices)
-
-            for matched_indices in matches:
-
-                forward_matched_bond = [matched_indices[0], matched_indices[1]]
-                reverse_matched_bond = [matched_indices[1], matched_indices[0]]
-
-                if (
-                    forward_matched_bond in matched_bonds
-                    or reverse_matched_bond in matched_bonds
-                    or forward_matched_bond in bond_idxs
-                    or reverse_matched_bond in bond_idxs
-                ):
-                    continue
-
-                matched_bonds.append(forward_matched_bond)
-                bond_idxs.append(forward_matched_bond)
-                bond_idx_params.append(index)
-
-        am1_charges = np.array(am1_charges)
-        bond_idxs = np.array(bond_idxs)
-        bond_idx_params = np.array(bond_idx_params)
-
-        deltas = params[bond_idx_params]
-        incremented = ops.index_add(am1_charges, bond_idxs[:, 0], deltas)
-        decremented = ops.index_add(incremented, bond_idxs[:, 1], -deltas)
-
-        q_params = decremented
         assert q_params.shape[0] == mol.GetNumAtoms()  # check that return shape is consistent with input mol
 
         return q_params

commit 55a2b41867ccec1afa3164e2101c152d12dee0fa

Fixes bug where AM1CCCHandler failed on charge molecules
* Bug introduced in 1ac56298ca05f64b612acae0379bae6797ac7a81

timemachine/ff/handlers/nonbonded.py
@@ -57,6 +57,11 @@ def oe_assign_charges(mol, charge_model="AM1BCCELF10"):
 
     partial_charges = np.array([atom.GetPartialCharge() for atom in oemol.GetAtoms()])
 
+    # Verify that the charges sum up to an integer
+    net_charge = np.sum(partial_charges)
+    net_charge_is_integral = np.isclose(net_charge, np.round(net_charge), atol=1e-5)
+    assert net_charge_is_integral, f"Charge is not an integer: {net_charge}"
+
     # https://github.com/proteneer/timemachine#forcefield-gotchas
     # "The charges have been multiplied by sqrt(ONE_4PI_EPS0) as an optimization."
     inlined_constant = np.sqrt(constants.ONE_4PI_EPS0)
@@ -202,7 +207,7 @@ def compute_or_load_bond_smirks_matches(mol, smirks_list):
 
 def apply_bond_charge_corrections(initial_charges, bond_idxs, deltas):
     """For an arbitrary collection of ordered bonds and associated increments `(a, b, delta)`,
-    update `charges` by `charges[a] += delta`, `charges[b] -= delta`
+    update `charges` by `charges[a] += delta`, `charges[b] -= delta`.
 
     Notes
     -----
@@ -226,12 +231,9 @@ def apply_bond_charge_corrections(initial_charges, bond_idxs, deltas):
     assert len(deltas) == len(bond_idxs)
 
     net_charge = jnp.sum(initial_charges)
-    net_charge_is_integral = jnp.isclose(net_charge, jnp.round(net_charge), atol=1e-5)
-
     final_net_charge = jnp.sum(final_charges)
     net_charge_is_unchanged = jnp.isclose(final_net_charge, net_charge, atol=1e-5)
 
-    assert net_charge_is_integral
     assert net_charge_is_unchanged
 
     # print some safety warnings

timemachine/ff/smirnoff_converter.py
@@ -170,12 +170,12 @@ if __name__ == "__main__":
                     sigma = 2.0 * rmin_half / (2.0 ** (1.0 / 6.0))
                 else:
                     sigma = parse_quantity(s.attributes["sigma"].value)
-                # Take sqrt of epislon to avoid singularity in backprop
+                # Take sqrt of epsilon to avoid singularity in backprop
                 params.append([patt, sigma, np.sqrt(epsilon)])
             vdws = {"patterns": params, "props": {}}
             for key, val in xmldoc.getElementsByTagName("vdW")[0].attributes.items():
                 if key == "cutoff":
-                    # we don't do cuttoffs.
+                    # we don't do cutoffs.
                     continue
                 elif "scale" in key:
                     val = float(val)

tests/test_handlers.py
@@ -10,6 +10,7 @@ import pytest
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+from timemachine.constants import ONE_4PI_EPS0
 from timemachine.ff.charges import AM1CCC_CHARGES
 from timemachine.ff.handlers import bonded, nonbonded
 from timemachine.ff.handlers.deserialize import deserialize_handlers
@@ -615,6 +616,30 @@ def test_compute_or_load_am1_charges():
     cached_am1_charges = [nonbonded.compute_or_load_am1_charges(mol) for mol in all_mols]
     for (fresh, cached) in zip(fresh_am1_charges, cached_am1_charges):
         np.testing.assert_array_equal(fresh, cached)
+
+def test_charging_compounds_with_non_zero_charge():
+    patterns = [
+        ["[#6a:1]:[#6a:2]", 0.0],
+        ["[#7X3ar5,#7X3+1,#7X3+0$(*-[#6X3$(*=[#7X3+1])]),$([#7X3](-[#8X1-1])=[#8X1]),$([#7X3](=[#8X1])=[#8X1]):1]-[#8X1,#8X2:2]", 0.2380991882939545],
+    ]
+
+    smirks = [x[0] for x in patterns]
+    params = np.array([x[1] for x in patterns])
+    props = None
+    am1h = nonbonded.AM1CCCHandler(smirks, params, props)
+
+    positive_mol = Chem.AddHs(Chem.MolFromSmiles("c1cc[nH+]cc1"))
+    AllChem.EmbedMolecule(positive_mol)
+
+    negative_mol = Chem.AddHs(Chem.MolFromSmiles("[N+](=O)([O-])[O-]"))
+    AllChem.EmbedMolecule(negative_mol)
+
+    es_params = am1h.parameterize(positive_mol)
+    np.testing.assert_almost_equal(np.sum(es_params / np.sqrt(ONE_4PI_EPS0)), 1.0, decimal=5)
+
+    es_params = am1h.parameterize(negative_mol)
+    np.testing.assert_almost_equal(np.sum(es_params) / np.sqrt(ONE_4PI_EPS0), -1.0, decimal=5)
+
 
 
 def test_compute_or_load_bond_smirks_matches():