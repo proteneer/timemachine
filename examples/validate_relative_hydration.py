0ddbe952ea98468b6b576554e34f6ebf19c15c1d
Josh Fass <jfass@relaytx.com>
2022-04-07 13:57:16 -0400

    Use optimized solvent decoupling protocol (#689)
    * Use optimized solvent decoupling protocol in RABFE
    * Use optimized solvent decoupling protocol in RHFE

--- examples/validate_relative_hydration.py
+++ examples/validate_relative_hydration.py
@@ -11,7 +11,7 @@ from rdkit import Chem
 
 from timemachine.fe import model_rabfe
 from timemachine.fe.free_energy_rabfe import (
-    construct_absolute_lambda_schedule_solvent,
+    construct_pre_optimized_absolute_lambda_schedule_solvent,
     construct_relative_lambda_schedule,
     get_romol_conf,
     setup_relative_restraints_by_distance,
@@ -68,7 +68,7 @@ if __name__ == "__main__":
 
     dataset = Dataset(mols)
 
-    absolute_solvent_schedule = construct_absolute_lambda_schedule_solvent(cmd_args.num_windows)
+    absolute_solvent_schedule = construct_pre_optimized_absolute_lambda_schedule_solvent(cmd_args.num_windows)
     relative_solvent_schedule = construct_relative_lambda_schedule(cmd_args.num_windows - 1)
     solvent_system, solvent_coords, solvent_box, solvent_topology = builders.build_water_system(4.0)
 
