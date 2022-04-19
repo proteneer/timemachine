d153d7fcae003f2602db8da18bfb3fec1d4c0efa
Josh Fass <jfass@relaytx.com>
2022-04-19 14:28:39 -0400

    Test sequential_monte_carlo

--- tests/test_smc.py
+++ tests/test_smc.py
@@ -1,6 +1,5 @@
-from cmath import log
 from timemachine.md.smc import (
-    #sequential_monte_carlo,
+    sequential_monte_carlo,
     Resampler,
     effective_sample_size,
     null_resample,
@@ -13,6 +12,10 @@ from timemachine.md.smc import (
 import numpy as np
 from scipy.special import logsumexp
 import pytest
+from jax import jit, vmap
+
+from timemachine.fe.reweighting import one_sided_exp
+from timemachine.testsystems.gaussian1d import make_gaussian_testsystem
 
 def generate_log_weights(n):
     """sample log_weights ~ N(mu=mean, sigma=exp(log_stddev))
@@ -125,3 +128,73 @@ def test_fractional_effective_sample_size():
         fractional_ess = fractional_effective_sample_size(log_weights)
 
         np.testing.assert_almost_equal(fractional_ess, ess / n_particles)
+
+
+@pytest.mark.parametrize("resampling_fxn", [null_resample, multinomial_resample, conditional_multinomial_resample])
+def test_sequential_monte_carlo(resampling_fxn: Resampler):
+    """Run SMC with the desired resampling_fxn on a Gaussian 1D test system, and assert that
+    * running estimates of the free energy as a fxn of lambda match analytical free energies
+    """
+    np.random.seed(2022)
+
+    # construct test system
+    target_mean, target_log_sigma = 1, -2
+    params = np.array([target_mean, target_log_sigma])  # TODO: randomize / parameterize
+    n_particles = 10000
+    n_windows = 100
+    lambdas = np.linspace(0, 1, n_windows)
+    u_fxn, _, sample, reduced_free_energy = make_gaussian_testsystem()
+    ref_free_energies = np.array([reduced_free_energy(lam, params) for lam in lambdas])
+    ref_delta_fs = ref_free_energies - ref_free_energies[0]
+
+    # prepare inputs for SMC
+    samples = sample(0, params, n_particles).flatten()
+    assert samples.shape == (n_particles,)
+
+    vec_u_fxn = jit(vmap(u_fxn, in_axes=(0, None, None)))
+
+    def log_prob(xs, lam):
+        xs = np.array(xs)
+        return -vec_u_fxn(xs, lam, params)
+
+    def propagate(xs, lam):
+        """random-walk metropolis with N(0, proposal_scale(lam)) proposals"""
+        n = len(xs)
+        xs = np.array(xs)
+        assert xs.shape == (n,)
+
+        proposal_scale = np.exp(lam * target_log_sigma)  # to match annealed_gaussian_def  # TODO: extract
+        proposals = xs + proposal_scale * np.random.randn(n)
+        assert proposals.shape == xs.shape
+
+        # metropolis
+        log_probs_before = log_prob(xs, lam)
+        log_probs_after = log_prob(proposals, lam)
+        log_accept_probs = np.minimum(log_probs_after - log_probs_before, 0)
+        accept_probs = np.exp(log_accept_probs)
+        assert accept_probs.shape == (n,)
+
+        # accept / reject
+        test_vals = np.random.rand(n)
+        accept_mask = accept_probs >= test_vals
+        updated = np.where(accept_mask, proposals, xs)
+        assert updated.shape == xs.shape
+        
+        return updated
+
+    # apply SMC
+    result_dict = sequential_monte_carlo(samples, lambdas, propagate, log_prob, resampling_fxn)
+    
+    # compute running delta_f estimates
+    log_weights_traj = result_dict['log_weights_traj']
+    assert log_weights_traj.shape == (n_windows, n_particles)
+    running_estimates = np.array([one_sided_exp(-log_weights) for log_weights in log_weights_traj])
+
+    # assert all running estimates are within ~ 0.1 kT of reference
+    np.testing.assert_array_almost_equal(running_estimates, ref_delta_fs, decimal=1)
+
+    # assert this isn't a "no-op" test
+    constant_predictions = np.ones(n_windows) * np.mean(ref_delta_fs)
+    mse_vs_smc_estimates = np.mean((ref_delta_fs - running_estimates)**2)
+    mse_vs_constant = np.mean((ref_delta_fs - constant_predictions)**2)
+    assert mse_vs_constant / mse_vs_smc_estimates > 1000
