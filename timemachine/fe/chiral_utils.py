a34d448926b3b1468df7ba6278c90221eddf35e8
Yutong Zhao <proteneer@gmail.com>
2022-06-10 18:22:23 -0400

    Test chiral utils, find chiral_bonds and find chiral_atoms
    Remove chiral tag for bonds
    
    Add chiral restraints documentation

--- timemachine/fe/chiral_utils.py
+++ timemachine/fe/chiral_utils.py
@@ -1,6 +1,7 @@
 import itertools
 
 import numpy as np
+from rdkit import Chem
 
 from timemachine.potentials.chiral_restraints import pyramidal_volume, torsion_volume
 
@@ -9,6 +10,24 @@ def setup_chiral_atom_restraints(mol, conf, a_idx):
     """
     Setup chiral atom restraints for the molecule at a_idx by inspecting the
     given geometry.
+
+    Parameters
+    ----------
+    mol: Chem.Mol
+        input molecule
+
+    conf: np.ndarray (N,3)
+        conformation of the molecule
+
+    a_idx: int
+        Which atom to setup restraints for
+
+    Returns
+    -------
+    list of 4-tuple
+        Has length N choose 3, where N is the number of neighbors
+        (c0, i0, j0, k0), (c0, i1, j1, k1), ...
+
     """
     nbs = mol.GetAtomWithIdx(a_idx).GetNeighbors()
     restr_idxs = []
@@ -27,7 +46,29 @@ def setup_chiral_atom_restraints(mol, conf, a_idx):
 def setup_chiral_bond_restraints(mol, conf, src_idx, dst_idx):
     """
     Setup chiral bond restraints for the molecule at a_idx by inspecting the
-    given geometry
+    given geometry.
+
+    Parameters
+    ----------
+    mol: Chem.Mol
+        input molecule
+
+    conf: np.ndarray (N,3)
+        conformation of the molecule
+
+    src_idx: int
+        Which starting atom of the bond to setup restraints for
+
+    dst_idx: int
+        Which ending atom of the bond to setup restraints for
+
+    Returns
+    -------
+    List of 4-tuple
+        Returns up to 4 chiral volumes based on the torsion of the form:
+        (i_0, src_idx, dst_idx, l_0), (i_1, src_idx, dst_idx, l_1), ...
+
+        Note that i_j may not be necessarily less than l_j
     """
     src_nbs = [a.GetIdx() for a in mol.GetAtomWithIdx(src_idx).GetNeighbors()]
     dst_nbs = [a.GetIdx() for a in mol.GetAtomWithIdx(dst_idx).GetNeighbors()]
@@ -55,3 +96,70 @@ def setup_chiral_bond_restraints(mol, conf, src_idx, dst_idx):
                 signs.append(-1)
 
     return np.array(restr_idxs), np.array(signs)
+
+
+def find_chiral_atoms(mol):
+    """
+    Find chiral atoms in a molecule. Note that an atom is chiral if it has an non-invertible
+    energy barrier. Even a center like methane is considered chiral.
+
+    Parameters
+    ----------
+    mol: Chem.Mol
+        input molecule
+
+    Returns
+    -------
+    set of int
+        Chiral atoms
+
+    """
+    # these should be mutually exclusive, but if any pattern is hit then the results
+    # are accumulated to a set
+    chiral_patterns = [
+        "[X4:1]",  # any tetrahedral atom
+        "[#16X3,#15X3:1]",  # trivalent sulfur, phosphorous are assumed to be non-invertible
+        "[#7X3:1](~[R])(~[R])~[R]",  # nitrogen directly bonded to three ring atoms
+    ]
+
+    chiral_atoms = set()
+    for patt in chiral_patterns:
+        query_mol = Chem.MolFromSmarts(patt)
+        assert query_mol is not None
+        for match in mol.GetSubstructMatches(query_mol):
+            chiral_atoms.add(match[0])
+
+    return chiral_atoms
+
+
+def find_chiral_bonds(mol):
+    """
+    Find chiral bonds in a molecule. Current limited to double bonds and amides. Similarly,
+    a bond is considered chiral if it has an extremely high rotational barrier that would
+    be typically kinetically inaccessible.
+
+    Parameters
+    ----------
+    mol: Chem.Mol
+        input molecule
+
+    Returns
+    -------
+    set of 2-tuple
+        Chiral bonds
+
+    """
+
+    chiral_patterns = [
+        "[X2,X3:1]=[X2,X3:2]",  # all double bonds with two or three neighbors,
+        "[NX3,NX2:1][CX3:2](=[OX1])",  # amide bond
+    ]
+
+    chiral_bonds = set()
+    for patt in chiral_patterns:
+        query_mol = Chem.MolFromSmarts(patt)
+        assert query_mol is not None
+        for match in mol.GetSubstructMatches(query_mol):
+            chiral_bonds.add(tuple(sorted([match[0], match[1]])))
+
+    return chiral_bonds
