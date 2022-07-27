e3a6d1f8d023fb44e46656ae1b6d27689dc03944
Josh Fass <jfass@relaytx.com>
2022-07-27 12:35:33 -0400

    Test reversibility using custom_ops potentials

--- tests/test_reference_velocity_verlet_integrator.py
+++ tests/test_reference_velocity_verlet_integrator.py
@@ -1,30 +1,67 @@
+from functools import partial
+
 import numpy as np
 from jax import config, grad, jit
 from jax import numpy as jnp
 
 config.update("jax_enable_x64", True)
 
-
 from timemachine.integrator import VelocityVerletIntegrator
+from timemachine.testsystems.relative import hif2a_ligand_pair
 
 
-def assert_reversible(x0, v0, update_fxn):
+def assert_reversible(x0, v0, update_fxn, atol=1e-8):
+    """assert that
+    * F(I(x0, v0))      != (x0, v0)
+    * F(I(F(I(x0, v0))) == (x0, v0)"""
+    close = partial(np.allclose, atol=atol)
+
+    # integrate, flip v, integrate, flip v
     x1, v1 = update_fxn(x0, v0)
-    x0_, v0_ = update_fxn(x1, -v1)
+    x0_, _v0_ = update_fxn(x1, -v1)
+    v0_ = -_v0_
 
-    assert np.isclose(x0_, x0).all()
-    assert np.isclose(-v0_, v0).all()
+    # assert we got back where we started
+    assert close(x0_, x0), f"max(abs(x0 - x0_)) = {np.max(np.abs(x0 - x0_))}"
+    assert close(v0_, v0), f"max(abs(v0 - v0_)) = {np.max(np.abs(v0 - v0_))}"
 
     # also assert this is not a no-op
-    assert not np.isclose(x1, x0).all()
+    assert (not close(x1, x0)) and (not close(v1, v0)), "update_fxn didn't do anything!"
+
+
+def assert_reversibility_using_step_implementations(intg, x0, v0, n_steps=1000, atol=1e-8):
+    """assert reversibility .step and .multiple_steps implementations"""
+
+    # check step implementation
+    def step_update(x, v):
+        for t in range(n_steps):
+            x, v = intg.step(x, v)
+        return x, v
+
+    assert_reversible(x0, v0, step_update, atol=atol)
+
+    # check multiple_steps implementation
+    def multiple_steps_update(x, v):
+        xs, vs = intg.multiple_steps(x, v, n_steps)
+        return xs[-1], vs[-1]
+
+    assert_reversible(x0, v0, multiple_steps_update, atol=atol)
 
 
 def test_reversibility_with_jax_potentials():
+    """on a simple jax-transformable potential (quartic oscillators)
+    with randomized parameters and initial conditions
+    (n oscillators, masses, dt, x0, v0)
+    assert all 3 update functions
+    (public: .step and .multiple_steps, private: ._update_via_fori_loop)
+    are reversible"""
+
     np.random.seed(2022)
 
     def U(x):
         return jnp.sum(x ** 4)
 
+    @jit
     def force(x):
         return -grad(U)(x)
 
@@ -37,24 +74,48 @@ def test_reversibility_with_jax_potentials():
 
         intg = VelocityVerletIntegrator(force, masses, dt)
 
-        # check jax.lax.fori_loop implementation
+        # check "public" .step and .multiple_steps implementations
+        assert_reversibility_using_step_implementations(intg, x0, v0, n_steps)
+
+        # also check "private" jax.lax.fori_loop implementation
         @jit
         def jax_update(x, v):
             return intg._update_via_fori_loop(x, v, n_steps)
 
-        assert_reversible(x0, v0, jax_update)
+        assert_reversible(x0, v0, jax_update, atol=1e-8)
 
-        # check step implementation
-        def step_update(x, v):
-            for t in range(n_steps):
-                x, v = intg.step(x, v)
-            return x, v
 
-        assert_reversible(x0, v0, step_update)
+def test_reversibility_with_custom_ops_potentials():
+    """Check reversibility of "public" .step and .multiple_steps implementations when `force_fxn`
+    is a custom_op potential"""
+    np.random.seed(2022)
 
-        # check multiple_steps implementation
-        def multiple_steps_update(x, v):
-            xs, vs = intg.multiple_steps(x, v, n_steps)
-            return xs[-1], vs[-1]
+    # define a Python force fxn that calls custom_ops
+    rfe = hif2a_ligand_pair
+    unbound_potentials, sys_params, masses = rfe.prepare_vacuum_edge(rfe.ff.get_ordered_params())
+    coords = rfe.prepare_combined_coords()
+    bound_potentials = [
+        ubp.bind(params).bound_impl(np.float32) for (ubp, params) in zip(unbound_potentials, sys_params)
+    ]
+    box = 100 * np.eye(3)
 
-        assert_reversible(x0, v0, multiple_steps_update)
+    def force(coords):
+        du_dxs = np.array([bp.execute(coords, box, 0.5)[0] for bp in bound_potentials])
+        return -np.sum(du_dxs, 0)
+
+    dt = 1.5e-3
+    intg = VelocityVerletIntegrator(force, masses, dt)
+
+    x0 = np.array(coords)
+
+    for n_steps in [1, 10, 100, 500, 1000]:  # , 10000]:
+        v0 = np.random.randn(*coords.shape)
+
+        # check "public" .step and .multiple_steps implementations
+        assert_reversibility_using_step_implementations(intg, x0, v0, n_steps, atol=1e-10)
+
+    # TODO: possibly investigate why n_steps=10000 fails
+    # assert_reversibility_using_step_implementations(intg, x0, v0, n_steps=10000, atol=0.1)
+    # * also fails with reduced dt = 1.0e-3
+    # * passes with greatly reduced dt = 1.0e-4
+    # TODO: possibly replace with SummedPotential?
