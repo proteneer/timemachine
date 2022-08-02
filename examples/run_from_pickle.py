60b4dbe312433cb0fcc53a68d949a69f464e03ee
Yutong Zhao <proteneer@gmail.com>
2022-08-02 12:00:02 -0400

    WIP

--- examples/run_from_pickle.py
+++ examples/run_from_pickle.py
@@ -16,10 +16,14 @@ def read_from_args():
 
     print("Processing pickle: ", args.pickle_path)
 
-    with open(args.pickle_path) as fh:
+    with open(args.pickle_path, "rb") as fh:
         (meta, solvent_res, solvent_top, complex_res, complex_top) = pickle.load(fh)
+        print("running solvent")
         for initial_state in solvent_res.initial_states:
             all_coords, all_boxes = rbfe.sample(initial_state, solvent_res.protocol)
+        print("running complex")
+        for initial_state in complex_res.initial_states:
+            all_coords, all_boxes = rbfe.sample(initial_state, complex_res.protocol)
 
 
 if __name__ == "__main__":
