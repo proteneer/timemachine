88716a01655b83f9d354c24f61113110b89bcd10
Forrest York <forrest.york@gmail.com>
2022-08-10 08:40:52 -0600

    PR Feedback: Improve velocity verlet tests
    * Verifies reversibility of multiple_steps_U
    * Uses np.testing assertions
    * Completes sentence
    * Uses non-equilibrium lambda schedule, does accumulate more error

--- tests/test_velocity_verlet_integrator.py
+++ tests/test_velocity_verlet_integrator.py
@@ -16,66 +16,75 @@ def setup_velocity_verlet(bps, x0, box, dt, masses):
     return intg, context
 
 
-def assert_reversible(x0, v0, update_fxn, atol=1e-10):
+def assert_reversible(x0, v0, update_fxn, lambdas, atol=1e-10):
     """Define a fxn self_inverse as composition of flip_velocities and update_fxn,
     then assert that
     * self_inverse is its own inverse
     * self_inverse is not trivial (aka not the identity function)
     """
 
-    def self_inverse(x, v):
+    def self_inverse(x, v, lamb_sched):
         """integrate forward in time, flip v
         (expected to be an "involution" i.e. its own inverse)"""
-        x_next, v_next = update_fxn(x, v)
-        return x_next, -v_next
-
-    close = partial(np.allclose, atol=atol)
+        x_next, v_next = update_fxn(x, v, lamb_sched)
+        return x_next, -v_next, lamb_sched[::-1]
 
     # assert "self_inverse" is really its own inverse
-    x1, v1 = self_inverse(x0, v0)
-    x0_, v0_ = self_inverse(x1, v1)
+    x1, v1, rev_lamb = self_inverse(x0, v0, lambdas)
+    x0_, v0_, _ = self_inverse(x1, v1, rev_lamb)
 
-    assert close(x0_, x0), f"max(abs(x0 - x0_)) = {np.max(np.abs(x0 - x0_))}"
-    assert close(v0_, v0), f"max(abs(v0 - v0_)) = {np.max(np.abs(v0 - v0_))}"
+    np.testing.assert_almost_equal(x0_, x0, decimal=10)
+    np.testing.assert_almost_equal(v0_, v0, decimal=10)
 
+    close = partial(np.allclose, atol=atol)
     # also assert this is not a no-op
     assert (not close(x1, x0)) and (not close(v1, v0))
 
 
-def assert_reversibility_using_step_implementations(context, n_steps=1000, atol=1e-10):
+def assert_reversibility_using_step_implementations(context, schedule, atol=1e-10):
     """Assert reversibility of .step and .multiple_steps implementations"""
 
     x0 = context.get_x_t()
     v0 = context.get_v_t()
 
     # check step implementation
-    def step_update(x, v, lamb=0.0):
+    def step_update(x, v, lamb_sched):
         context.set_x_t(x)
         context.set_v_t(v)
 
-        for t in range(n_steps):
+        for lamb in lamb_sched:
             context.step(lamb)
         # Must call finalize in case of using step
-        context.finalize(lamb)
+        context.finalize(lamb_sched[-1])
         x = context.get_x_t()
         v = context.get_v_t()
         return x, v
 
-    assert_reversible(x0, v0, step_update, atol=atol)
+    assert_reversible(x0, v0, step_update, schedule, atol=atol)
 
     # check multiple_steps implementation
-    def multiple_steps_update(x, v, lamb=0.0):
+    def multiple_steps_update(x, v, lamb_sched):
         context.set_x_t(x)
         context.set_v_t(v)
-        _, xs, _ = context.multiple_steps(np.ones(n_steps) * lamb)
+        _, xs, _ = context.multiple_steps(lamb_sched)
         v = context.get_v_t()
         return xs[-1], v
 
-    assert_reversible(x0, v0, multiple_steps_update, atol=atol)
+    assert_reversible(x0, v0, multiple_steps_update, schedule, atol=atol)
+
+    def multiple_steps_U_update(x, v, lamb_sched):
+        # Doesn't use the lamb sched, as multiple_steps_U is always run as a equilibrium simulation
+        context.set_x_t(x)
+        context.set_v_t(v)
+        _, xs, _ = context.multiple_steps_U(0.0, len(lamb_sched), [], 0, 0)
+        v = context.get_v_t()
+        return xs[-1], v
+
+    assert_reversible(x0, v0, multiple_steps_U_update, schedule, atol=atol)
 
 
 def test_reversibility():
-    """Check reversibility of "public" .step and .multiple_steps implementations when constructing a Context with"""
+    """Check reversibility of "public" .step and .multiple_steps implementations for a Context using the VelocityVerlet integrator"""
 
     np.random.seed(2022)
 
@@ -90,14 +99,15 @@ def test_reversibility():
 
     dt = 1.5e-3
 
-    # Is not infinitely reversible, will fail after 5000 steps due to accumulation of coords/velos in float
-    for n_steps in [1, 10, 100, 500, 1000, 3000]:
+    # Is not infinitely reversible, will fail after 3000 steps due to accumulation of coords/velos in floating point
+    for n_steps in [1, 10, 100, 500, 1000, 2000]:
+        lamb_sched = np.linspace(0, 1, n_steps)
         v0 = np.random.randn(*coords.shape)
         intg, ctxt = setup_velocity_verlet(bound_potentials, coords, box, dt, masses)  # noqa
         ctxt.set_v_t(v0)
 
         # check "public" .step and .multiple_steps implementations
-        assert_reversibility_using_step_implementations(ctxt, n_steps, atol=1e-10)
+        assert_reversibility_using_step_implementations(ctxt, lamb_sched, atol=1e-10)
 
 
 def test_matches_reference():
