ebfc6db51d7e8e4ed75f078ad5b37e97c75e81a9
Josh Fass <jfass@relaytx.com>
2022-08-19 14:27:35 -0400

    Clean up docstrings

--- timemachine/maps/estimators.py
+++ timemachine/maps/estimators.py
@@ -1,3 +1,5 @@
+"""Utilities for computing mapped works or mapped u_kn matrix, incorporating invertible map_fxns"""
+
 import numpy as np
 from pymbar.utils import kln_to_kn  # TODO: should this be "vendored"?
 
@@ -5,20 +7,29 @@ __all__ = ["compute_mapped_reduced_work", "compute_mapped_u_kn"]
 
 
 def compute_mapped_reduced_work(src_samples, src_reduced_energy_fxn, dst_reduced_energy_fxn, map_fxn):
-    """minus log impportance weight,including a change of variables defined by map_fxn"""
-    mapped_samples, logdetjacs = map_fxn(src_samples)
+    """Compute minus log importance weights, including a change of variables defined by map_fxn
+
+    References
+    ----------
+    [Jarzynski, 2002] Targeted free energy perturbation
+        https://journals.aps.org/pre/abstract/10.1103/PhysRevE.65.046122
+    """
 
     # compare with version without map_fxn
     # return dst_reduced_energy_fxn(src_samples) - src_reduced_energy_fxn(src_samples)
 
+    mapped_samples, logdetjacs = map_fxn(src_samples)
     return dst_reduced_energy_fxn(mapped_samples) - src_reduced_energy_fxn(src_samples) - logdetjacs
 
 
 def compute_mapped_u_kn(sample_lists, reduced_energy_fxns, map_fxns):
-    """evaluate every sample from state i in every state j,
-    incorporating a change of variables defined by map_fxns[i, j]
+    """Evaluate every sample from state i in every state j,
+    incorporating user-defined change of variables supplied in map_fxns[i, j]
 
-    [Paliwal, Shirts, 2013] https://aip.scitation.org/doi/abs/10.1063/1.4801332
+    References
+    ----------
+    [Paliwal, Shirts, 2013] Multistate reweighting and configuration mapping ...
+        https://aip.scitation.org/doi/abs/10.1063/1.4801332
     """
 
     K = len(sample_lists)
@@ -32,12 +43,12 @@ def compute_mapped_u_kn(sample_lists, reduced_energy_fxns, map_fxns):
 
         for l in range(K):
             u_l = reduced_energy_fxns[l]
-            map_k_to_l = map_fxns[k, l]
-
-            xs_mapped, logdetjacs = map_k_to_l(xs_k)
 
             # compare with version without maps
             # u_kln[k, l, :N_k[k]] = u_l(xs_k)
+
+            map_k_to_l = map_fxns[k, l]
+            xs_mapped, logdetjacs = map_k_to_l(xs_k)
             u_kln[k, l, : N_k[k]] = u_l(xs_mapped) - logdetjacs
 
     u_kn = kln_to_kn(u_kln, N_k)
