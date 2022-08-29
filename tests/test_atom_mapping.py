8a27bc61a0ecf7c09c7907af1fda60a12f164dbb
Josh Fass <jfass@relaytx.com>
2022-08-29 10:46:07 -0400

    Delete unused code in atom_mapping.py

--- tests/test_atom_mapping.py
+++ tests/test_atom_mapping.py
@@ -1,16 +1,4 @@
-# TODO: test mcs atom mapping
-# TODO: move run-time distance check here?
-
-# TODO: test distance atom mapping
-
-from timemachine.fe.atom_mapping import (
-    compute_all_pairs_mcs,
-    compute_transformation_size_matrix,
-    get_core_by_geometry,
-    get_core_by_mcs,
-    mcs_map,
-    transformation_size,
-)
+from timemachine.fe.atom_mapping import get_core_by_geometry, get_core_by_mcs, mcs_map
 from timemachine.testsystems.relative import hif2a_ligand_pair
 
 
@@ -18,27 +6,6 @@ def test_mcs_map():
     mcs_result = mcs_map(hif2a_ligand_pair.mol_a, hif2a_ligand_pair.mol_b, threshold=0.5)
     assert mcs_result.queryMol is not None
     assert mcs_result.numAtoms > 1
-
-
-def test_transformation_size():
-    assert transformation_size(n_A=5, n_B=5, n_MCS=5) == 0
-    assert transformation_size(n_A=5, n_B=5, n_MCS=0) > 0
-
-
-def test_compute_all_pairs_mcs():
-    mols = hif2a_ligand_pair.mol_a, hif2a_ligand_pair.mol_b
-    mcs_s = compute_all_pairs_mcs(mols)
-    assert mcs_s.shape == (len(mols), len(mols))
-
-
-def test_compute_transformation_size_matrix():
-    mols = hif2a_ligand_pair.mol_a, hif2a_ligand_pair.mol_b
-    mcs_s = compute_all_pairs_mcs(mols)
-
-    transformation_sizes = compute_transformation_size_matrix(mols, mcs_s)
-
-    assert (transformation_sizes >= 0).all()
-    assert transformation_sizes.shape == (len(mols), len(mols))
 
 
 def test_get_core_by_mcs():
--- timemachine/fe/atom_mapping.py
+++ timemachine/fe/atom_mapping.py
@@ -5,7 +5,7 @@ from rdkit import Chem
 from rdkit.Chem import rdFMCS
 
 from timemachine.fe.topology import AtomMappingError
-from timemachine.fe.utils import core_from_distances, simple_geometry_mapping
+from timemachine.fe.utils import simple_geometry_mapping
 
 
 class CompareDist(rdFMCS.MCSAtomCompare):
@@ -85,67 +85,6 @@ def mcs_map_graph_only_complete_rings(a, b, timeout: int = 3600, smarts: Optiona
         atomCompare=Chem.rdFMCS.AtomCompare.CompareAny,
         bondCompare=Chem.rdFMCS.BondCompare.CompareAny,
     )
-
-
-def transformation_size(n_A, n_B, n_MCS):
-    """Heuristic size of transformation in terms of
-        the number of atoms in A, B, and MCS(A, B)
-
-    Notes
-    -----
-    * YTZ suggests (2021/01/26)
-        (n_A + n_B) - n_MCS
-        which is size-extensive
-
-    * JF modified (2021/01/27)
-        (n_A - n_MCS) + (n_B - n_MCS)
-        which is 0 when A = B
-
-    * Alternatives considered but not tried:
-        * min(n_A, n_B) - n_MCS
-        * max(n_A, n_B) - n_MCS
-        * 1.0 - (n_MCS / min(n_A, n_B))
-    """
-    return (n_A + n_B) - 2 * n_MCS
-
-
-def compute_all_pairs_mcs(mols, threshold: float = 0.5):
-    """Generate square matrix of MCS(mols[i], mols[j]) for all pairs i,j
-
-    Parameters
-    ----------
-    mols : iterable of RDKit Mols
-
-    Returns
-    -------
-    mcs_s : numpy.ndarray of rdkit.Chem.rdFMCS.MCSResult's, of shape (len(mols), len(mols))
-
-    Notes
-    -----
-    TODO: mcs_map should be symmetric, so only have to do the upper triangle of this matrix
-    """
-    mcs_s = np.zeros((len(mols), len(mols)), dtype=object)
-
-    for i in range(len(mols)):
-        for j in range(i, len(mols)):
-            mapped = mcs_map(mols[i], mols[j], threshold=threshold)
-            mcs_s[i, j] = mapped
-            mcs_s[j, i] = mapped
-    return mcs_s
-
-
-def compute_transformation_size_matrix(mols, mcs_s):
-    N = len(mols)
-    transformation_sizes = np.zeros((N, N))
-    for i in range(N):
-        for j in range(i, N):
-            n_i = mols[i].GetNumAtoms()
-            n_j = mols[j].GetNumAtoms()
-            n_mcs = mcs_s[i, j].numAtoms
-            transforms = transformation_size(n_i, n_j, n_mcs)
-            transformation_sizes[i, j] = transforms
-            transformation_sizes[j, i] = transforms
-    return transformation_sizes
 
 
 # 4. Construct and serialize the relative transformations
@@ -230,17 +169,6 @@ def _assert_core_reasonableness(mol_a, mol_b, core):
     assert len(set(core[:, 1])) == len(core)
 
 
-def get_core_by_matching(mol_a, mol_b, threshold=1.0):
-    """Only allow to map a pair of atoms together if their conformer coordinates are within threshold.
-
-    Of the allowable core mappings, return the maximum-weight matching of maximal-cardinality,
-        where weight(i,j) = threshold - distance(mol_a[i], mol_b[j])
-    """
-    core = core_from_distances(mol_a, mol_b, threshold)
-    _assert_core_reasonableness(mol_a, mol_b, core)
-    return core
-
-
 def get_core_by_geometry(mol_a, mol_b, threshold=0.5):
     """Only allow to map a pair of atoms together if their conformer coordinates are within threshold.
 
@@ -250,36 +178,3 @@ def get_core_by_geometry(mol_a, mol_b, threshold=0.5):
     core = simple_geometry_mapping(mol_a, mol_b, threshold)
     _assert_core_reasonableness(mol_a, mol_b, core)
     return core
-
-
-def _get_unique_match(mol, core):
-    matches = mol.GetSubstructMatches(core)
-    assert len(matches) == 1
-    return matches[0]
-
-
-def get_core_by_smarts(mol_a, mol_b, core_smarts):
-    """no atom mapping errors with this one, but the core size is smaller"""
-    query = Chem.MolFromSmarts(core_smarts)
-    return np.array([_get_unique_match(mol_a, query), _get_unique_match(mol_b, query)]).T
-
-
-# 3.1. Define a custom maximum common substructure search method
-
-
-def _identify_hub(transformation_sizes):
-    return int(np.argmin(transformation_sizes.sum(0)))
-
-
-def get_star_map(mols, threshold: float = 0.5):
-    mcs_s = compute_all_pairs_mcs(mols, threshold=threshold)
-    transformation_sizes = compute_transformation_size_matrix(mols, mcs_s)
-
-    hub_index = _identify_hub(transformation_sizes)
-    hub = mols[hub_index]
-
-    # for each "spoke" in the star map, construct serializable transformation "hub -> spoke"
-    others = list(mols)
-    others.pop(hub_index)
-
-    return hub, others
