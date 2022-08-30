7053768c3088a08d5a8f90f2d6a0019c3892bc93
Josh Fass <jfass@relaytx.com>
2022-08-30 14:28:08 -0400

    Add failing test

--- tests/test_atom_mapping.py
+++ tests/test_atom_mapping.py
@@ -1,3 +1,5 @@
+from rdkit import Chem
+
 from timemachine.fe.atom_mapping import get_core_by_mcs, mcs_map
 from timemachine.testsystems.relative import hif2a_ligand_pair
 
@@ -13,3 +15,20 @@ def test_get_core_by_mcs():
     query = mcs_map(mol_a, mol_b).queryMol
     core = get_core_by_mcs(mol_a, mol_b, query)
     assert core.shape[1] == 2
+
+
+def test_ring_size_change():
+    """On a pair of molecules containing a 10-atom+ common substructure & a ring size change,
+    assert that the MCS is not trivial"""
+
+    smi_a = "C(C1CCCC1)C1=CC2=C(CCC2)C=C1"
+    smi_b = "C(C1CCC1)C1=CC2=C(CCC2)C=C1"
+
+    mol_a = Chem.AddHs(Chem.MolFromSmiles(smi_a))
+    mol_b = Chem.AddHs(Chem.MolFromSmiles(smi_b))
+
+    mol_a.Compute2DCoords()
+    mol_b.Compute2DCoords()
+
+    mcs_result = mcs_map(mol_a, mol_b)
+    assert mcs_result.numAtoms > 2
