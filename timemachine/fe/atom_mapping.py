097a30f912f5e03df3064f2b143bf547ee0915d5
Josh Fass <jfass@relaytx.com>
2022-08-30 14:28:55 -0400

    Remove unused argument

--- timemachine/fe/atom_mapping.py
+++ timemachine/fe/atom_mapping.py
@@ -51,7 +51,7 @@ def mcs_map(a, b, threshold: float = 2.0, timeout: int = 5, smarts: Optional[str
     return rdFMCS.FindMCS([a, b], params)
 
 
-def mcs_map_graph_only_complete_rings(a, b, timeout: int = 3600, smarts: Optional[str] = None):
+def mcs_map_graph_only_complete_rings(a, b, timeout: int = 3600):
     """Find the MCS map of going from A to B, disregarding conformer information. This also ensures
     that core-core bonds are not broken."""
     return rdFMCS.FindMCS(
