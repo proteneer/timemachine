81a8ebff566d72cbdeca9ac1a59f119a868cd729
Josh Fass <jfass@relaytx.com>
2022-08-31 16:37:17 -0400

    Expand test
    Addressing https://github.com/proteneer/timemachine/pull/812#discussion_r959986908

--- tests/test_atom_mapping.py
+++ tests/test_atom_mapping.py
@@ -1,6 +1,7 @@
+import pytest
 from rdkit import Chem
 
-from timemachine.fe.atom_mapping import get_core_by_mcs, mcs
+from timemachine.fe.atom_mapping import AtomMappingError, get_core_by_mcs, mcs
 from timemachine.testsystems.relative import hif2a_ligand_pair
 
 
@@ -19,7 +20,7 @@ def test_get_core_by_mcs():
 
 def test_ring_size_change():
     """On a pair of molecules containing a 10-atom+ common substructure & a ring size change,
-    assert that the MCS is not trivial"""
+    assert that the MCS is not trivial, and that mcs(a, b) didn't modify a or b in-place"""
 
     smi_a = "C(C1CCCC1)C1=CC2=C(CCC2)C=C1"
     smi_b = "C(C1CCC1)C1=CC2=C(CCC2)C=C1"
@@ -27,8 +28,31 @@ def test_ring_size_change():
     mol_a = Chem.AddHs(Chem.MolFromSmiles(smi_a))
     mol_b = Chem.AddHs(Chem.MolFromSmiles(smi_b))
 
+    # to later check that mols weren't modified in place...
+    hash_a = hash(Chem.MolToSmiles(mol_a))
+    hash_b = hash(Chem.MolToSmiles(mol_b))
+
+    # test default settings, which assume approx. aligned conformers
     mol_a.Compute2DCoords()
     mol_b.Compute2DCoords()
 
     mcs_result = mcs(mol_a, mol_b)
     assert mcs_result.numBonds > 1
+
+    # expect failure if retry=False
+    with pytest.raises(AtomMappingError) as e:
+        _ = mcs(mol_a, mol_b, retry=False)
+    assert "MCS" in str(e)
+
+    # also test graph-only setting
+    mcs_result = mcs(mol_a, mol_b, conformer_aware=False)
+    assert mcs_result.numBonds > 1
+
+    # again expect failure if retry=False
+    with pytest.raises(AtomMappingError) as e:
+        _ = mcs(mol_a, mol_b, conformer_aware=False, retry=False)
+    assert "MCS" in str(e)
+
+    # assert that the previous 4 calls to mcs didn't modify the molecules in-place
+    assert hash(Chem.MolToSmiles(mol_a)) == hash_a
+    assert hash(Chem.MolToSmiles(mol_b)) == hash_b
