30e42fa1ea70ae75a9506120bdc19a1bb2bc5f6b
Forrest York <forrest.york@gmail.com>
2022-09-06 10:16:42 -0600

    PR Feedback: Simplify round_up_even

--- timemachine/cpp/src/gpu_utils.cu
+++ timemachine/cpp/src/gpu_utils.cu
@@ -1,12 +1,6 @@
 #include "gpu_utils.cuh"
 
-int round_up_even(int count) {
-    if (count % 2 == 1) {
-        return count += 1;
-    } else {
-        return count;
-    }
-}
+int round_up_even(int count) { return count + (count % 2); }
 
 curandStatus_t templateCurandNormal(curandGenerator_t generator, float *outputPtr, size_t n, float mean, float stddev) {
     return curandGenerateNormal(generator, outputPtr, n, mean, stddev);
