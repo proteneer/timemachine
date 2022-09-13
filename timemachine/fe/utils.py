63e29eeb7ec3921e956042434096b05c749b82c0
Forrest York <forrest.york@gmail.com>
2022-09-13 11:24:40 -0600

    Uses correct conversion from kcal/mol -> kj/mol

--- timemachine/fe/utils.py
+++ timemachine/fe/utils.py
@@ -5,6 +5,8 @@ from rdkit import Chem
 from rdkit.Chem import AllChem, Draw
 from rdkit.Chem.Draw import rdMolDraw2D
 
+from timemachine import constants
+
 
 def to_md_units(q):
     return q.value_in_unit_system(simtk.unit.md_unit_system)
@@ -47,7 +49,7 @@ def convert_uIC50_to_kJ_per_mole(amount_in_uM):
     """
     TODO: more sig figs
     """
-    return 0.593 * np.log(amount_in_uM * 1e-6) * 4.18
+    return 0.593 * np.log(amount_in_uM * 1e-6) * constants.KCAL_TO_KJ
 
 
 def convert_uM_to_kJ_per_mole(amount_in_uM):
@@ -65,7 +67,7 @@ def convert_uM_to_kJ_per_mole(amount_in_uM):
         Binding potency in kJ/mol.
 
     """
-    return 0.593 * np.log(amount_in_uM * 1e-6) * 4.18
+    return 0.593 * np.log(amount_in_uM * 1e-6) * constants.KCAL_TO_KJ
 
 
 # TODO: add a module for atom-mapping, with RDKit MCS based and other approaches
