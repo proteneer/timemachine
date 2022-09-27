56d5159f01c9b589fd458e9c4d22096a7276b689
Matt Wittmann <mwittmann@relaytx.com>
2022-09-27 09:23:44 -0700

    Strengthen test

--- tests/test_single_topology_v3.py
+++ tests/test_single_topology_v3.py
@@ -455,6 +455,9 @@ def test_interpolate_harmonic_force_constant(src_k, dst_k, k_min, lambda_interva
 
     lambdas = np.arange(0.01, 1.0, 0.01)
 
+    # all interpolated values less than k_min
+    np.testing.assert_array_less(1.0, f(src_k, dst_k, lambdas) / k_min + 1e-9)
+
     def assert_nondecreasing(f):
         y = f(lambdas)
         np.testing.assert_array_less(y[:-1] / y[1:], 1.0 + 1e-9)
