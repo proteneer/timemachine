45b377e0437a50beedd6def6745ba2abc779c9b5
Forrest York <badisa@users.noreply.github.com>
2022-09-28 10:57:11 -0600

    Update timemachine/cpp/src/constants.hpp
    Co-authored-by: Matt Wittmann <mwittmann@relaytx.com>

--- timemachine/cpp/src/constants.hpp
+++ timemachine/cpp/src/constants.hpp
@@ -1,3 +1,3 @@
 
-static const double BOLTZ = 0.008314462618;  // kJ/mol per kelvin, molar gas constant / 1000
+static const double BOLTZ = 0.008314462618;  // molar gas constant in kJ/mol per kelvin
 static const double AVOGADRO = 6.0221367e23; // mol^-1
