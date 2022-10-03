2fe1af5934060476e48540cc45f0714f13e81b2c
Josh Fass <jfass@relaytx.com>
2022-10-03 11:23:22 -0400

    Remove simtk.unit in constants.py

--- timemachine/constants.py
+++ timemachine/constants.py
@@ -1,5 +1,3 @@
-from simtk import unit
-
 BOLTZMANN = 1.380658e-23  # J/kelvin
 AVOGADRO = 6.0221367e23  # mol^-1
 RGAS = BOLTZMANN * AVOGADRO  # J/mol per kelvin
@@ -7,10 +5,7 @@ BOLTZ = RGAS / 1000  # kJ/mol per kelvin
 ONE_4PI_EPS0 = 138.935456
 VIBRATIONAL_CONSTANT = 1302.79  # http://openmopac.net/manual/Hessian_Matrix.html
 
-ENERGY_UNIT = unit.kilojoule_per_mole
-DISTANCE_UNIT = unit.nanometer
 
-kB = unit.BOLTZMANN_CONSTANT_kB * unit.AVOGADRO_CONSTANT_NA
 DEFAULT_FF = "smirnoff_2_0_0_ccc.py"
 DEFAULT_TEMP = 300  # kelvin
 DEFAULT_KT = BOLTZ * DEFAULT_TEMP  # kJ/mol
