proteneer/timemachine

commit 95dc14a32cb7c07b2a02d3fbd72811f0263c130d

Add optional 4-d decoupling args to reference potentials

timemachine/potentials/jax_utils.py
@@ -78,7 +78,12 @@ def delta_r(ri, rj, box=None):
     return diff
 
 
-def distance_on_pairs(ri, rj, box=None, offsets=None):
+def distance_on_pairs(
+    ri,
+    rj,
+    box=None,
+    w_offsets=None,  # per-pair 4-d offset
+):
     """O(n) where n = len(ri) = len(rj)
 
     Notes
@@ -88,8 +93,8 @@ def distance_on_pairs(ri, rj, box=None, offsets=None):
     assert len(ri) == len(rj)
 
     diff = delta_r(ri, rj, box)
-    if offsets is not None:
-        diff = jnp.concatenate([diff, jnp.array(offsets).reshape(-1, 1)], axis=1)
+    if w_offsets is not None:
+        diff = jnp.concatenate([diff, jnp.array(w_offsets).reshape(-1, 1)], axis=1)
 
     dij = jnp.linalg.norm(diff, axis=-1)
 
@@ -140,16 +145,44 @@ def get_interacting_pair_indices_batch(confs, boxes, pairs, cutoff=1.2):
     return batch_pairs
 
 
-def pairwise_distances(x, box):
-    # nonbonded distances require the periodic box
-    assert x.shape[1] == 3 or x.shape[1] == 4  # 3d or 4d
-    ri = jnp.expand_dims(x, 0)
-    rj = jnp.expand_dims(x, 1)
-    d2ij = jnp.sum(jnp.power(delta_r(ri, rj, box), 2), axis=-1)
-    N = d2ij.shape[0]
-    d2ij = jnp.where(jnp.eye(N), 0, d2ij)
-    dij = jnp.where(jnp.eye(N), 0, jnp.sqrt(d2ij))
-    return dij
+def pairwise_distances(x, box, w=None):
+    """
+    Compute the (N, N) periodic distance matrix given an (N, D) array of
+    coordinates.
+
+    Optionally accepts an (N, 1) array of coordinates in the "lifting"
+    (typically 4th) dimension; if passed, computes distances assuming periodic
+    boundaries for the primary dimensions and aperiodicty in the lifting
+    dimension.
+
+    Parameters
+    ----------
+    x : ndarray (N, D)
+        input coordinates
+
+    box : ndarray (D, D)
+        dimensions of periodic box
+
+    w : ndarray (N, 1), optional
+        coordinates in aperiodic lifting dimension
+    """
+    n, d = x.shape
+    assert box.shape == (d, d)
+    if w is not None:
+        assert w.shape[0] == n
+
+    d_ijk = delta_r(x[:, None], x[None, :], box)  # (x_i, x_j, dimension)
+    d2_ij = jnp.sum(d_ijk ** 2, axis=2)
+
+    if w is not None:
+        dw_ij = w[:, None] - w[None, :]
+        d2_ij += dw_ij ** 2
+
+    # prevent nans in gradient
+    d2_ij = d2_ij.at[jnp.diag_indices_from(d2_ij)].set(0.0)
+
+    d_ij = jnp.sqrt(d2_ij)
+    return d_ij
 
 
 def distance_from_one_to_others(x_i, x_others, box=None, cutoff=jnp.inf):

tests/nonbonded/test_nonbonded_interaction_group.py
@@ -91,14 +91,11 @@ def test_nonbonded_interaction_group_correctness(
     host_idxs = np.setdiff1d(np.arange(num_atoms), ligand_idxs)
 
     def ref_ixngroups(conf, params, box, lamb):
-
         # compute 4d coordinates
-        w = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
-        conf_4d = jax_utils.augment_dim(conf, w)
-        box_4d = (1000 * jax.numpy.eye(4)).at[:3, :3].set(box)
+        w_coords = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
 
         vdW, electrostatics = nonbonded.nonbonded_interaction_groups(
-            conf_4d, params, box_4d, ligand_idxs, host_idxs, beta, cutoff
+            conf, params, box, ligand_idxs, host_idxs, beta, cutoff, w_coords
         )
         return jax.numpy.sum(vdW + electrostatics)
 
@@ -155,14 +152,11 @@ def test_nonbonded_interaction_group_interpolated_correctness(
 
     @nonbonded.interpolated
     def ref_ixngroups(conf, params, box, lamb):
-
         # compute 4d coordinates
-        w = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
-        conf_4d = jax_utils.augment_dim(conf, w)
-        box_4d = (1000 * jax.numpy.eye(4)).at[:3, :3].set(box)
+        w_coords = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
 
         vdW, electrostatics = nonbonded.nonbonded_interaction_groups(
-            conf_4d, params, box_4d, ligand_idxs, host_idxs, beta, cutoff
+            conf, params, box, ligand_idxs, host_idxs, beta, cutoff, w_coords
         )
         return jax.numpy.sum(vdW + electrostatics)
 

tests/nonbonded/test_nonbonded_pair_list.py
@@ -35,13 +35,12 @@ def test_nonbonded_pair_list_invalid_pair_idxs():
 def make_ref_potential(pair_idxs, scales, lambda_plane_idxs, lambda_offset_idxs, beta, cutoff):
     @functools.wraps(nonbonded.nonbonded_on_specific_pairs)
     def wrapped(conf, params, box, lamb):
-
         # compute 4d coordinates
-        w = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
-        conf_4d = jax_utils.augment_dim(conf, w)
-        box_4d = (1000 * jax.numpy.eye(4)).at[:3, :3].set(box)
+        w_coords = jax_utils.compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
 
-        vdW, electrostatics = nonbonded.nonbonded_on_specific_pairs(conf_4d, params, box_4d, pair_idxs, beta, cutoff)
+        vdW, electrostatics = nonbonded.nonbonded_on_specific_pairs(
+            conf, params, box, pair_idxs, beta, cutoff, w_coords
+        )
         return jax.numpy.sum(scales[:, 1] * vdW + scales[:, 0] * electrostatics)
 
     return wrapped

tests/test_jax_nonbonded.py
@@ -22,7 +22,7 @@ from timemachine.fe.reweighting import one_sided_exp
 from timemachine.ff.handlers import openmm_deserializer
 from timemachine.md import builders
 from timemachine.potentials.jax_utils import (
-    convert_to_4d,
+    compute_lifting_parameter,
     get_all_pairs_indices,
     pairs_from_interaction_groups,
     pairwise_distances,
@@ -248,26 +248,13 @@ def _nonbonded_clone(
     """
 
     N = conf.shape[0]
-
-    if conf.shape[-1] == 3:
-        conf = convert_to_4d(conf, lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
-
-    # make 4th dimension of box large enough so its roughly aperiodic
-    if box is not None:
-        if box.shape[-1] == 3:
-            box_4d = jnp.eye(4) * 1000
-            box_4d = box_4d.at[:3, :3].set(box)
-        else:
-            box_4d = box
-    else:
-        box_4d = None
-    box = box_4d
+    w_coords = compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
 
     # TODO: len(pairs) == n_interactions -- may want to break this
     #   up into more manageable blocks if n_interactions is large
     pairs = get_all_pairs_indices(N)
 
-    lj, coulomb = nonbonded_on_specific_pairs(conf, params, box, pairs, beta, cutoff)
+    lj, coulomb = nonbonded_on_specific_pairs(conf, params, box, pairs, beta, cutoff, w_coords)
 
     # keep only eps > 0
     inds_i, inds_j = pairs.T

timemachine/potentials/nonbonded.py
@@ -10,7 +10,6 @@ from typing_extensions import TypeAlias
 from timemachine.potentials import jax_utils
 from timemachine.potentials.jax_utils import (
     compute_lifting_parameter,
-    convert_to_4d,
     delta_r,
     distance_on_pairs,
     pairs_from_interaction_groups,
@@ -199,20 +198,7 @@ def nonbonded(
     if lambda_offset_idxs is None:
         lambda_offset_idxs = np.zeros(N)
 
-    if conf.shape[-1] == 3:
-        conf = convert_to_4d(conf, lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
-
-    # make 4th dimension of box large enough so its roughly aperiodic
-    if box is not None:
-        if box.shape[-1] == 3:
-            box_4d = jnp.eye(4) * 1000
-            box_4d = box_4d.at[:3, :3].set(box)
-        else:
-            box_4d = box
-    else:
-        box_4d = None
-
-    box = box_4d
+    w_coords = compute_lifting_parameter(lamb, lambda_plane_idxs, lambda_offset_idxs, cutoff)
 
     charges = params[:, 0]
     sig = params[:, 1]
@@ -226,7 +212,7 @@ def nonbonded(
     eps_j = jnp.expand_dims(eps, 1)
     eps_ij = combining_rule_epsilon(eps_i, eps_j)
 
-    dij = pairwise_distances(conf, box)
+    dij = pairwise_distances(conf, box, w_coords)
 
     keep_mask = jnp.ones((N, N)) - jnp.eye(N)
     keep_mask = jnp.where(eps_ij != 0, keep_mask, 0)
@@ -270,7 +256,14 @@ def nonbonded(
 
 
 def nonbonded_on_specific_pairs(
-    conf, params, box, pairs, beta: float, cutoff: Optional[float] = None, rescale_mask=None
+    conf,
+    params,
+    box,
+    pairs,
+    beta: float,
+    cutoff: Optional[float] = None,
+    w_coords: Optional[Array] = None,  # per-particle 4-d coordinate
+    rescale_mask=None,
 ):
     """See `nonbonded` docstring for more details
 
@@ -287,7 +280,8 @@ def nonbonded_on_specific_pairs(
     inds_l, inds_r = pairs.T
 
     # distances and cutoff
-    dij = distance_on_pairs(conf[inds_l], conf[inds_r], box)
+    w_offsets = w_coords[pairs[:, 0]] - w_coords[pairs[:, 1]] if w_coords is not None else None
+    dij = distance_on_pairs(conf[inds_l], conf[inds_r], box, w_offsets)
     if cutoff is None:
         cutoff = np.inf
     keep_mask = dij <= cutoff
@@ -378,7 +372,16 @@ def validate_interaction_group_idxs(n_atoms, a_idxs, b_idxs):
     assert len(b_idxs) == len(B)
 
 
-def nonbonded_interaction_groups(conf, params, box, a_idxs, b_idxs, beta: float, cutoff: Optional[float] = None):
+def nonbonded_interaction_groups(
+    conf,
+    params,
+    box,
+    a_idxs,
+    b_idxs,
+    beta: float,
+    cutoff: Optional[float] = None,
+    w_coords: Optional[Array] = None,  # per-particle 4-d coordinate
+):
     """Nonbonded interactions between all pairs of atoms $(i, j)$
     where $i$ is in the first set and $j$ in the second.
 
@@ -386,7 +389,7 @@ def nonbonded_interaction_groups(conf, params, box, a_idxs, b_idxs, beta: float,
     """
     validate_interaction_group_idxs(len(conf), a_idxs, b_idxs)
     pairs = pairs_from_interaction_groups(a_idxs, b_idxs)
-    vdW, electrostatics = nonbonded_on_specific_pairs(conf, params, box, pairs, beta, cutoff)
+    vdW, electrostatics = nonbonded_on_specific_pairs(conf, params, box, pairs, beta, cutoff, w_coords)
     return vdW, electrostatics
 
 

commit c2fbff0c536022e1d041a2534e5ec4cdb214b199

Handle `box=None` in `pairwise_distances`, add tests (#865)
* Add tests

* Handle box=None in pairwise_distances
  Fixes regression introduced in 95dc14a32cb7c07b2a02d3fbd72811f0263c130d

* Refines assertion on shape of w

setup.py
@@ -100,7 +100,12 @@ setup(
             "mypy==0.942",
             "pre-commit==2.17.0",
         ],
-        "test": ["pytest", "pytest-cov", "hilbertcurve==1.0.5", "hypothesis==6.54.6"],
+        "test": [
+            "pytest",
+            "pytest-cov",
+            "hilbertcurve==1.0.5",
+            "hypothesis[numpy]==6.54.6",
+        ],
     },
     package_data={
         "timemachine": [

timemachine/potentials/jax_utils.py
@@ -127,10 +127,10 @@ def get_interacting_pair_indices_batch(confs, boxes, pairs, cutoff=1.2):
     return batch_pairs
 
 
-def pairwise_distances(x, box, w=None):
+def pairwise_distances(x, box=None, w=None):
     """
-    Compute the (N, N) periodic distance matrix given an (N, D) array of
-    coordinates.
+    Compute the (N, N) distance matrix given an (N, D) array of coordinates. If
+    `box` is passed, computes nearest distances assuming periodic boundaries.
 
     Optionally accepts an (N, 1) array of coordinates in the "lifting"
     (typically 4th) dimension; if passed, computes distances assuming periodic
@@ -142,21 +142,21 @@ def pairwise_distances(x, box, w=None):
     x : ndarray (N, D)
         input coordinates
 
-    box : ndarray (D, D)
+    box : ndarray (D, D), optional
         dimensions of periodic box
 
     w : ndarray (N, 1), optional
         coordinates in aperiodic lifting dimension
     """
     n, d = x.shape
-    assert box.shape == (d, d)
-    if w is not None:
-        assert w.shape[0] == n
+    if box is not None:
+        assert box.shape == (d, d)
 
     d_ijk = delta_r(x[:, None], x[None, :], box)  # (x_i, x_j, dimension)
     d2_ij = jnp.sum(d_ijk ** 2, axis=2)
 
     if w is not None:
+        assert w.shape == (n,)
         dw_ij = w[:, None] - w[None, :]
         d2_ij += dw_ij ** 2
 

tests/test_jax_utils.py
@@ -1,5 +1,10 @@
+from functools import partial
+
+import hypothesis.strategies as st
 import numpy as np
 import pytest
+from hypothesis import example, given, seed
+from hypothesis.extra.numpy import array_shapes, arrays
 from jax import jit
 from jax import numpy as jnp
 from jax import vmap
@@ -13,6 +18,7 @@ from timemachine.potentials.jax_utils import (
     get_all_pairs_indices,
     get_interacting_pair_indices_batch,
     pairs_from_interaction_groups,
+    pairwise_distances,
 )
 
 pytestmark = [pytest.mark.nogpu]
@@ -129,3 +135,65 @@ def test_batched_neighbor_inds():
 
     assert neighbor_distances.shape == (n_confs, n_neighbor_pairs)
     assert np.sum(neighbor_distances < cutoff) == np.sum(full_distances < cutoff)
+
+
+def test_pairwise_distances_assertions():
+    with pytest.raises(AssertionError):
+        pairwise_distances(np.empty((4, 3)), np.empty((1, 1)))  # inconsistent box shape
+    _ = pairwise_distances(np.empty((4, 3)), np.empty((3, 3)))  # ok
+
+    with pytest.raises(AssertionError):
+        pairwise_distances(np.empty((4, 3)), np.empty((3, 3)), np.empty((3,)))  # inconsistent w_coords shape
+    _ = pairwise_distances(np.empty((4, 3)), np.empty((3, 3)), np.empty((4,)))  # ok
+
+
+finite_floats = partial(st.floats, allow_nan=False, allow_infinity=False, allow_subnormal=False)
+coordinates = arrays(np.float64, array_shapes(min_dims=2, max_dims=2), elements=finite_floats(-1e6, 1e6))
+
+
+@given(coordinates)
+@seed(2022)
+def test_pairwise_distances(x):
+    n, _ = x.shape
+    dij = pairwise_distances(x)
+    assert dij.shape == (n, n)
+    assert (dij >= 0.0).all()
+
+
+@st.composite
+def coords_box_w_triples(draw):
+    x = draw(coordinates)
+    n, d = x.shape
+    box_diag = draw(arrays(np.float64, d, elements=finite_floats(1e-6, 1e6)))
+    w = draw(arrays(np.float64, n, elements=finite_floats(-1e6, 1e6)))
+    return x, box_diag, w
+
+
+@given(coords_box_w_triples())
+@example((np.array([[0, 0], [0.5, 0]]), np.array([[1, 1]]), None))
+@example((np.array([[0, 0], [1.5, 0]]), np.array([[1, 1]]), None))
+@seed(2022)
+def test_pairwise_distances_periodic(coords_box_w):
+    x, box_diag, _ = coords_box_w
+    n, _ = x.shape
+    dij = pairwise_distances(x, np.diagflat(box_diag))
+    assert dij.shape == (n, n)
+    assert (dij >= 0.0).all()
+    assert (dij <= box_diag.max()).all()
+
+
+@given(coords_box_w_triples())
+@example((np.array([[0, 0], [1.0, 0]]), np.array([[1, 1]]), np.array([0.0, 0.0])))
+@example((np.array([[0, 0], [1.0, 0]]), np.array([[1, 1]]), np.array([0.5, 0.5])))
+@seed(2022)
+def test_pairwise_distances_periodic_lifting(coords_box_w):
+    x, box_diag, w = coords_box_w
+    n, _ = x.shape
+    dij0 = pairwise_distances(x, np.diagflat(box_diag))
+    dij = pairwise_distances(x, np.diagflat(box_diag), w)
+    assert dij.shape == (n, n)
+    assert (dij >= dij0).all()
+
+    # guard assertion to prevent spurious failure due to roundoff error
+    if np.std(w) / (1.0 + np.linalg.norm(x)) > 1e-6:
+        assert dij.sum() > dij0.sum()