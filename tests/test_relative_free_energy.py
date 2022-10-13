1950a9238c545eb3d2d747e3be064877e490dd67
Forrest York <forrest.york@gmail.com>
2022-10-13 09:12:08 -0600

    PR Feedback: Verify number of frames

--- tests/test_relative_free_energy.py
+++ tests/test_relative_free_energy.py
@@ -168,9 +168,15 @@ def test_steps_per_frames():
     mol_a, mol_b, core = get_hif2a_ligand_pair_single_topology()
     forcefield = Forcefield.load_from_file(DEFAULT_FF)
     seed = 2022
-    res = run_vacuum(mol_a, mol_b, core, forcefield, None, 5, seed, n_eq_steps=10, steps_per_frame=2, n_windows=3)
+    frames = 5
+    res = run_vacuum(mol_a, mol_b, core, forcefield, None, frames, seed, n_eq_steps=10, steps_per_frame=2, n_windows=3)
+    assert res.frames[0].shape[0] == frames
 
-    test_res = run_vacuum(mol_a, mol_b, core, forcefield, None, 2, seed, n_eq_steps=10, steps_per_frame=5, n_windows=3)
+    frames = 2
+    test_res = run_vacuum(
+        mol_a, mol_b, core, forcefield, None, frames, seed, n_eq_steps=10, steps_per_frame=5, n_windows=3
+    )
+    assert test_res.frames[0].shape[0] == frames
 
     # The last frame from the trajectories should match as num_frames * steps_per_frame are equal
     for frame, test_frame in zip(res.frames, test_res.frames):
