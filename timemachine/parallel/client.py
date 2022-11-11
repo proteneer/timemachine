c198a32eeabd76c2c5f6066fd0c713d22298c028
Matt Wittmann <mwittmann@relaytx.com>
2022-11-11 14:54:16 -0800

    Revert "Expose underlying future methods in wrapper"
    This reverts commit e4b0173da8bd158df4017356d1e43bae36fd6fdb.

--- timemachine/parallel/client.py
+++ timemachine/parallel/client.py
@@ -94,6 +94,12 @@ class WrappedFuture:
         self._future = future
         self._id = job_id
 
+    def result(self):
+        return self._future.result()
+
+    def done(self):
+        return self._future.done()
+
     @property
     def id(self):
         return self._id
@@ -101,9 +107,6 @@ class WrappedFuture:
     @property
     def name(self):
         return str(self._id)
-
-    def __getattr__(self, attr):
-        return getattr(self._future, attr)
 
 
 class SerialClient(AbstractClient):
