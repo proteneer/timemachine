89fb1d8e41e5b6c9df4fb521da1199dce1d023e9
Forrest York <forrest.york@gmail.com>
2022-11-19 10:43:29 -0700

    Removes k_coords_to_kv
    * Prefer k_coords_to_kv_gather as the more general case

--- timemachine/cpp/src/kernels/k_nonbonded.cu
+++ timemachine/cpp/src/kernels/k_nonbonded.cu
@@ -1,45 +1,5 @@
 #include "k_nonbonded.cuh"
 
-void __global__ k_coords_to_kv(
-    const int N,
-    const double *__restrict__ coords,
-    const double *__restrict__ box,
-    const unsigned int *__restrict__ bin_to_idx,
-    unsigned int *__restrict__ keys,
-    unsigned int *__restrict__ vals) {
-
-    const int atom_idx = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (atom_idx >= N) {
-        return;
-    }
-
-    // these coords have to be centered
-    double bx = box[0 * 3 + 0];
-    double by = box[1 * 3 + 1];
-    double bz = box[2 * 3 + 2];
-
-    double binWidth = max(max(bx, by), bz) / (HILBERT_GRID_DIM - 1.0);
-
-    double x = coords[atom_idx * 3 + 0];
-    double y = coords[atom_idx * 3 + 1];
-    double z = coords[atom_idx * 3 + 2];
-
-    x -= bx * floor(x / bx);
-    y -= by * floor(y / by);
-    z -= bz * floor(z / bz);
-
-    unsigned int bin_x = x / binWidth;
-    unsigned int bin_y = y / binWidth;
-    unsigned int bin_z = z / binWidth;
-
-    keys[atom_idx] = bin_to_idx[bin_x * HILBERT_GRID_DIM * HILBERT_GRID_DIM + bin_y * HILBERT_GRID_DIM + bin_z];
-    // uncomment below if you want to preserve the atom ordering
-    // keys[atom_idx] = atom_idx;
-    vals[atom_idx] = atom_idx;
-}
-
-// TODO: DRY with k_coords_to_kv
 void __global__ k_coords_to_kv_gather(
     const int N,
     const unsigned int *__restrict__ atom_idxs,
