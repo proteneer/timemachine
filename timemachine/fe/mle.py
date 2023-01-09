94ca4cc874674e65f447f026705ca6b313938e3a
Forrest York <forrest.york@gmail.com>
2023-01-09 14:17:05 -0700

    Adds MLE
    Co-authored-by: Josh Fass <jfass@relaytx.com>

---
+++ timemachine/fe/mle.py
@@ -0,0 +1,106 @@
+import networkx as nx
+import numpy as np
+from jax import jit
+from jax import numpy as jnp
+from jax import value_and_grad
+from jax.scipy.stats import norm
+from scipy.optimize import minimize
+
+
+def make_stddevs_finite(stddevs, min_stddev=1e-3):
+    """Ignore claims that stddev < min_stddev"""
+    return jnp.maximum(stddevs, min_stddev)
+
+
+def gaussian_log_likelihood(
+    node_vals,  # [K] float array
+    edge_idxs,  # [E, 2] int array, taking values in range(K)
+    edge_diffs,  # [E] float array
+    edge_stddevs,  # [E] float array
+):
+    r"""how likely are observed edge_diffs given underlying node_vals?
+
+    likelihood(edge_diffs) = \prod_{i=0}^E
+        Gaussian(
+            edge_diffs[i];
+            mean=node_vals[edge_idxs[i][1]] - node_vals[edge_idxs[i][0]],
+            stddev=edge_stddevs[i],
+        )
+
+    References
+    ----------
+    [Xu, 2019] Optimal measurement network of pairwise differences
+        https://doi.org/10.1021/acs.jcim.9b00528
+        https://github.com/forcefield/DiffNet
+    """
+    src_idxs, dst_idxs = edge_idxs.T
+    implied_diffs = node_vals[dst_idxs] - node_vals[src_idxs]
+
+    # Note: can swap in other likelihood functions here besides norm.logpdf
+    sanitized_edge_stddevs = make_stddevs_finite(edge_stddevs)
+    edge_lls = norm.logpdf(x=edge_diffs, loc=implied_diffs, scale=sanitized_edge_stddevs)
+
+    return jnp.sum(edge_lls)
+
+
+def _assert_edges_complete(edge_idxs):
+    K = np.max(edge_idxs) + 1
+    assert set(range(K)) == set(edge_idxs.flatten())
+
+
+def _assert_edges_connected(edge_idxs):
+    g = nx.Graph(list(map(tuple, edge_idxs)))
+    assert nx.number_connected_components(g) == 1
+
+
+def _assert_edges_valid(edge_idxs):
+    # some assertions for validity of a graph
+    _assert_edges_complete(edge_idxs)
+    _assert_edges_connected(edge_idxs)
+
+
+def wrap_for_scipy_optimize(f):
+    # utility for interfacing with scipy.optimize L-BFGS-B
+    vg = jit(value_and_grad(f))
+
+    def wrapped(x):
+        v, g = vg(x)
+        return float(v), np.array(g, dtype=np.float64)
+
+    return wrapped
+
+
+def infer_node_vals(edge_idxs, edge_diffs, edge_stddevs):
+    """
+    Given pairwise comparisons involving K states,
+    return a length-K vector of underlying absolute values
+    by finding node_vals that maximizing likelihood of edge_diffs.
+    The result is determined up to an arbitrary additive constant --
+    by convention this function sets result[0] = 0.0.
+
+    Parameters
+    ----------
+    edge_idxs: [E, 2] array, where the values are in range(K).
+    edge_diffs: [E] array, relative values
+    edge_stddevs: [E] array, positive
+
+    Returns
+    -------
+        [K] array, inferred absolute values (up to arbitrary constant)
+    """
+
+    # check shapes
+    assert len(edge_diffs) == len(edge_idxs), f"{len(edge_diffs)} != {len(edge_idxs)}"
+    _assert_edges_valid(edge_idxs)
+
+    # maximize likelihood of observed edge diffs
+    @wrap_for_scipy_optimize
+    def loss(x):
+        return -gaussian_log_likelihood(x, edge_idxs, edge_diffs, edge_stddevs)
+
+    K = np.max(edge_idxs) + 1
+    x0 = np.zeros(K)  # maybe initialize smarter, e.g. using random spanning tree?
+    result = minimize(loss, x0, jac=True, tol=0).x
+
+    # arbitrary additive constant
+    return result - result[0]
