88b635ad0b5b9312ed76d8792a75d2676d45eea6
Matt Wittmann <mwittmann@relaytx.com>
2023-02-08 10:31:17 -0800

    Fix output formatting

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -54,7 +54,7 @@ def run_example(
     if env is not None:
         subprocess_env.update(env)
     subprocess_args = [sys.executable, str(example_path), *cli_args]
-    print("Running with args:", "".join(subprocess_args))
+    print("Running with args:", " ".join(subprocess_args))
     proc = subprocess.run(
         subprocess_args,
         env=subprocess_env,
