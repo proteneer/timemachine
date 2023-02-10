572e1cc9804f4f36e70cca299a1a69f34dbcb53c
Matt Wittmann <mwittmann@relaytx.com>
2023-02-10 09:19:42 -0800

    DRY tests

--- tests/test_stored_arrays.py
+++ tests/test_stored_arrays.py
@@ -79,12 +79,9 @@ def test_stored_arrays_doesnt_hold_references(ctor):
 @given(lists(chunks()))
 @seed(2023)
 def test_stored_arrays_eq(chunks):
-    a = StoredArrays()
-    b = StoredArrays()
-    for chunk in chunks:
-        a.extend(chunk)
-        b.extend(chunk)
-
+    a = stored_arrays_from_chunks(chunks)
+    b = stored_arrays_from_chunks(chunks)
+    assert a == a
     assert a is not b
     assert a == b
 
@@ -94,12 +91,8 @@ def test_stored_arrays_eq(chunks):
 @example([np.array([]), np.array([1])], [np.array([1])])
 def test_stored_arrays_neq(chunks1, chunks2):
     assume(not all(np.array_equal(a, b, equal_nan=True) for a, b in zip(chunks1, chunks2)))
-    a = StoredArrays()
-    for chunk in chunks1:
-        a.extend(chunk)
-    b = StoredArrays()
-    for chunk in chunks2:
-        b.extend(chunk)
+    a = stored_arrays_from_chunks(chunks1)
+    b = stored_arrays_from_chunks(chunks2)
     assert a != b
 
 
