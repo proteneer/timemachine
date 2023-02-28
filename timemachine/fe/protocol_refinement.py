b02d44850ed3c532e977396b6efc172ea4a6cd73
Matt Wittmann <mwittmann@relaytx.com>
2023-02-28 13:16:58 -0800

    Rename insert -> copy_and_insert

--- timemachine/fe/protocol_refinement.py
+++ timemachine/fe/protocol_refinement.py
@@ -37,12 +37,12 @@ def greedy_bisection_step(
     pairs_by_cost = [(cost, left_idx, pair) for left_idx, (pair, cost) in enumerate(zip(pairs, costs))]
     _, left_idx, (left, right) = max(pairs_by_cost)
     new_state = make_intermediate(left, right)
-    refined_protocol = insert(protocol, left_idx + 1, new_state)
+    refined_protocol = copy_and_insert(protocol, left_idx + 1, new_state)
 
     return refined_protocol, (costs, left_idx, new_state)
 
 
-def insert(xs: List[_T], idx: int, x: _T) -> List[_T]:
+def copy_and_insert(xs: List[_T], idx: int, x: _T) -> List[_T]:
     assert idx <= len(xs)
     xs_ = xs.copy()
     xs_.insert(idx, x)
