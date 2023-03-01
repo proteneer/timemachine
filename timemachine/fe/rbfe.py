6b5dded092b3be2462d7315ebc3ba52131097c15
Matt Wittmann <mwittmann@relaytx.com>
2023-03-01 15:14:31 -0800

    Refine assertion to what is actually required

--- timemachine/fe/rbfe.py
+++ timemachine/fe/rbfe.py
@@ -226,14 +226,13 @@ def setup_optimized_initial_state(
     seed: int,
 ) -> InitialState:
 
-    assert set(s.lamb for s in optimized_initial_states).issuperset(
-        [0.0, 1.0]
-    ), "end states must be included in optimized_initial_states"
-
     # Use pre-optimized initial state with the closest value of lambda as a starting point for optimization.
 
     # NOTE: The current approach for generating optimized conformations in `optimize_coordinates` creates a
     # discontinuity at lambda=0.5. Ensure that we pick a pre-optimized state on the same side of 0.5 as `lamb`:
+    assert any(s.lamb <= 0.5 for s in optimized_initial_states)
+    assert any(s.lamb > 0.5 for s in optimized_initial_states)
+
     states_subset = [s for s in optimized_initial_states if (s.lamb <= 0.5) == (lamb <= 0.5)]
     nearest_optimized = min(states_subset, key=lambda s: abs(lamb - s.lamb))
 
