407611eceac6566f1fd1c4c010c941aa5bb73496
Forrest York <forrest.york@gmail.com>
2023-04-07 06:37:19 -0600

    WIP

--- examples/absolute_hydration_energy.py
+++ examples/absolute_hydration_energy.py
@@ -1,5 +1,6 @@
 import argparse
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import numpy as np
 from rdkit import Chem
@@ -7,27 +8,46 @@ from rdkit import Chem
 from timemachine.constants import DEFAULT_FF
 from timemachine.fe import pdb_writer
 from timemachine.fe.absolute_hydration import run_solvent
-from timemachine.fe.free_energy import MDParams
+from timemachine.fe.free_energy import MDParams, image_frames
 from timemachine.fe.utils import get_mol_name, read_sdf
 from timemachine.ff import Forcefield
-from timemachine.parallel.client import CUDAPoolClient
+from timemachine.parallel.client import CUDAPoolClient, FileClient
 from timemachine.parallel.utils import get_gpu_count
 
 Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps)
 
 
-def write_trajectory_as_pdb(mol, all_frames, host_topology, prefix):
-    for window_idx, window_frames in enumerate(all_frames):
-        out_path = f"{prefix}_{window_idx}.pdb"
-        writer = pdb_writer.PDBWriter([host_topology, mol], out_path)
-        for frame in window_frames:
-            writer.write_frame(frame * 10)
-        writer.close()
+def write_ligand_trajectory_as_pdb(file_client, mol, res, prefix):
+    # initial states all have same ligand idxs/bonds, which is what is impotant here.
+    initial_state = res.final_result.initial_states[0]
+    for i, (frames, boxes) in enumerate(zip(res.frames, res.boxes)):
+        # Only writing out ligands, but image_frames also centers ligands
+        frames = image_frames(initial_state, frames, boxes)
+        out_path = f"{prefix}_{i}.pdb"
+        with NamedTemporaryFile(suffix=".pdb") as temp:
+            writer = pdb_writer.PDBWriter([mol], temp.name)
+            for frame in frames:
+                # Only write out ligand frames
+                writer.write_frame(frame[initial_state.ligand_idxs] * 10)
+            writer.close()
+            file_client.store_stream(out_path, open(temp.name, "rb"))
+
+
+def run_simulation(file_client, mol, ff, params, seed, windows):
+    solvent_res, solvent_top, solvent_host_config = run_solvent(mol, ff, None, seed, params, n_windows=windows)
+    dG = np.sum(solvent_res.final_result.dGs)
+    dG_err = np.linalg.norm(solvent_res.final_result.dG_errs)
+    mol.SetProp("AHFE dG (kJ/mol)", str(dG))
+    mol.SetProp("AHFE dG err (kJ/mol)", str(dG_err))
+    write_ligand_trajectory_as_pdb(file_client, mol, solvent_res, get_mol_name(mol) + "_ligand_traj")
+    return mol
 
 
 def read_from_args():
 
-    parser = argparse.ArgumentParser(description="Estimate Absolute hydration energy free energy")
+    parser = argparse.ArgumentParser(
+        description="Estimate Absolute hydration energy free energy with local MD options."
+    )
     parser.add_argument(
         "--n_frames",
         type=int,
@@ -38,13 +58,13 @@ def read_from_args():
         "--steps_per_frame",
         type=int,
         help="number of steps between frame collection",
-        default=1000,
+        default=600,
     )
     parser.add_argument(
         "--local_steps",
         type=int,
-        help="number of local steps collected in each frame between frame collection",
-        default=1000,
+        help="number of local steps collected in each frame between frame collection. Global steps is steps_per_frame - local_steps",
+        default=500,
     )
     parser.add_argument(
         "--k",
@@ -95,6 +115,7 @@ def read_from_args():
         radius=args.radius,
     )
     cli = CUDAPoolClient(get_gpu_count())
+    file_client = FileClient()
     futures = {}
     for mol in mols:
         mol_name = get_mol_name(mol)
@@ -103,16 +124,12 @@ def read_from_args():
         if mol_name in futures:
             print(f"Found duplicate name '{mol_name}', skipping...")
             continue
-        fut = cli.submit(run_solvent, mol, forcefield, None, args.seed, params, n_windows=args.n_windows)
+        fut = cli.submit(run_simulation, file_client, mol, forcefield, params, args.seed, args.n_windows)
         futures[mol_name] = (mol, fut)
 
     with Chem.SDWriter(args.output_path) as writer:
         for name, (mol, fut) in futures.items():
-            solvent_res, solvent_top, solvent_host_config = fut.result()
-            dG = np.sum(solvent_res.final_result.dGs)
-            dG_err = np.linalg.norm(solvent_res.final_result.dG_errs)
-            mol.SetProp("AHFE dG (kJ/mol)", str(dG))
-            mol.SetProp("AHFE dG err (kJ/mol)", str(dG_err))
+            mol = fut.result()
             writer.write(mol)
 
 
