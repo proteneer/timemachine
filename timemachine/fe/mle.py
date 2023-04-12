b2aefb57062763f26f8b4b7b3b54471cf4eec3f8
Matt Wittmann <mwittmann@relaytx.com>
2023-04-12 15:57:43 -0700

    Fix condition for edges missing values
    Incorrectly considered 0 as missing

--- timemachine/fe/mle.py
+++ timemachine/fe/mle.py
@@ -263,7 +263,9 @@ def infer_node_vals_and_errs_networkx(
         """Remove edges with missing edge_diff_prop or edge_stddev_prop; then, remove any isolated nodes"""
         sg = g.copy()
 
-        edges_missing_val = [e for e, v in sg.edges.items() if not v.get(edge_diff_prop) or not v.get(edge_stddev_prop)]
+        edges_missing_val = [
+            e for e, v in sg.edges.items() if v.get(edge_diff_prop) is None or v.get(edge_stddev_prop) is None
+        ]
         sg.remove_edges_from(edges_missing_val)
 
         isolated_nodes = list(nx.isolates(sg))
