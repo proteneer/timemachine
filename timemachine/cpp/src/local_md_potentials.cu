57f21ba6cf5a201ba2f7a99f3580474e81a82570
Forrest York <forrest.york@gmail.com>
2023-04-17 14:26:23 -0600

    PR Feedback: Fixes typo in comment

--- timemachine/cpp/src/local_md_potentials.cu
+++ timemachine/cpp/src/local_md_potentials.cu
@@ -12,7 +12,7 @@
 
 namespace timemachine {
 
-// Struct to as a CUB <= operation
+// Struct to as a CUB < operation
 struct LessThan {
     int compare;
     CUB_RUNTIME_FUNCTION __device__ __forceinline__ explicit LessThan(int compare) : compare(compare) {}
