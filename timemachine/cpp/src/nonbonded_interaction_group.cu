100d41f6773e15ed44ce4ceca4ab81edac0e56d4
Joseph Kaus <jkaus@relaytx.com>
2023-05-01 10:49:55 -0400

    Make naming consistent

--- timemachine/cpp/src/nonbonded_interaction_group.cu
+++ timemachine/cpp/src/nonbonded_interaction_group.cu
@@ -26,7 +26,7 @@ NonbondedInteractionGroup<RealType>::NonbondedInteractionGroup(
     const std::vector<int> &col_atom_idxs,
     const double beta,
     const double cutoff,
-    const std::optional<std::set<int>> &col_atom_idxs,
+    const std::optional<std::set<int>> &group_2_atom_idxs,
     const bool disable_hilbert_sort,
     const double nblist_padding)
     : N_(N), NR_(row_atom_idxs.size()), NC_(col_atom_idxs.size()),
@@ -50,24 +50,24 @@ NonbondedInteractionGroup<RealType>::NonbondedInteractionGroup(
 
     this->validate_idxs(N_, row_atom_idxs, col_atom_idxs, false);
 
-    if (col_atom_idxs) {
-        std::vector<int> col_atom_idxs_h(col_atom_idxs->begin(), col_atom_idxs->end());
+    if (group_2_atom_idxs) {
+        std::vector<int> col_atom_idxs_h(group_2_atom_idxs->begin(), group_2_atom_idxs->end());
         if (col_atom_idxs_h.size() == static_cast<long unsigned int>(N)) {
-            throw std::runtime_error("must be less then N(" + std::to_string(N) + ") col indices");
+            throw std::runtime_error("must be less then N(" + std::to_string(N) + ") group 2 indices");
         }
         verify_atom_idxs(N_, col_atom_idxs_h);
 
         // row and col idxs must be disjoint
-        std::set<int> unique_row_idxs(row_atom_idxs.begin(), row_atom_idxs.end());
+        std::set<int> unique_row_idxs(group_1_atom_idxs.begin(), group_1_atom_idxs.end());
         for (int col_atom_idx : col_atom_idxs_h) {
             if (unique_row_idxs.find(col_atom_idx) != unique_row_idxs.end()) {
-                throw std::runtime_error("row and col indices must be disjoint");
+                throw std::runtime_error("group 1 and group 2 indices must be disjoint");
             }
         }
     }
 
     cudaSafeMalloc(&d_col_atom_idxs_, N_ * sizeof(*d_col_atom_idxs_));
-    cudaSafeMalloc(&d_row_atom_idxs_, N_ * sizeof(*d_row_atom_idxs_));
+    cudaSafeMalloc(&d_group_1_atom_idxs_, N_ * sizeof(*d_row_atom_idxs_));
 
     cudaSafeMalloc(&d_perm_, N_ * sizeof(*d_perm_));
 
