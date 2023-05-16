db91fbf3cc51077f7f90382b4091344d32e72c68
Matt Wittmann <mwittmann@relaytx.com>
2023-05-16 07:57:12 -0700

    Modify heuristic for dummy group selection (#1019)
    * Modify dummy group selection heuristic to fix issue
    
    Replaces shortest_path <= 2 heuristic for assigning dummy atoms to the
    same group with one based on identifying connected components of the
    induced subgraph, making an arbitrary choice of anchor, and merging
    groups with the same anchor
    
    * Add assertion that hybrid has a single connected component

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,7 +1,8 @@
 import pytest
 from rdkit import Chem
 
-from timemachine.fe.dummy import identify_dummy_groups, identify_root_anchors
+from timemachine.fe.dummy import convert_bond_list_to_nx, generate_dummy_group_assignments
+from timemachine.fe.utils import get_romol_bonds
 
 # These tests check the various utilities used to turn off interactions
 # such that the end-states are separable. A useful glossary of terms is as follows.
@@ -20,77 +21,7 @@ from timemachine.fe.dummy import identify_dummy_groups, identify_root_anchors
 pytestmark = [pytest.mark.nogpu]
 
 
-def get_bond_idxs(mol):
-    # not necessarily canonicalized!
-    idxs = []
-    for bond in mol.GetBonds():
-        idxs.append((bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()))
-    return idxs
-
-
-def test_identify_root_anchors():
-    """
-    Test the identification of root anchors given a dummy atom.
-
-    For example, if D were the dummy atom below:
-
-            D---1---2
-           /
-          0
-
-    its root anchors are the set of atoms {0, 1}
-
-    """
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1CCC1N"))
-    core = [0, 1, 2, 3]
-    anchors = identify_root_anchors(mol, core, dummy_atom=4)
-    assert set(anchors) == set([3])
-
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1CC2NC2C1"))
-    core = [0, 1, 2, 4, 5]
-    anchors = identify_root_anchors(mol, core, dummy_atom=3)
-    assert set(anchors) == set([2, 4])
-
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1OCC11CCCCC1"))
-    core = [3, 4, 5, 6, 7, 8]
-    anchors = identify_root_anchors(mol, core, dummy_atom=0)
-    assert set(anchors) == set([3])
-    anchors = identify_root_anchors(mol, core, dummy_atom=1)
-    assert set(anchors) == set([3])
-    anchors = identify_root_anchors(mol, core, dummy_atom=2)
-    assert set(anchors) == set([3])
-
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1CC1.C1CCCCC1"))
-    core = [3, 4, 5, 6, 7, 8]
-    anchors = identify_root_anchors(mol, core, dummy_atom=0)
-    assert set(anchors) == set()
-    anchors = identify_root_anchors(mol, core, dummy_atom=1)
-    assert set(anchors) == set()
-    anchors = identify_root_anchors(mol, core, dummy_atom=2)
-    assert set(anchors) == set()
-
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1CC2NC2C1"))
-    core = [0, 1, 2, 5]
-    anchors = identify_root_anchors(mol, core, dummy_atom=3)
-    assert set(anchors) == set([2, 5])
-    anchors = identify_root_anchors(mol, core, dummy_atom=4)
-    assert set(anchors) == set([2, 5])
-
-    # cyclohexane with a nitrogen inside
-    mol = get_bond_idxs(Chem.MolFromSmiles("C1C2CC3CC1N23"))
-    core = [0, 1, 2, 3, 4, 5]
-    anchors = identify_root_anchors(mol, core, dummy_atom=6)
-    assert set(anchors) == set([1, 3, 5])
-
-
-def assert_set_equality(a_sets, b_sets):
-    # utility function to check that list of sets are equal
-    frozen_a = [frozenset(a) for a in a_sets]
-    frozen_b = [frozenset(b) for b in b_sets]
-    assert frozenset(frozen_a) == frozenset(frozen_b)
-
-
-def test_identify_dummy_groups():
+def test_generate_dummy_group_assignments():
     r"""
     Test the heuristic for partitioning dummy atoms into dummy groups.
 
@@ -122,34 +53,42 @@ def test_identify_dummy_groups():
       0-------1
 
     """
-    bond_idxs = get_bond_idxs(Chem.MolFromSmiles("FC1CC1(F)N"))
-    core = [1, 2, 3]
-    dg = identify_dummy_groups(bond_idxs, core)
-    assert_set_equality(dg, [{0}, {4, 5}])
 
-    bond_idxs = get_bond_idxs(Chem.MolFromSmiles("FC1CC1(F)NN"))
-    core = [1, 2, 3]
-    dg = identify_dummy_groups(bond_idxs, core)
-    assert_set_equality(dg, [{0}, {4, 5, 6}])
+    def equivalent_assignment(left, right):
+        def to_comparable(dgas):
+            return frozenset(frozenset((k, frozenset(v)) for k, v in dgs.items()) for dgs in dgas)
 
-    bond_idxs = get_bond_idxs(Chem.MolFromSmiles("C1CC11OO1"))
+        return to_comparable(left) == to_comparable(right)
+
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("FC1CC1(F)N")))
+    core = [1, 2, 3]
+    dgas = list(generate_dummy_group_assignments(g, core))
+    assert equivalent_assignment(dgas, [{1: {0}, 3: {4, 5}}])
+
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("FC1CC1(F)NN")))
+    core = [1, 2, 3]
+    dgas = list(generate_dummy_group_assignments(g, core))
+    assert equivalent_assignment(dgas, [{1: {0}, 3: {4, 5, 6}}])
+
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("C1CC11OO1")))
     core = [0, 1, 2]
-    dg = identify_dummy_groups(bond_idxs, core)
-    assert_set_equality(dg, [{3, 4}])
+    dgas = list(generate_dummy_group_assignments(g, core))
+    assert equivalent_assignment(dgas, [{2: {3, 4}}])
 
-    bond_idxs = get_bond_idxs(Chem.MolFromSmiles("C1CC2OOC12"))
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("C1CC2OOC12")))
     core = [0, 1, 2, 5]
-    dg = identify_dummy_groups(bond_idxs, core)
-    assert_set_equality(dg, [{3, 4}])
+    dgas = list(generate_dummy_group_assignments(g, core))
+    assert equivalent_assignment(dgas, [{2: {3, 4}}, {5: {3, 4}}])
 
     # example above, where O's are dummy atoms, and Cs are core
-    bond_idxs = get_bond_idxs(Chem.MolFromSmiles("OC1COO1"))
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("OC1COO1")))
     core = [1, 2]
-    dg = identify_dummy_groups(bond_idxs, core)
-    assert_set_equality(dg, [{0, 3, 4}])
+    dgas = list(generate_dummy_group_assignments(g, core))
+    # one or two groups depending on choice of anchor atom for {3, 4}
+    assert equivalent_assignment(dgas, [{1: {0}, 2: {3, 4}}, {1: {0, 3, 4}}])
 
 
-def assert_anchor_group_equality(a_groups, b_groups):
-    frozen_a = [tuple(a) for a in a_groups]
-    frozen_b = [tuple(b) for b in b_groups]
-    assert frozenset(frozen_a) == frozenset(frozen_b)
+def test_generate_dummy_group_assignments_empty_core():
+    g = convert_bond_list_to_nx(get_romol_bonds(Chem.MolFromSmiles("OC1COO1")))
+    core = []
+    assert list(generate_dummy_group_assignments(g, core)) == []
