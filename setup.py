fc90f4eb53eaa11ad44c3d356f61547bfdd82bb5
Matt Wittmann <mwittmann@relaytx.com>
2023-05-18 12:28:23 -0700

    Add test

--- setup.py
+++ setup.py
@@ -122,6 +122,7 @@ setup(
             "pytest-cov",
             "hilbertcurve==1.0.5",
             "hypothesis[numpy]==6.54.6",
+            "py3Dmol",
         ],
         "viz": ["py3Dmol"],
     },
