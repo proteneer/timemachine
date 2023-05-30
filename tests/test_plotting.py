77d914d7632ca4f3e39119819cd738ada7fac545
Forrest York <forrest.york@gmail.com>
2023-05-30 16:01:23 -0600

    PR Feedback: Make plotting test nogpu

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pytest
 
 from timemachine.fe.plots import plot_forward_and_reverse_ddg
 
 
+@pytest.mark.nogpu
 def test_forward_and_reverse_ddg_plot():
     rng = np.random.default_rng(2023)
     ukln_shape = (47, 2, 2, 2000)
