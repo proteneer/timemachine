62556c47b3523ce503c949184b1e099caa0a7925
Forrest York <forrest.york@gmail.com>
2023-05-31 09:57:24 -0600

    PR Feedback: Separate out plotting
    * Adds a dG plot for legs

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pytest
 
-from timemachine.fe.plots import plot_forward_and_reverse_ddg
+from timemachine.fe.plots import plot_forward_and_reverse_ddg, plot_forward_and_reverse_dg
+
+# All plotting tests are to be tested without gpus
+pytestmark = [pytest.mark.nogpu]
 
 
-@pytest.mark.nogpu
 def test_forward_and_reverse_ddg_plot():
     rng = np.random.default_rng(2023)
     ukln_shape = (47, 2, 2, 2000)
@@ -12,3 +14,33 @@ def test_forward_and_reverse_ddg_plot():
     dummy_complex_ukln = rng.random(size=ukln_shape)
 
     plot_forward_and_reverse_ddg(dummy_solv_ukln, dummy_complex_ukln)
+
+
+def test_forward_and_reverse_ddg_plot_validation():
+    rng = np.random.default_rng(2023)
+    ukln_shape = (47, 2, 2, 2000)
+    dummy_solv_ukln = rng.random(size=ukln_shape)
+    dummy_complex_ukln = rng.random(size=ukln_shape)
+
+    with pytest.raises(AssertionError, match="fewer samples than chunks"):
+        plot_forward_and_reverse_ddg(dummy_solv_ukln, dummy_complex_ukln, chunks=ukln_shape[-1] + 1)
+    # Verify that with different size arrays it fails
+    with pytest.raises(AssertionError):
+        plot_forward_and_reverse_ddg(dummy_solv_ukln, dummy_complex_ukln[0])
+
+
+def test_forward_and_reverse_dg_plot():
+    rng = np.random.default_rng(2023)
+    ukln_shape = (47, 2, 2, 2000)
+    dummy_ukln = rng.random(size=ukln_shape)
+
+    plot_forward_and_reverse_dg(dummy_ukln)
+
+
+def test_forward_and_reverse_dg_plot_validation():
+    rng = np.random.default_rng(2023)
+    ukln_shape = (47, 2, 2, 2000)
+    dummy_ukln = rng.random(size=ukln_shape)
+
+    with pytest.raises(AssertionError, match="fewer samples than chunks"):
+        plot_forward_and_reverse_dg(dummy_ukln, chunks=ukln_shape[-1] + 1)
