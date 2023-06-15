417e541237a285c18b8bbba01b79e89070a5f534
Forrest York <forrest.york@gmail.com>
2023-06-15 10:13:50 -0600

    Adds PotentialExecutor
    * Provide a single entry point for optimizing python execution of
    potentials
    * All accumulation done in Fixed point
    * Support both bound and unbound potentials

---
+++ timemachine/cpp/src/potential_executor.cu
@@ -0,0 +1,105 @@
+#include "gpu_utils.cuh"
+#include "potential_executor.hpp"
+
+const static int D = 3;
+
+namespace timemachine {
+
+PotentialExecutor::PotentialExecutor(int N, bool parallel)
+    : N_(N), parallel_(parallel), d_x_(N * D), d_box_(D * D), d_du_dx_buffer_(N * D), d_u_buffer_(N){};
+
+PotentialExecutor::~PotentialExecutor() {}
+
+void PotentialExecutor::execute_potentials(
+    const std::vector<int> param_sizes,
+    const std::vector<std::shared_ptr<Potential>> &pots,
+    const double *h_x,
+    const double *h_box,
+    const std::vector<double *> &d_params, // Vector of pointers to device arrays
+    unsigned long long *h_du_dx,
+    unsigned long long *h_du_dp,
+    unsigned long long *h_u) {
+    cudaStream_t stream = static_cast<cudaStream_t>(0);
+    d_x_.copy_from(h_x);
+    d_box_.copy_from(h_box);
+
+    if (h_du_dx != nullptr) {
+        gpuErrchk(cudaMemsetAsync(d_du_dx_buffer_.data, 0, d_du_dx_buffer_.size, stream));
+    }
+
+    if (h_u != nullptr) {
+        gpuErrchk(cudaMemsetAsync(d_u_buffer_.data, 0, d_u_buffer_.size, stream));
+    }
+
+    int num_params = 0;
+    for (int size : param_sizes) {
+        num_params += size;
+    }
+
+    std::unique_ptr<DeviceBuffer<unsigned long long>> du_dp_buffer;
+    if (h_du_dp != nullptr) {
+        du_dp_buffer.reset(new DeviceBuffer<unsigned long long>(num_params));
+        gpuErrchk(cudaMemsetAsync(du_dp_buffer->data, 0, du_dp_buffer->size, stream));
+    }
+
+    int offset = 0;
+    if (parallel_) {
+        for (int i = 0; i < pots.size(); i++) {
+            // Always sync the new streams with the incoming stream to ensure that the state
+            // of the incoming buffers are valid
+            manager_.sync_from(i, stream);
+        }
+    }
+    cudaStream_t pot_stream = stream;
+    for (int i = 0; i < pots.size(); i++) {
+        if (parallel_) {
+            pot_stream = manager_.get_stream(i);
+        }
+        int pot_param_count = param_sizes[i];
+        pots[i]->execute_device(
+            N_,
+            pot_param_count,
+            d_x_.data,
+            pot_param_count > 0 ? d_params[i] : nullptr,
+            d_box_.data,
+            h_du_dx == nullptr ? nullptr : d_du_dx_buffer_.data,
+            h_du_dp == nullptr ? nullptr : du_dp_buffer->data + offset,
+            h_u == nullptr ? nullptr : d_u_buffer_.data,
+            pot_stream);
+
+        offset += pot_param_count;
+        if (parallel_) {
+            manager_.sync_to(i, stream);
+        }
+    }
+
+    if (h_du_dx != nullptr) {
+        gpuErrchk(cudaMemcpyAsync(h_du_dx, d_du_dx_buffer_.data, d_du_dx_buffer_.size, cudaMemcpyDeviceToHost, stream));
+    }
+
+    if (h_du_dp != nullptr) {
+        gpuErrchk(cudaMemcpyAsync(h_du_dp, du_dp_buffer->data, du_dp_buffer->size, cudaMemcpyDeviceToHost, stream));
+    }
+
+    if (h_u != nullptr) {
+        gpuErrchk(cudaMemcpyAsync(h_u, d_u_buffer_.data, d_u_buffer_.size, cudaMemcpyDeviceToHost, stream));
+    }
+
+    gpuErrchk(cudaStreamSynchronize(stream));
+};
+
+void PotentialExecutor::du_dp_fixed_to_float(
+    const std::vector<int> param_sizes,
+    const std::vector<std::shared_ptr<Potential>> &pots,
+    const unsigned long long *h_du_dp,
+    double *h_du_dp_float) {
+    int offset = 0;
+
+    for (int i = 0; i < pots.size(); i++) {
+        int bp_size = param_sizes[i];
+        pots[i]->du_dp_fixed_to_float(N_, bp_size, h_du_dp + offset, h_du_dp_float + offset);
+        offset += bp_size;
+    }
+}
+
+} // namespace timemachine
---
+++ timemachine/potentials/executor.py
@@ -0,0 +1,82 @@
+from functools import partial
+from typing import Any, List, Optional, Tuple, cast
+
+import jax
+import jax.numpy as jnp
+from jax.core import Tracer
+from numpy.typing import NDArray
+from timemchine.lib import custom_ops
+
+
+class PotentialExecutor:
+    """Interface to execute a set of potentials efficiently for host side code with the option of running the potentials in parallel"""
+
+    def __init__(self, N: int, parallel: bool = True):
+        self._N = N
+        self._runner = custom_ops.PotentialExecutor(N, parallel)
+
+    def execute_bound(
+        self,
+        bps: List[custom_ops.BoundPotential],
+        coords: NDArray,
+        box: NDArray,
+        compute_du_dx: bool = True,
+        compute_u: bool = True,
+    ) -> Tuple[Optional[NDArray], Optional[float]]:
+        """Execute a list of bound potentials on a set of coords and box"""
+        assert coords.shape[0] == self._N, "Number of coordinates don't match"
+        du_dx, u = self._runner.execute_bound(bps, coords, box, compute_du_dx, compute_u)
+        return du_dx, u
+
+    def execute(self, pots: List[custom_ops.Potential], coords: NDArray, params: List[NDArray], box: NDArray) -> float:
+        """Execute a list of potentials on a set of coords and box.
+
+        Supports jax.grad to compute the du_dx and du_dp components.
+        """
+        assert coords.shape[0] == self._N, "Number of coordinates don't match"
+        if len(pots) != len(params):
+            raise RuntimeError("Number of potentials and params must agree")
+        param_sizes = [len(p) for p in params]
+        flattened_params = jnp.concatenate([p.reshape(-1) for p in params])
+        u = _call_runner_unbound(self._runner, pots, param_sizes, coords, flattened_params, box)
+        return cast(float, u)
+
+
+@partial(jax.custom_jvp, nondiff_argnums=(0, 1, 2))
+def _call_runner_unbound(
+    runner: custom_ops.PotentialExecutor,
+    pots: List[custom_ops.Potential],
+    param_sizes: List[int],
+    coords: NDArray,
+    params: NDArray,
+    box: NDArray,
+) -> float:
+    _, _, u = runner.execute_unbound(pots, param_sizes, coords, params, box, False, False, True)
+    return u
+
+
+# Add JAX custom derivative rules
+# https://jax.readthedocs.io/en/latest/notebooks/Custom_derivative_rules_for_Python_code.html#custom-jvps-with-jax-custom-jvp
+@_call_runner_unbound.defjvp
+def _(
+    runner: custom_ops.PotentialExecutor, pots: List[custom_ops.Potential], param_sizes: List[int], primals, tangents
+) -> Tuple[Any, Any]:
+    x, params, box = primals
+    dx, dp, dbox = tangents
+
+    if isinstance(dbox, Tracer):
+        raise RuntimeError("box derivatives not supported")
+
+    compute_du_dx = isinstance(dx, Tracer)
+    compute_du_dp = isinstance(dp, Tracer)
+
+    du_dx, du_dp, u = runner.execute_unbound(pots, param_sizes, x, params, box, compute_du_dx, compute_du_dp, True)
+
+    tangent_out = jnp.zeros_like(u)
+
+    if compute_du_dx:
+        tangent_out += jnp.sum(du_dx * dx)
+    if compute_du_dp:
+        tangent_out += jnp.sum(du_dp * dp)
+
+    return u, tangent_out
