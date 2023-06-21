62acf0d777cc353339d96f07cc28066f92c7f531
Forrest York <badisa@users.noreply.github.com>
2023-06-21 13:08:44 -0600

    Remove ixn count sync (#1053)
    * Fixes minor typos
    
    * Removes interaction count sync from nonbonded potentials
    
    * Adds custom threads per block for nonbonded kernel
    
    * Faster with 256 threads and more blocks
    
    * Adds check in context of the box volumes
    
    * Use shared memory directly to remove 6 registers
    
    * Provides a performance speed up
    
    * Fixes typo in multiple_steps_U
    
    * Neighborlist wasn't importing the cuda runtime and would result in the inability to find the cudaStream_t type if not entered implicitly by nvcc

--- timemachine/cpp/src/local_md_utils.hpp
+++ timemachine/cpp/src/local_md_utils.hpp
@@ -7,8 +7,6 @@
 
 namespace timemachine {
 
-bool is_nonbonded_all_pairs_potential(std::shared_ptr<Potential> pot);
-
 void set_nonbonded_potential_idxs(
     std::shared_ptr<Potential> pot, const int num_idxs, const unsigned int *d_idxs, const cudaStream_t stream);
 
@@ -25,10 +23,6 @@ void set_nonbonded_ixn_potential_idxs(
 
 std::shared_ptr<BoundPotential>
 construct_ixn_group_potential(const int N, std::shared_ptr<Potential> pot, const int P, const double *d_params);
-
-// Recursively populate nb_pots potentials with the NonbondedAllPairs
-void get_nonbonded_all_pair_potentials(
-    std::vector<std::shared_ptr<BoundPotential>> input, std::vector<std::shared_ptr<BoundPotential>> &flattened);
 
 void verify_local_md_parameters(double radius, double k);
 
