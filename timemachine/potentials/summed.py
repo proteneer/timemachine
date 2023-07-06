deaaf885c1fa6c0ed2240995c35b693ac7327a9a
Matt Wittmann <mwittmann@relaytx.com>
2023-07-06 11:59:20 -0700

    Handle inhomogenous parameter dimensionality

--- timemachine/potentials/summed.py
+++ timemachine/potentials/summed.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from typing import Optional, Sequence, Tuple
 
 import jax.numpy as jnp
@@ -33,7 +34,7 @@ def summed_potential(
         shapes of the parameter array input for each potential term (must be same length as U_fns)
     """
     assert len(U_fns) == len(shapes)
-    sizes = np.prod(shapes, axis=1)
+    sizes = [reduce(lambda x, y: x * y, shape) for shape in shapes]
     # np.split expects indices, must increment sizes to be indices
     split_indices = np.cumsum(sizes)
     paramss = [ps.reshape(shape) for ps, shape in zip(np.split(params, split_indices[:-1]), shapes)]
