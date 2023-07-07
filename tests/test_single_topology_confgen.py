669f1a37e56a0e5da26d7306d582601f37eca014
Matt Wittmann <mwittmann@relaytx.com>
2023-07-07 14:27:05 -0700

    Update default atom mapping constraints for RBFE (#1068)

--- tests/test_single_topology_confgen.py
+++ tests/test_single_topology_confgen.py
@@ -41,7 +41,7 @@ def run_edge(mol_a, mol_b, protein_path, n_windows):
         max_cores=1e6,
         enforce_core_core=True,
         ring_matches_ring_only=True,
-        complete_rings=True,
+        complete_rings=False,
         enforce_chiral=True,
         min_threshold=0,
     )
