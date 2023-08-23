b8aebff39f3b1f814c373f79ee598e46fab96082
Matt Wittmann <mwittmann@relaytx.com>
2023-08-23 14:39:11 -0700

    Add RWMH test

---
+++ tests/test_md_moves.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+import numpy as np
+from scipy.stats import ks_2samp
+
+from timemachine.md.moves import MetropolisHastingsMove
+
+
+def test_rwmh():
+    """Test sampling from a 1-d normal distribution using Random Walk Metropolis-Hastings"""
+
+    np.random.seed(2023)
+    n_samples = 100_000
+    dx = 0.1
+
+    class RWMH1D(MetropolisHastingsMove[float]):
+        def propose_with_dlogq(self, x: float) -> Tuple[float, float]:
+            x_prop = x + np.random.uniform(-dx, dx)
+            log_q = -(x_prop ** 2) / 2
+            return x_prop, log_q
+
+    rwmh = RWMH1D()
+    x_0 = np.random.uniform(-0.3, 0.3)
+    rw_samples = rwmh.sample_chain(x_0, n_samples)
+
+    tau = round(2 / dx ** 2)
+    decorrelated_rw_samples = rw_samples[tau::tau]
+
+    target_samples = np.random.normal(0, 1, size=(len(decorrelated_rw_samples),))
+
+    _, pvalue = ks_2samp(decorrelated_rw_samples, target_samples)
+
+    assert pvalue >= 0.05
