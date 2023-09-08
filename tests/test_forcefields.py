f1fcea9452cc049f28010cac05979cecb517998b
jkausrelay <101571135+jkausrelay@users.noreply.github.com>
2023-09-08 17:11:49 -0400

    Fix split ixn (#1138)
    * Fix bug where order of split ff handles was wrong
    
    * Add test for combine_params

--- tests/test_forcefields.py
+++ tests/test_forcefields.py
@@ -5,10 +5,10 @@ from warnings import catch_warnings
 
 import numpy as np
 import pytest
-from common import temporary_working_dir
+from common import load_split_forcefields, temporary_working_dir
 
 from timemachine import constants
-from timemachine.ff import Forcefield
+from timemachine.ff import Forcefield, combine_params
 from timemachine.ff.handlers.deserialize import deserialize_handlers
 
 pytestmark = [pytest.mark.nocuda]
@@ -84,3 +84,51 @@ def test_load_default():
     for (ref_handle, test_handle) in zip(ref.get_ordered_handles(), test.get_ordered_handles()):
         assert ref_handle.smirks == test_handle.smirks
         np.testing.assert_array_equal(ref_handle.params, test_handle.params)
+
+
+def test_split():
+    ffs = load_split_forcefields()
+
+    def check(ff):
+        params = ff.get_params()
+        np.testing.assert_array_equal(ff.hb_handle.params, params.hb_params)
+        np.testing.assert_array_equal(ff.ha_handle.params, params.ha_params)
+        np.testing.assert_array_equal(ff.pt_handle.params, params.pt_params)
+        np.testing.assert_array_equal(ff.it_handle.params, params.it_params)
+        np.testing.assert_array_equal(ff.q_handle.params, params.q_params)
+        np.testing.assert_array_equal(ff.q_handle_intra.params, params.q_params_intra)
+        np.testing.assert_array_equal(ff.q_handle_solv.params, params.q_params_solv)
+        np.testing.assert_array_equal(ff.lj_handle.params, params.lj_params)
+        np.testing.assert_array_equal(ff.lj_handle_intra.params, params.lj_params_intra)
+        np.testing.assert_array_equal(ff.lj_handle_solv.params, params.lj_params_solv)
+
+        assert ff.get_ordered_handles() == [
+            ff.hb_handle,
+            ff.ha_handle,
+            ff.pt_handle,
+            ff.it_handle,
+            ff.q_handle,
+            ff.q_handle_intra,
+            ff.q_handle_solv,
+            ff.lj_handle,
+            ff.lj_handle_intra,
+            ff.lj_handle_solv,
+        ]
+
+        combined = combine_params(ff.get_params(), ff.get_params())
+        np.testing.assert_array_equal((ff.hb_handle.params, ff.hb_handle.params), combined.hb_params)
+        np.testing.assert_array_equal((ff.ha_handle.params, ff.ha_handle.params), combined.ha_params)
+        np.testing.assert_array_equal((ff.pt_handle.params, ff.pt_handle.params), combined.pt_params)
+        np.testing.assert_array_equal((ff.it_handle.params, ff.it_handle.params), combined.it_params)
+        np.testing.assert_array_equal((ff.q_handle.params, ff.q_handle.params), combined.q_params)
+        np.testing.assert_array_equal((ff.q_handle_intra.params, ff.q_handle_intra.params), combined.q_params_intra)
+        np.testing.assert_array_equal((ff.q_handle_solv.params, ff.q_handle_solv.params), combined.q_params_solv)
+        np.testing.assert_array_equal((ff.lj_handle.params, ff.lj_handle.params), combined.lj_params)
+        np.testing.assert_array_equal((ff.lj_handle_intra.params, ff.lj_handle_intra.params), combined.lj_params_intra)
+        np.testing.assert_array_equal((ff.lj_handle_solv.params, ff.lj_handle_solv.params), combined.lj_params_solv)
+
+    check(ffs.ref)
+    check(ffs.intra)
+    check(ffs.solv)
+    check(ffs.prot)
+    check(ffs.prot)
