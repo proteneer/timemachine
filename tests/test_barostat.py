33a4f68a838941d63e1640e940706dfe4f31692e
Forrest York <badisa@users.noreply.github.com>
2023-09-26 12:59:36 -0600

    Make tests more robust to noise (#1159)
    * Makes barostat more robust to noise
    
    * The default value of 1% of volume is reasonable and thus may not adapt
    
    * Fixes incorrect behavior in hilbert sort test
    
    * Was the max displacement rather than the max distance
    * Noticed by @maxentile 
    
    * Increases threshold of local MD test to be more robust to randomness
    
    * Running with different seeds can trigger failures.

--- tests/test_barostat.py
+++ tests/test_barostat.py
@@ -606,9 +606,13 @@ def test_barostat_scaling_behavior():
     scaling = baro.get_volume_scale_factor()
     assert scaling > 0
 
+    # Set to an intentionally bad factor to ensure it adapts
+    bad_scaling_factor = 0.5 * compute_box_volume(host_box)
+    baro.set_volume_scale_factor(bad_scaling_factor)
+    assert baro.get_volume_scale_factor() == bad_scaling_factor
     ctxt.multiple_steps(100)
     # The scaling should adapt between moves
-    assert scaling != baro.get_volume_scale_factor()
+    assert bad_scaling_factor > baro.get_volume_scale_factor()
 
     # Reset the scaling to the previous value
     baro.set_volume_scale_factor(scaling)
