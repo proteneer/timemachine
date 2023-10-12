ff88f8762f5e809c6df5e09b36a5ac85b268ad04
Forrest York <forrest.york@gmail.com>
2023-10-12 07:57:27 -0600

    PR Feedback: Simplify the reference code

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -14,8 +14,8 @@ def hamiliton_product(q1: NDArray, q2: NDArray) -> NDArray:
     ----------
     https://en.wikipedia.org/wiki/Quaternion#Hamilton_product
     """
-    w1, x1, y1, z1 = q1[0], q1[1], q1[2], q1[3]
-    w2, x2, y2, z2 = q2[0], q2[1], q2[2], q2[3]
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
     output = np.empty(4)
     output[0] = (w1 * w2) - (x1 * x2) - (y1 * y2) - (z1 * z2)
     output[1] = (w1 * x2) + (x1 * w2) + (y1 * z2) - (z1 * y2)
