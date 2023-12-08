9b5d1d07ca8fe7869dc343e8880b20356075e5c9
Matt Wittmann <mwittmann@relaytx.com>
2023-12-08 12:35:20 -0800

    Promote to property test

--- tests/hrex/test_hrex.py
+++ tests/hrex/test_hrex.py
@@ -1,26 +1,29 @@
+import hypothesis.strategies as st
 import numpy as np
+from hypothesis import given, seed
 
 from timemachine.md.hrex import get_samples_by_iter_by_replica
 
 
-def test_get_samples_by_iter_by_replica():
-    rng = np.random.default_rng()
-    n_states = 5
-    n_iters = 10
-    samples_per_iter = 2
+@given(
+    st.integers(1, 10)
+    .flatmap(lambda n_states: st.lists(st.permutations(range(n_states)), min_size=1, max_size=10))
+    .map(np.array)
+)
+@seed(2023)
+def test_get_samples_by_iter_by_replica(perms):
+    n_iters, n_states = perms.shape
+    replica_idx_by_state_by_iter = perms
 
+    # NOTE: the implementation is agnostic to the instantiation of Samples; here we test with Samples = int
     # (replica, iter) -> samples
-    samples_by_iter_by_replica_ref = rng.uniform(0, 1, size=(n_states, n_iters, samples_per_iter))
+    samples_by_iter_by_replica_ref = np.arange(n_states * n_iters).reshape((n_states, n_iters))
 
-    # (iter, state) -> replica
-    replica_idx_by_state_by_iter = rng.permuted(np.repeat(np.arange(n_states)[None, :], n_iters, axis=0), axis=1)
     # (iter, replica) -> samples
     samples_by_replica_by_iter = samples_by_iter_by_replica_ref.swapaxes(0, 1)
 
     # (iter, state) -> samples
-    samples_by_state_by_iter = np.take_along_axis(
-        samples_by_replica_by_iter, replica_idx_by_state_by_iter[:, :, None], 1
-    )
+    samples_by_state_by_iter = np.take_along_axis(samples_by_replica_by_iter, replica_idx_by_state_by_iter, 1)
 
     samples_by_iter_by_replica = get_samples_by_iter_by_replica(
         samples_by_state_by_iter.tolist(), replica_idx_by_state_by_iter.tolist()
