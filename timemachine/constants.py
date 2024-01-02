8d53e33c27e06d79b33d4f73c5e9a899d61e1430
Matt Wittmann <mwittmann@relaytx.com>
2024-01-02 09:22:22 -0800

    Enable disallow_torsion_flips by default

--- timemachine/constants.py
+++ timemachine/constants.py
@@ -35,6 +35,6 @@ DEFAULT_ATOM_MAPPING_KWARGS = {
     "ring_matches_ring_only": True,
     "complete_rings": False,
     "enforce_chiral": True,
-    "disallow_torsion_flips": False,
+    "disallow_torsion_flips": True,
     "min_threshold": 0,
 }
