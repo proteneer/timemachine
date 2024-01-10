ca4e5fbe726e030f13bb036912c1b16bfbd12f32
Matt Wittmann <mwittmann@relaytx.com>
2024-01-09 19:30:34 -0800

    Add atom mapping filter disallowing flipped planar torsions in core (#1229)
    * Add check for flipped planar torsions
    
    * Add atom mapping flag to disable planar torsion flips
    
    * Change atom mapping defaults to disable planar torsion flips

--- tests/test_chiral_utils.py
+++ tests/test_chiral_utils.py
@@ -4,7 +4,14 @@ from rdkit import Chem
 from rdkit.Chem import AllChem
 
 from timemachine.fe import chiral_utils, utils
-from timemachine.fe.chiral_utils import ChiralCheckMode, ChiralRestrIdxSet, find_atom_map_chiral_conflicts
+from timemachine.fe.chiral_utils import (
+    ChiralCheckMode,
+    ChiralRestrIdxSet,
+    find_atom_map_chiral_conflicts,
+    setup_find_flipped_planar_torsions,
+)
+from timemachine.ff import Forcefield
+from timemachine.md.minimizer import replace_conformer_with_minimized
 from timemachine.potentials.chiral_restraints import U_chiral_atom_batch, U_chiral_bond_batch
 
 pytestmark = [pytest.mark.nocuda]
@@ -308,3 +315,39 @@ def test_chiral_conflict_mixed():
 
     assert len(mixed_map_flips) == 8
     assert len(mixed_map_undefineds) == 1
+
+
+def test_find_flipped_planar_torsions():
+    # double bond
+    mol = Chem.MolFromSmiles(r"Cl\C(F)=N/F")
+    AllChem.EmbedMolecule(mol, randomSeed=0)
+
+    core_ok = [0, 1, 2, 3, 4]
+    core_bad = [2, 1, 0, 3, 4]
+
+    find_flipped_planar_torsions = setup_find_flipped_planar_torsions(mol, mol)
+
+    assert find_flipped_planar_torsions(core_ok) == []
+    assert set(find_flipped_planar_torsions(core_bad)) == {
+        ((0, 1, 3, 4), (2, 1, 3, 4)),
+        ((2, 1, 3, 4), (0, 1, 3, 4)),
+    }
+
+    # amide
+    mol = Chem.AddHs(Chem.MolFromSmiles("O=C(N)C"))
+    AllChem.EmbedMolecule(mol, randomSeed=0)
+
+    # rdkit embedding has non-planar torsions spanning the amide bond
+    replace_conformer_with_minimized(mol, Forcefield.load_default())
+
+    core_ok = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    core_bad = [0, 1, 2, 3, 5, 4, 6, 7, 8]
+
+    find_flipped_planar_torsions = setup_find_flipped_planar_torsions(mol, mol)
+    assert find_flipped_planar_torsions(core_ok) == []
+    assert set(find_flipped_planar_torsions(core_bad)) == {
+        ((0, 1, 2, 4), (0, 1, 2, 5)),
+        ((0, 1, 2, 5), (0, 1, 2, 4)),
+        ((3, 1, 2, 4), (3, 1, 2, 5)),
+        ((3, 1, 2, 5), (3, 1, 2, 4)),
+    }
