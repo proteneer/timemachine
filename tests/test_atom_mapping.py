1ab494c6d3f40c0932c9ff3802a6aec31bbf5abe
Matt Wittmann <mwittmann@relaytx.com>
2024-01-11 13:59:38 -0800

    Add clarifying comment

--- tests/test_atom_mapping.py
+++ tests/test_atom_mapping.py
@@ -1005,4 +1005,4 @@ def test_get_cores_and_diagnostics():
 
         assert diagnostics.core_size >= len(all_cores[0])
         assert diagnostics.num_cores >= len(all_cores)
-        assert diagnostics.total_nodes_visited >= diagnostics.core_size
+        assert diagnostics.total_nodes_visited >= diagnostics.core_size  # must visit at least one node per edge in core
