e874f2da4a07e59562ad78d352b4b02fb6307240
Josh Fass <jfass@relaytx.com>
2024-01-11 06:49:19 -0800

    Test has_chiral_atom_flips (#1230)
    Assert has_chiral_atom_flips is symmetric on random atom mappings of random pairs of FreeSolv molecules
    
    ---------
    
    Co-authored-by: Matt Wittmann <mwittmann@relaytx.com>

--- tests/test_chiral_utils.py
+++ tests/test_chiral_utils.py
@@ -1,15 +1,20 @@
+from collections import Counter
+
 import numpy as np
 import pytest
 from rdkit import Chem
 from rdkit.Chem import AllChem
 
+from timemachine.datasets import fetch_freesolv
 from timemachine.fe import chiral_utils, utils
 from timemachine.fe.chiral_utils import (
     ChiralCheckMode,
     ChiralRestrIdxSet,
     find_atom_map_chiral_conflicts,
+    has_chiral_atom_flips,
     setup_find_flipped_planar_torsions,
 )
+from timemachine.fe.utils import get_romol_conf
 from timemachine.ff import Forcefield
 from timemachine.md.minimizer import replace_conformer_with_minimized
 from timemachine.potentials.chiral_restraints import U_chiral_atom_batch, U_chiral_bond_batch
@@ -253,12 +258,16 @@ def test_chiral_conflict_flip():
     assert len(identity_flips) == 0
     assert len(identity_undefineds) == 0
 
+    assert not has_chiral_atom_flips(identity_map, chiral_set_a, chiral_set_b)
+
     swap_map_flips = find_atom_map_chiral_conflicts(swap_map, chiral_set_a, chiral_set_b, mode=ChiralCheckMode.FLIP)
     swap_map_undefineds = find_atom_map_chiral_conflicts(
         swap_map, chiral_set_a, chiral_set_b, mode=ChiralCheckMode.UNDEFINED
     )
     assert len(swap_map_flips) == 8  # TODO: deduplicate idxs?
     assert len(swap_map_undefineds) == 0
+
+    assert has_chiral_atom_flips(swap_map, chiral_set_a, chiral_set_b)
 
 
 def test_chiral_conflict_undefined():
@@ -315,6 +324,61 @@ def test_chiral_conflict_mixed():
 
     assert len(mixed_map_flips) == 8
     assert len(mixed_map_undefineds) == 1
+
+    assert has_chiral_atom_flips(mixed_map, chiral_set_a, chiral_set_b)
+
+
+def test_has_chiral_atom_flips_symmetric(n_trials=100):
+    """On random atom mappings of random pairs of freesolv molecules,
+    assert has_chiral_atom_flips(core, a, b) == has_chiral_atom_flips(core[:,::-1], b, a)
+    """
+    rng = np.random.default_rng(2024)
+
+    mols = fetch_freesolv()
+
+    answers = []  # keep track of fraction of time has_chiral_atom_flips(core, a, b) is True or False
+
+    for _ in range(n_trials):
+        mol_a = mols[rng.integers(0, len(mols))]
+        mol_b = mols[rng.integers(0, len(mols))]
+
+        chiral_set_a = ChiralRestrIdxSet.from_mol(mol_a, get_romol_conf(mol_a))
+        chiral_set_b = ChiralRestrIdxSet.from_mol(mol_b, get_romol_conf(mol_b))
+
+        N_a, N_b = mol_a.GetNumAtoms(), mol_b.GetNumAtoms()
+        core_size = rng.integers(1, min(N_a, N_b))
+
+        # on a large fraction of instances, include a random conflict
+        if rng.uniform() < 0.75 and len(chiral_set_a.disallowed_set) > 0 and len(chiral_set_b.disallowed_set) > 0:
+            # on half of these, introduce the conflict in one direction vs. another
+            if rng.uniform() < 0.5:
+                _core_a = rng.choice(list(chiral_set_a.allowed_set))
+                _core_b = rng.choice(list(chiral_set_b.disallowed_set))
+            else:
+                _core_a = rng.choice(list(chiral_set_a.disallowed_set))
+                _core_b = rng.choice(list(chiral_set_b.allowed_set))
+        else:
+            _core_a = np.arange(N_a)
+            rng.shuffle(_core_a)
+            _core_a = _core_a[:4]
+
+            _core_b = np.arange(N_b)
+            rng.shuffle(_core_b)
+            _core_b = _core_b[:4]
+
+        core_size = min(len(_core_a), len(_core_b))
+        core = np.array([_core_a[:core_size], _core_b[:core_size]]).T
+
+        ans_fwd = has_chiral_atom_flips(core, chiral_set_a, chiral_set_b)
+        ans_rev = has_chiral_atom_flips(core[:, ::-1], chiral_set_b, chiral_set_a)
+
+        answers.append(ans_fwd)
+
+        assert ans_fwd == ans_rev
+
+    num_instances_true_v_false = Counter(answers)
+    print("test_has_chiral_atom_conflicts_symmetric::num_instances_true_v_false", Counter(answers))
+    assert set(num_instances_true_v_false) == {True, False}, "random test likely uninformative"
 
 
 def test_find_flipped_planar_torsions():
