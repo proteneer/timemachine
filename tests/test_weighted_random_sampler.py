a4ce7f96fb0dee896a9963e9ef1d2731b5758b5d
Forrest York <forrest.york@gmail.com>
2024-01-22 13:02:42 -0700

    PR Feedback: Verify that running sampler repeatedly passes

--- tests/test_weighted_random_sampler.py
+++ tests/test_weighted_random_sampler.py
@@ -37,9 +37,10 @@ def test_segmented_random_sampler(seed, size, num_samples, precision):
 
 
 @pytest.mark.parametrize("seed", [2024])
+@pytest.mark.parametrize("repeats", [10])
 @pytest.mark.parametrize("num_samples", [8000])
 @pytest.mark.parametrize("precision", [np.float32, np.float64])
-def test_segmented_random_sampler_jagged_batches_simple_distributions(seed, num_samples, precision):
+def test_segmented_random_sampler_jagged_batches_simple_distributions(seed, repeats, num_samples, precision):
     """Make sure that jagged arrays are handled_correctly"""
     klass = custom_ops.SegmentedWeightedRandomSampler_f32
     if precision == np.float64:
@@ -49,7 +50,7 @@ def test_segmented_random_sampler_jagged_batches_simple_distributions(seed, num_
 
     weights = []
     for _ in range(num_samples // 2):
-        #  Scale each weight by a uniform value
+        # Scale each weight by a uniform value
         weights.append(np.array([7.5, 2.5]) * rng.uniform() * 100.0)
         weights.append(np.array([3.3, 3.4, 3.3]) * rng.uniform() * 100.0)
     expected_percentages = [normalize_probabilities(weights[0]), normalize_probabilities(weights[1])]
@@ -59,21 +60,23 @@ def test_segmented_random_sampler_jagged_batches_simple_distributions(seed, num_
     sampler = klass(largest_segment, len(weights), seed)
     sampler_b = klass(largest_segment, num_samples, seed)
 
-    test_selection = sampler.sample(weights)
-    assert len(test_selection) == len(weights)
+    # Verify that calling the same sampler multiple times produces passes the test
+    for _ in range(repeats):
+        test_selection = sampler.sample(weights)
+        assert len(test_selection) == len(weights)
 
-    # Should produce an identical result
-    np.testing.assert_array_equal(sampler_b.sample(weights), test_selection)
+        # Should produce an identical result
+        np.testing.assert_array_equal(sampler_b.sample(weights), test_selection)
 
-    # Check the pairs of two by grabbing the even indices
-    _, counts = np.unique(np.array(test_selection)[::2], return_counts=True)
-    percentages = counts / (len(weights) // 2)
-    np.testing.assert_allclose(expected_percentages[0], percentages, atol=0.05)
+        # Check the pairs of two by grabbing the even indices
+        _, counts = np.unique(np.array(test_selection)[::2], return_counts=True)
+        percentages = counts / (len(weights) // 2)
+        np.testing.assert_allclose(expected_percentages[0], percentages, atol=0.05)
 
-    # Check the set of three values by grabbing the odd indices
-    _, counts = np.unique(np.array(test_selection)[1:][::2], return_counts=True)
-    percentages = counts / (len(weights) // 2)
-    np.testing.assert_allclose(expected_percentages[1], percentages, atol=0.05)
+        # Check the set of three values by grabbing the odd indices
+        _, counts = np.unique(np.array(test_selection)[1:][::2], return_counts=True)
+        percentages = counts / (len(weights) // 2)
+        np.testing.assert_allclose(expected_percentages[1], percentages, atol=0.05)
 
 
 @pytest.mark.parametrize("seed", [2024])
