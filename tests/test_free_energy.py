cd1ef2094cea66b81b73028ceb163ad4ee453edd
Josh Fass <jfass@relaytx.com>
2024-02-01 10:53:27 -0500

    Update set of expected potentials in plot test

--- tests/test_free_energy.py
+++ tests/test_free_energy.py
@@ -286,9 +286,14 @@ def test_plot_pair_bar_plots(mock_fig, hif2a_ligand_pair_single_topology_lam0_st
     )
     make_pair_bar_plots(pair_result, DEFAULT_TEMP, "")
     assert mock_fig.call_args is not None
-    assert set(mock_fig.call_args.args[0]) == set(
-        ["HarmonicBond", "HarmonicAngleStable", "PeriodicTorsion", "NonbondedPairListPrecomputed"]
-    )
+    expcted_potentials = {
+        "HarmonicBond",
+        "HarmonicAngleStable",
+        "PeriodicTorsion",
+        "NonbondedPairListPrecomputed",
+        "ChiralAtomRestraint",
+    }
+    assert set(mock_fig.call_args.args[0]) == expcted_potentials
 
 
 def test_run_sims_bisection_early_stopping(hif2a_ligand_pair_single_topology_lam0_state):
