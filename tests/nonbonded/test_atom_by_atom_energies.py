f268de695a17a8c90905237cd1ba009677972a45
Forrest York <forrest.york@gmail.com>
2024-02-16 18:40:04 -0700

    Remove prints

--- tests/nonbonded/test_atom_by_atom_energies.py
+++ tests/nonbonded/test_atom_by_atom_energies.py
@@ -49,10 +49,8 @@ def test_nonbonded_atom_by_atom_energies_match(num_mols, adjustments, precision,
     for _ in range(adjustments):
         adjusted_params = params.copy()
         adjusted_params[:, 3] = rng.uniform(0, 1, size=adjusted_params[:, 3].shape)
-        print(adjusted_params[:10])
         mover = BDExchangeMove(beta, cutoff, adjusted_params, all_group_idxs, DEFAULT_TEMP)
         ref_energies = mover.U_fn_unsummed(conf, box, target_atoms, all_atoms)
         comp_energies = func(target_atoms, conf, adjusted_params, box, mover.nb_beta, mover.nb_cutoff)
-        print(ref_energies.shape)
         assert ref_energies.shape == comp_energies.shape
         assert_energy_arrays_match(ref_energies, comp_energies, atol=atol, rtol=rtol, threshold=threshold)
