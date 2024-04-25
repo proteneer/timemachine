b0cfef05b748dd6bc103f9970c42b4633b633133
Josh Fass <jfass@relaytx.com>
2024-04-25 15:10:52 -0400

    Tighten rtol in test of nonbonded_mol_energy

--- tests/nonbonded/test_nonbonded_mol_energy.py
+++ tests/nonbonded/test_nonbonded_mol_energy.py
@@ -63,7 +63,7 @@ def test_nonbonded_mol_energy_potential_validation(precision):
 
 @pytest.mark.memcheck
 @pytest.mark.parametrize("num_mols", [1, 2, 15, 100, 4085])
-@pytest.mark.parametrize("precision,atol,rtol", [(np.float64, 1e-8, 1e-8), (np.float32, 1e-4, 2e-3)])
+@pytest.mark.parametrize("precision,atol,rtol", [(np.float64, 1e-8, 1e-8), (np.float32, 1e-4, 1e-3)])
 def test_nonbonded_mol_energy_matches_exchange_mover_batch_U(num_mols, precision, atol, rtol):
     """Assert that NonbondedMolEnergyPotential Cuda implementation produces the same
     energies as the reference jax version in the BDExchangeMover"""
