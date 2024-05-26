e1f918e1f692e9ecb4f66c8b6f718cd796b7669a
Forrest York <forrest.york@gmail.com>
2024-05-25 20:39:57 -0600

    Reapply "Avoid reading in an invalid sample, triggering an invalid memory access"
    This reverts commit 75f11ff9f301be082cc985d4c4155191820112d1.

--- timemachine/cpp/src/kernels/k_exchange.cu
+++ timemachine/cpp/src/kernels/k_exchange.cu
@@ -747,20 +747,31 @@ template void __global__ k_setup_destination_weights_for_targeted<double>(
 );
 
 void __global__ k_adjust_sample_idxs(
+    const int total_proposals,
     const int batch_size,
+    const int *__restrict__ noise_offset,           // [1]
     const int *__restrict__ targeting_inner_volume, // [batch_size]
     const int *__restrict__ inner_count,            // [1]
-    const int *__restrict__ partitioned_indices,    // [inner_count]
+    const int *__restrict__ partitioned_indices,    // [K]
     int *__restrict__ sample_idxs                   // [batch_size]
 ) {
-    const int local_inner_count = inner_count[0];
+    const int current_offset = *noise_offset;
+    const int local_inner_count = *inner_count;
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
 
     while (idx < batch_size) {
-        const int target_inner = targeting_inner_volume[idx];
-        const int offset = target_inner == 1 ? local_inner_count : 0;
-        const int before = sample_idxs[idx];
-        sample_idxs[idx] = partitioned_indices[before + offset];
+        if (idx + current_offset >= total_proposals) {
+            // The value here is not important, as the result will be ignored in subsuequent kernels
+            // This is done to avoid the fact that the original sampled indices may contain invalid state
+            // thanks to the way the batching performed.
+            sample_idxs[idx] = 0;
+        } else {
+            const int target_inner = targeting_inner_volume[idx];
+            const int offset = target_inner == 1 ? local_inner_count : 0;
+            const int before = sample_idxs[idx];
+            sample_idxs[idx] = partitioned_indices[before + offset];
+            idx += gridDim.x * blockDim.x;
+        }
         idx += gridDim.x * blockDim.x;
     }
 }
