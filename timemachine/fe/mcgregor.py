38b29a4a8ee3011ee160c991e235a292445980ba
Matt Wittmann <mwittmann@relaytx.com>
2024-07-24 11:14:17 -0700

    Explicitly handle max_connected_components=None

--- timemachine/fe/mcgregor.py
+++ timemachine/fe/mcgregor.py
@@ -191,7 +191,7 @@ class Graph:
                 n_ccs += 1
                 if cc_size < min_connected_component_size:
                     return True
-                if n_ccs == max_connected_components:
+                if max_connected_components is not None and n_ccs == max_connected_components:
                     # if we've seen the maximum number of connected components, we should have seen all of the mapped nodes
                     return not mapped_nodes.issubset(seen)
 
