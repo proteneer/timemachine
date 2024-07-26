85bbc9022b2e0dbe551ba24dba2c8aa13d63fe59
Forrest York <badisa@users.noreply.github.com>
2024-07-25 19:22:26 -0600

    Add failing test case of minimization (#1351)
    * Default xfail to strict
    
    * Adds toy system demonstrating minimization failure
    
    * Make sure it only fails with a minimization error
    
    ---------
    
    Co-authored-by: Josh Fass <jfass@relaytx.com>

--- tests/test_minimizer.py
+++ tests/test_minimizer.py
@@ -13,6 +13,8 @@ from timemachine.ff.handlers import openmm_deserializer
 from timemachine.md import builders, minimizer
 from timemachine.md.barostat.utils import compute_box_volume
 from timemachine.md.minimizer import equilibrate_host_barker, make_host_du_dx_fxn
+from timemachine.potentials import NonbondedPairList
+from timemachine.potentials.jax_utils import distance_on_pairs
 
 
 @pytest.mark.parametrize(
@@ -197,3 +199,46 @@ def test_local_minimize_water_box():
     u_init_test, g_init_test = val_and_grad_fn(x0)
     assert u_init == u_init_test
     np.testing.assert_array_equal(g_init, g_init_test)
+
+
+@pytest.mark.xfail(raises=minimizer.MinimizationError, reason="Doesn't work currently, but should eventually")
+def test_minimizer_failure_toy_system():
+    """https://github.com/proteneer/timemachine/pull/1346 introduced a minimization scheme that minimizes
+    at lambda = 0.1 and at 0.0. The need for lambda = 0.1 was due to certain ligands being very close to
+    protein residues, this test replicates the failure case with a small toy system to allow investigation in the future
+    """
+
+    # Atoms 2933 and 38815 from PFKFB3 with ligands 20, 43
+    nb_params = np.array(
+        [
+            [0.9323587, 0.13247664, 0.25629826, 0.0],
+            [0.52629348, 0.12916129, 0.26202344, 0.0],
+        ]
+    )
+    beta = 2.0
+    cutoff = 1.2
+    pairlist = NonbondedPairList([0, 1], [1.0, 1.0], beta, cutoff)
+
+    # Small delta in coordinate space that results in failure
+    dx = 0.04
+    coords = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [dx, dx, dx],
+        ]
+    )
+    box = np.eye(3) * 100.0
+
+    initial_distance = distance_on_pairs(coords[None, 0], coords[None, 1], box)
+
+    bound_impl = pairlist.bind(nb_params).to_gpu(np.float32).bound_impl
+
+    du_dx = lambda x: bound_impl.execute(x, box, compute_u=False)[0]
+    initial_force_norms = np.linalg.norm(du_dx(coords))
+
+    minimized_coords = minimizer.fire_minimize(coords, du_dx, 100)
+
+    final_distance = distance_on_pairs(minimized_coords[None, 0], minimized_coords[None, 1], box)
+    assert not np.isclose(initial_distance, final_distance, atol=2e-4)
+    assert initial_force_norms > np.linalg.norm(du_dx(minimized_coords))
+    minimizer.check_force_norm(minimized_coords)
