99d6557e6a7f40b49a7c56fa622e0be3b43f31c2
Forrest York <forrest.york@gmail.com>
2024-07-31 14:12:23 -0600

    PR Feedback: Add note about requirement that max_visits must be less than or equal to max_node_visits

--- timemachine/fe/atom_mapping.py
+++ timemachine/fe/atom_mapping.py
@@ -137,7 +137,7 @@ def get_cores(
         The distance cutoff that non-ring atoms must satisfy.
 
     max_visits: int
-        Maximum number of nodes we can visit for a given core size.
+        Maximum number of nodes we can visit for a given core size. Must be less than or equal to max_node_visits.
 
     max_node_visits: int
         Maximum number of nodes that can be visited overall to find a core.
