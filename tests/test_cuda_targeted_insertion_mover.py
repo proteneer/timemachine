d06564efd3dab8ba85e11089e7cf743224104bf8
Forrest York <forrest.york@gmail.com>
2024-09-10 16:45:34 -0600

    PR Feedback: Fix failing tests

--- tests/test_cuda_targeted_insertion_mover.py
+++ tests/test_cuda_targeted_insertion_mover.py
@@ -993,7 +993,7 @@ def test_targeted_moves_with_complex_and_ligand_in_brd4(
 
     bps = initial_state.potentials
 
-    ligand_env_pot = get_bound_potential_by_type(bps, SummedPotential)
+    ligand_env_pot = get_bound_potential_by_type(bps, SummedPotential).potential
     nb = get_bound_potential_by_type(bps, Nonbonded)
     bond_pot = get_bound_potential_by_type(bps, HarmonicBond).potential
 
@@ -1001,7 +1001,7 @@ def test_targeted_moves_with_complex_and_ligand_in_brd4(
         i for i, pot in enumerate(ligand_env_pot.potentials) if isinstance(pot, NonbondedInteractionGroup)
     )
 
-    water_params = ligand_env_pot.potential.params_init[ixn_group_idx]
+    water_params = ligand_env_pot.params_init[ixn_group_idx]
 
     bond_list = get_bond_list(bond_pot)
     all_group_idxs = get_group_indices(bond_list, conf.shape[0])
