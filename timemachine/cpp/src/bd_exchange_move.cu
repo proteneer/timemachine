proteneer/timemachine

commit 2abb1c01902348a61cde701e58474876c1fae9e6

Add method to get samples from context without storing to disk (#1334)
* Adds sample_with_context_iter to get frames in memory

* Allows more fine grained control of when data is in memory or on disk

* Avoid reading chunks into memory for storage

---------

Co-authored-by: Matt Wittmann <mwittmann@relaytx.com>

timemachine/fe/free_energy.py
@@ -1,7 +1,7 @@
 import time
 from dataclasses import dataclass, replace
 from functools import cache
-from typing import Callable, List, Optional, Sequence, Tuple, Union
+from typing import Callable, Iterator, List, Optional, Sequence, Tuple
 from warnings import warn
 
 import jax
@@ -543,9 +543,42 @@ def get_context(initial_state: InitialState, md_params: Optional[MDParams] = Non
     return Context(initial_state.x0, initial_state.v0, initial_state.box0, intg_impl, bound_impls, movers=movers)
 
 
-def sample_with_context(
-    ctxt: Context, md_params: MDParams, temperature: float, ligand_idxs: NDArray, max_buffer_frames: int
-) -> Trajectory:
+def sample_with_context_iter(
+    ctxt: Context, md_params: MDParams, temperature: float, ligand_idxs: NDArray, batch_size: int
+) -> Iterator[Tuple[NDArray, NDArray, NDArray]]:
+    """Sample a context using MDParams returning batches of frames up to `batch_size`. All results are returned
+    as numpy arrays that are in memory, and it is left to the user to act accordingly.
+
+    For getting a Trajectory object that stores the frames to disk, refer to `sample_with_context`.
+
+    Parameters
+    ----------
+    ctxt: Context
+        The context to use to generate samples
+
+    md_params: MDParams
+        The parameters that define the sampling of frames from the context
+
+    temperature: float
+        The temperature, in kelvin, used when running Local MD moves
+
+    ligand_idxs: np.ndarray
+        Array representing the indices of atoms that make up the ligand, determines the atoms considered as the center
+        of local MD.
+
+    batch_size: int
+        The most number of frames (coords and boxes) that will be kept in memory at one time.
+
+    Returns
+    -------
+    Iterator of 3-tuples
+        coords, boxes, final_velocities
+
+    Notes
+    -----
+    * If md_params.n_eq_steps is greater than 0, the barostat will be set to run every 15 steps regardless of what
+      the context defined. Will be reset to the original interval for production steps.
+    """
     # burn-in
     if md_params.n_eq_steps:
         # Set barostat interval to 15 for equilibration, then back to the original interval for production
@@ -573,7 +606,6 @@ def sample_with_context(
             n_steps=n_steps,
             store_x_interval=md_params.steps_per_frame,
         )
-        assert np.all(coords[-1] == ctxt.get_x_t())
         final_velocities = ctxt.get_v_t()
 
         return coords, boxes, final_velocities
@@ -601,22 +633,32 @@ def sample_with_context(
             coords.append(x_t)
             boxes.append(box_t)
 
-        assert np.all(coords[-1][-1] == ctxt.get_x_t())
         final_velocities = ctxt.get_v_t()
 
         return np.concatenate(coords), np.concatenate(boxes), final_velocities
-
-    all_coords: Union[NDArray, StoredArrays]
 
     steps_func = run_production_steps
     if md_params.local_steps > 0:
         steps_func = run_production_local_steps
 
+    for n_frames in batches(md_params.n_frames, batch_size):
+        yield steps_func(n_frames * md_params.steps_per_frame)
+
+
+def sample_with_context(
+    ctxt: Context, md_params: MDParams, temperature: float, ligand_idxs: NDArray, max_buffer_frames: int
+) -> Trajectory:
+    """Wrapper for `sample_with_context_iter` that stores the frames to disk and returns a Trajectory result.
+    Stores up to `max_buffer_frames` frames in memory before writing to disk.
+
+    Refer to `sample_with_context_iter` for parameter documentation
+    """
     all_coords = StoredArrays()
     all_boxes: List[NDArray] = []
     final_velocities: NDArray = None  # type: ignore # work around "possibly unbound" error
-    for n_frames in batches(md_params.n_frames, max_buffer_frames):
-        batch_coords, batch_boxes, final_velocities = steps_func(n_frames * md_params.steps_per_frame)
+    for batch_coords, batch_boxes, final_velocities in sample_with_context_iter(
+        ctxt, md_params, temperature, ligand_idxs, max_buffer_frames
+    ):
         all_coords.extend(batch_coords)
         all_boxes.extend(batch_boxes)
 
@@ -1146,7 +1188,6 @@ def run_sims_hrex(
     fraction_accepted_by_pair_by_iter: List[List[Tuple[int, int]]] = []
 
     # Reset the barostat from the equilibration interval to the production interval
-    barostat = context.get_barostat()
     state = initial_states[0]
     if barostat is not None and state.barostat is not None:
         barostat.set_interval(state.barostat.interval)
@@ -1162,7 +1203,7 @@ def run_sims_hrex(
 
     for current_frame in range(md_params.n_frames):
 
-        def sample_replica(xvb: CoordsVelBox, state_idx: StateIdx) -> Trajectory:
+        def sample_replica(xvb: CoordsVelBox, state_idx: StateIdx) -> Tuple[NDArray, NDArray, NDArray, Optional[float]]:
             context.set_x_t(xvb.coords)
             context.set_v_t(xvb.velocities)
             context.set_box(xvb.box)
@@ -1181,10 +1222,20 @@ def run_sims_hrex(
 
             md_params_replica = replace(md_params, n_frames=1, n_eq_steps=0, seed=state_idx + current_frame)
 
-            return sample_with_context(context, md_params_replica, temperature, ligand_idxs, max_buffer_frames=100)
+            assert md_params_replica.n_frames == 1
+            # Get the next set of frames from the iterator, which will be the only value returned
+            frame, box, final_velos = next(
+                sample_with_context_iter(context, md_params_replica, temperature, ligand_idxs, batch_size=1)
+            )
+            assert frame.shape[0] == 1
 
-        def replica_from_samples(traj: Trajectory) -> CoordsVelBox:
-            return CoordsVelBox(traj.frames[-1], traj.final_velocities, traj.boxes[-1])
+            final_barostat_volume_scale_factor = barostat.get_volume_scale_factor() if barostat is not None else None
+
+            return frame[-1], box[-1], final_velos, final_barostat_volume_scale_factor
+
+        def replica_from_samples(last_sample: Tuple[NDArray, NDArray, NDArray, Optional[float]]) -> CoordsVelBox:
+            frame, box, velos, _ = last_sample
+            return CoordsVelBox(frame, velos, box)
 
         hrex, samples_by_state_iter = hrex.sample_replicas(sample_replica, replica_from_samples)
         U_kl = compute_potential_matrix(potential, hrex, params_by_state, md_params.hrex_params.max_delta_states)
@@ -1202,8 +1253,13 @@ def run_sims_hrex(
         if len(initial_states) == 2:
             fraction_accepted_by_pair = fraction_accepted_by_pair[1:]  # remove stats for identity move
 
-        for samples, samples_iter in zip(samples_by_state, samples_by_state_iter):
-            samples.extend(samples_iter)
+        for samples, (xs, boxes, velos, final_barostat_volume_scale_factor) in zip(
+            samples_by_state, samples_by_state_iter
+        ):
+            samples.frames.extend([xs])
+            samples.boxes.extend([boxes])
+            samples.final_velocities = velos
+            samples.final_barostat_volume_scale_factor = final_barostat_volume_scale_factor
 
         fraction_accepted_by_pair_by_iter.append(fraction_accepted_by_pair)
 

timemachine/fe/stored_arrays.py
@@ -90,7 +90,7 @@ class StoredArrays(Sequence[NDArray]):
         return Path(self._dir.name)
 
     def extend(self, xs: Collection[ArrayLike]):
-        np.save(self._get_chunk_path(len(self._chunk_sizes)), np.array(xs))
+        np.save(self._get_chunk_path(len(self._chunk_sizes)), np.asarray(xs))
         self._chunk_sizes.append(len(xs))
 
     @staticmethod
@@ -115,12 +115,13 @@ class StoredArrays(Sequence[NDArray]):
         >>> StoredArrays.load(fc) == sa
         True
         """
-        for idx, chunk in enumerate(self._chunks()):
-            serialized_array = serialize_array(np.array(chunk))
-            path = self.get_chunk_path(prefix, idx)
-            if client.exists(str(path)):
-                raise FileExistsError(f"file already exists: {path}")
-            client.store(str(path), serialized_array)
+        for idx, _ in enumerate(self._chunk_sizes):
+            dest_path = self.get_chunk_path(prefix, idx)
+            if client.exists(str(dest_path)):
+                raise FileExistsError(f"file already exists: {dest_path}")
+            src_path = self._get_chunk_path(idx)
+            with open(src_path, "rb") as ifs:
+                client.store_stream(str(dest_path), ifs)
 
     @classmethod
     def load(cls: Type[_T], client: AbstractFileClient, prefix: Path = Path(".")) -> _T:

commit 4624f0354bcd4a1cc68a1adc5555745225419468

Clean up HREX, BDExchangeMover and TIBDExchangeMover (#1391)
* Add stream to kernel to ensure correct ordering

* Not used in practice and after a stream sync so it works, but janky

* Correctly patch the sampling function

* Broken in https://github.com/proteneer/timemachine/pull/1334

* Remove custom equilibration code

* Rely on the equilbration in sample_with_context_iter on the first frame
to avoid duplicated code. Does make timing less reliable if calling
`run_sims_hrex` but seem preferable to duplicated code

* Avoid setting the same seed for water sampler

* Using the MDParams seed means all of bisection schedules have the same
water sampling seed, which is undesirable.

timemachine/cpp/src/bd_exchange_move.cu
@@ -200,7 +200,7 @@ void BDExchangeMove<RealType>::move(
             // Run only after the first pass, to maintain meaningful `log_probability_host` values
             // Run a separate kernel to replace the before logsumexp values with the after if accepted a move
             // Could also recompute the logsumexp each round, but more expensive than probably necessary.
-            k_store_accepted_log_probability<RealType><<<1, 1, 0>>>(
+            k_store_accepted_log_probability<RealType><<<1, 1, 0, stream>>>(
                 num_target_mols_,
                 batch_size_,
                 d_selected_sample_.data,

timemachine/fe/free_energy.py
@@ -538,8 +538,8 @@ def get_context(initial_state: InitialState, md_params: Optional[MDParams] = Non
 
         water_params = get_water_sampler_params(initial_state)
 
-        # Generate a new random seed based on the md params seed
-        rng = np.random.default_rng(md_params.seed)
+        # Generate a new random seed based on the integrator seed, MDParams seed is constant across states
+        rng = np.random.default_rng(initial_state.integrator.seed)
         water_sampler_seed = rng.integers(np.iinfo(np.int32).max)
 
         water_sampler = custom_ops.TIBDExchangeMove_f32(
@@ -1221,54 +1221,13 @@ def run_sims_hrex(
         # Add an identity move to the mixture to ensure aperiodicity
         neighbor_pairs = [(StateIdx(0), StateIdx(0))] + neighbor_pairs
 
-    # Setup the barostat to run more often for equilibration
-    equil_barostat_interval = 15
     barostat = context.get_barostat()
-    if barostat is not None:
-        barostat.set_interval(equil_barostat_interval)
 
-    def get_equilibrated_xvb(xvb: CoordsVelBox, state_idx: StateIdx) -> CoordsVelBox:
-        # Set up parameters of the water sampler movers
-        if md_params.water_sampling_params is not None:
-            for mover in context.get_movers():
-                if isinstance(mover, WATER_SAMPLER_MOVERS):
-                    assert water_params_by_state is not None
-                    mover.set_params(water_params_by_state[state_idx])
-        if md_params.n_eq_steps > 0:
-            # Set the movers to 0 to ensure they all equilibrate the same way
-            # Ensure initial mover state is consistent across replicas
-            for mover in context.get_movers():
-                mover.set_step(0)
-
-            params = params_by_state[state_idx]
-            bound_potentials[0].set_params(params)
-
-            context.set_x_t(xvb.coords)
-            context.set_v_t(xvb.velocities)
-            context.set_box(xvb.box)
-
-            xs, boxes = context.multiple_steps(md_params.n_eq_steps, store_x_interval=0)
-            assert len(xs) == 1
-            x0 = xs[0]
-            v0 = context.get_v_t()
-            box0 = boxes[0]
-            xvb = CoordsVelBox(x0, v0, box0)
-        return xvb
-
-    initial_replicas = [
-        get_equilibrated_xvb(CoordsVelBox(s.x0, s.v0, s.box0), StateIdx(i)) for i, s in enumerate(initial_states)
-    ]
-
-    hrex = HREX.from_replicas(initial_replicas)
+    hrex = HREX.from_replicas([CoordsVelBox(s.x0, s.v0, s.box0) for s in initial_states])
 
     samples_by_state: List[Trajectory] = [Trajectory.empty() for _ in initial_states]
     replica_idx_by_state_by_iter: List[List[ReplicaIdx]] = []
     fraction_accepted_by_pair_by_iter: List[List[Tuple[int, int]]] = []
-
-    # Reset the barostat from the equilibration interval to the production interval
-    state = initial_states[0]
-    if barostat is not None and state.barostat is not None:
-        barostat.set_interval(state.barostat.interval)
 
     if (
         md_params.water_sampling_params is not None
@@ -1298,7 +1257,13 @@ def run_sims_hrex(
                 # Set the step so that all windows have the movers be called the same number of times.
                 mover.set_step(current_step)
 
-            md_params_replica = replace(md_params, n_frames=1, n_eq_steps=0, seed=state_idx + current_frame)
+            md_params_replica = replace(
+                md_params,
+                n_frames=1,
+                # Run equilibration as part of the first frame
+                n_eq_steps=md_params.n_eq_steps if current_frame == 0 else 0,
+                seed=state_idx + current_frame,
+            )
 
             assert md_params_replica.n_frames == 1
             # Get the next set of frames from the iterator, which will be the only value returned

tests/test_hrex_rbfe.py
@@ -18,7 +18,7 @@ from timemachine.fe.free_energy import (
     HREXSimulationResult,
     MDParams,
     WaterSamplingParams,
-    sample_with_context,
+    sample_with_context_iter,
 )
 from timemachine.fe.plots import (
     plot_hrex_replica_state_distribution_heatmap,
@@ -117,11 +117,11 @@ def test_hrex_rbfe_hif2a(hif2a_single_topology_leg, seed):
     rss_traj = []
 
     def sample_and_record_rss(*args, **kwargs):
-        traj = sample_with_context(*args, **kwargs)
+        result = sample_with_context_iter(*args, **kwargs)
         rss_traj.append(Process().memory_info().rss)
-        return traj
+        return result
 
-    with patch("timemachine.fe.free_energy.sample_with_context", sample_and_record_rss):
+    with patch("timemachine.fe.free_energy.sample_with_context_iter", sample_and_record_rss):
         result = estimate_relative_free_energy_bisection_hrex(
             mol_a,
             mol_b,
@@ -134,8 +134,10 @@ def test_hrex_rbfe_hif2a(hif2a_single_topology_leg, seed):
             min_cutoff=0.7 if host_name == "complex" else None,
         )
 
+    assert len(rss_traj) > n_windows * md_params.n_frames
     # Check that memory usage is not increasing
     rss_traj = rss_traj[10:]  # discard initial transients
+    assert len(rss_traj)
     rss_diff_count = np.sum(np.diff(rss_traj) != 0)
     rss_increase_count = np.sum(np.diff(rss_traj) > 0)
     assert stats.binom.pmf(rss_increase_count, n=rss_diff_count, p=0.5) >= 0.001