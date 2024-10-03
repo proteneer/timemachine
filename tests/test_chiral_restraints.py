Fix 1-4 scaling factors for both ligand and protein. (#1399)
* Fix 1-4 scaling factors.

* Update README

* Update comments

* Update PR per comments.

* Update tests

* Update assert_ff_optimizable tests

* Increase strength of force constants.

* Update example npzs

tests/test_chiral_restraints.py
@@ -160,7 +160,7 @@ class BaseTopologyRescaledCharges(topology.BaseTopology):
 @pytest.mark.nocuda
 def test_chiral_restraints_torsion():
     """For a charge-scaled version of hydrogen peroxide, assert that:
-    * without chiral bond restraints, cis/trans states are sampled ~ 25%/75%
+    * without chiral bond restraints, cis/trans states are sampled ~ 17%/83%
     * with chiral bond restraints, ~ only specified state is sampled"""
     mol = Chem.MolFromMolBlock(
         """
@@ -198,15 +198,16 @@ $$$$""",
         vols_orig.append(torsion_volume(*f[torsion_idxs]))
     vols_orig = np.array(vols_orig)
 
-    # the 25/75 ratio is dependent on the scale defined above, which affects the repulsive
+    # the 17/83 ratio is dependent on the scale defined above, which affects the repulsive
     # strength of the hydrogens.)
-    assert np.abs(np.mean(vols_orig > 0) - 0.25) < 0.05
+
+    assert np.abs(np.mean(vols_orig > 0) - 0.168) < 0.05
 
     all_signs = [1, -1]  # [trans, cis]
     for sign in all_signs:
 
         def U_total(x):
-            return U_fn(x) + U_chiral_bond(x, torsion_idxs, 1000.0, sign)
+            return U_fn(x) + U_chiral_bond(x, torsion_idxs, 2000.0, sign)
 
         vols_chiral = []
         frames = simulate_system(U_total, x0)

timemachine/ff/handlers/openmm_deserializer.py
@@ -60,36 +60,47 @@ def deserialize_nonbonded_force(force, N):
     # validate exclusions/exceptions to make sure they make sense
     for a_idx in range(force.getNumExceptions()):
         # tbd charge scale factors
-        src, dst, new_cp, new_sig, new_eps = force.getExceptionParameters(a_idx)
-        new_sig = value(new_sig)
-        new_eps = value(new_eps)
+        src, dst, new_q, new_sig, new_eps = force.getExceptionParameters(a_idx)
+        desired_q = value(new_q) * constants.ONE_4PI_EPS0
+        desired_sig = value(new_sig)
+        desired_eps = value(new_eps)
 
         src_sig = all_sig[src]
         dst_sig = all_sig[dst]
 
         src_eps = all_eps[src]
         dst_eps = all_eps[dst]
-        expected_sig = (src_sig + dst_sig) / 2
-        expected_eps = np.sqrt(src_eps * dst_eps)
+        initial_sig = (src_sig + dst_sig) / 2
+        initial_eps = np.sqrt(src_eps * dst_eps)
+
+        src_q = charge_params[src]
+        dst_q = charge_params[dst]
+
+        initial_q = src_q * dst_q
 
         exclusion_idxs_.append([src, dst])
-
-        # sanity check this (expected_eps can be zero), redo this thing
-
         # the lj_scale factor measures how much we *remove*
-        if expected_eps == 0:
-            if new_eps == 0:
+        if initial_eps == 0:
+            if desired_eps == 0:
                 lj_scale_factor = 1
             else:
-                raise RuntimeError("Divide by zero in epsilon calculation")
+                raise RuntimeError("No LJ scaling factor possible to arrive at desired_eps")
         else:
-            lj_scale_factor = 1 - new_eps / expected_eps
+            lj_scale_factor = 1 - desired_eps / initial_eps
 
-        scale_factors_.append(lj_scale_factor)
+        if initial_q == 0:
+            if desired_q == 0:
+                q_scale_factor = 1
+            else:
+                raise RuntimeError("No ES scaling factor possible to arrive at desired_q")
+        else:
+            q_scale_factor = 1 - desired_q / initial_q
 
-        # tbd fix charge_scale_factors using new_cp
-        if new_eps != 0:
-            np.testing.assert_almost_equal(expected_sig, new_sig)
+        scale_factors_.append((q_scale_factor, lj_scale_factor))
+
+        # check combining rules for sigmas are consistent
+        if desired_eps != 0:
+            np.testing.assert_almost_equal(initial_sig, desired_sig)
 
     exclusion_idxs = np.array(exclusion_idxs_, dtype=np.int32)
 
@@ -111,7 +122,7 @@ def deserialize_nonbonded_force(force, N):
     beta = 2.0  # erfc correction
 
     # use the same scale factors for electrostatics and lj
-    scale_factors = np.stack([scale_factors_, scale_factors_], axis=1)
+    scale_factors = np.array(scale_factors_)
 
     return nb_params, exclusion_idxs, beta, scale_factors
 