779ccbe8acb656384468da1c8a9798450a87f88c
Matt Wittmann <mwittmann@relaytx.com>
2024-11-13 19:38:54 -0800

    Fix from_idxs, remove unused code

--- timemachine/fe/rest/bond.py
+++ timemachine/fe/rest/bond.py
@@ -2,9 +2,7 @@ from collections.abc import Mapping, Sequence
 from dataclasses import dataclass, field
 
 import numpy as np
-from numpy.typing import ArrayLike, NDArray
-
-from timemachine.fe.single_topology import canonicalize_bonds
+from numpy.typing import NDArray
 
 
 @dataclass(frozen=True)
@@ -23,7 +21,7 @@ class CanonicalBond:
     def from_idxs(cls, i: int, j: int):
         return cls(i, j, _unsafe=True) if i < j else cls(j, i, _unsafe=True)
 
-    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]):
+    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]) -> "CanonicalBond":
         return CanonicalBond.from_idxs(a_to_b[self.i], a_to_b[self.j])
 
 
@@ -44,7 +42,7 @@ class CanonicalAngle:
     def from_idxs(cls, i: int, j: int, k: int):
         return cls(i, j, k, _unsafe=True) if i < k else cls(k, j, i, _unsafe=True)
 
-    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]):
+    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]) -> "CanonicalAngle":
         return CanonicalAngle.from_idxs(a_to_b[self.i], a_to_b[self.j], a_to_b[self.k])
 
 
@@ -64,33 +62,10 @@ class CanonicalTorsion:
 
     @classmethod
     def from_idxs(cls, i: int, j: int, k: int, l: int):
-        return cls(i, j, k, l, _unsafe=True)
+        return cls(i, j, k, l, _unsafe=True) if i < l else cls(l, k, j, i, _unsafe=True)
 
-    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]):
+    def translate(self, a_to_b: Mapping[int, int] | Sequence[int] | NDArray[np.int_]) -> "CanonicalTorsion":
         return CanonicalTorsion.from_idxs(a_to_b[self.i], a_to_b[self.j], a_to_b[self.k], a_to_b[self.l])
-
-
-@dataclass(frozen=True)
-class CanonicalIxns:
-    idxs: NDArray[np.int32]
-
-    _unsafe: bool = field(default=False, init=True)
-
-    def __post_init__(self):
-        if np.any(self.idxs[:, 0] >= self.idxs[:, -1]):
-            raise ValueError("bonds are not canonical")
-
-    @classmethod
-    def from_idxs(cls, bonds: ArrayLike, dim: int):
-        bonds = np.asarray(bonds)
-        if len(bonds) == 0:
-            bonds = bonds.reshape(0, dim)
-        else:
-            assert bonds.shape[1] == dim
-        return cls(canonicalize_bonds(bonds), _unsafe=True)
-
-    def translate(self, a_to_b: NDArray[np.int_]):
-        return CanonicalIxns.from_idxs(a_to_b[self.idxs], self.idxs.shape[1])
 
 
 def mkbond(i: int, j: int) -> CanonicalBond:
@@ -103,15 +78,3 @@ def mkangle(i: int, j: int, k: int) -> CanonicalAngle:
 
 def mktorsion(i: int, j: int, k: int, l: int) -> CanonicalTorsion:
     return CanonicalTorsion.from_idxs(i, j, k, l)
-
-
-def mkbonds(idxs: ArrayLike) -> CanonicalIxns:
-    return CanonicalIxns.from_idxs(idxs, 2)
-
-
-def mkangles(idxs: ArrayLike) -> CanonicalIxns:
-    return CanonicalIxns.from_idxs(idxs, 3)
-
-
-def mktorsions(idxs: ArrayLike) -> CanonicalIxns:
-    return CanonicalIxns.from_idxs(idxs, 4)
