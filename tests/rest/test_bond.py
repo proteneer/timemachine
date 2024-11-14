de24fcc146a91c58238fe9ce07fb3543571553eb
Matt Wittmann <mwittmann@relaytx.com>
2024-11-14 10:28:02 -0800

    Add test cases

--- tests/rest/test_bond.py
+++ tests/rest/test_bond.py
@@ -14,3 +14,14 @@ def test_rejects_non_canonical():
 
 def test_eq():
     assert CanonicalBond(1, 2) == CanonicalBond.from_idxs(1, 2)
+    assert CanonicalBond(1, 2) == CanonicalBond.from_idxs(2, 1)
+
+    assert CanonicalAngle(1, 2, 3) == CanonicalAngle.from_idxs(1, 2, 3)
+    assert CanonicalAngle(1, 2, 3) == CanonicalAngle.from_idxs(3, 2, 1)
+    assert CanonicalAngle(1, 2, 3) != CanonicalAngle.from_idxs(2, 3, 1)
+    assert CanonicalAngle(1, 2, 3) != CanonicalAngle.from_idxs(2, 1, 3)
+
+    assert CanonicalTorsion(1, 2, 3, 4) == CanonicalTorsion.from_idxs(1, 2, 3, 4)
+    assert CanonicalTorsion(1, 2, 3, 4) == CanonicalTorsion.from_idxs(4, 3, 2, 1)
+    assert CanonicalTorsion(1, 2, 3, 4) != CanonicalTorsion.from_idxs(2, 3, 4, 1)
+    assert CanonicalTorsion(1, 2, 3, 4) != CanonicalTorsion.from_idxs(4, 2, 3, 1)
