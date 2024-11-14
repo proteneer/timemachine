ff03ba351b35d651283bebaadc3ed9baa74442fe
Matt Wittmann <mwittmann@relaytx.com>
2024-11-14 10:09:03 -0800

    Duplicate get_rotatable_bonds to resolve circular import
    Cleanup

---
+++ timemachine/fe/rest/queries.py
@@ -0,0 +1,29 @@
+from rdkit import Chem
+from rdkit.Chem import rdMolDescriptors
+
+from .bond import CanonicalBond, mkbond
+
+
+def get_rotatable_bonds(mol: Chem.rdchem.Mol) -> set[CanonicalBond]:
+    """
+    Identify rotatable bonds in a molecule.
+
+    NOTE: This is an extremely crude and inaccurate method. This misses simple cases like benzoic acids, amides, etc.
+
+    Parameters
+    ----------
+    mol: ROMol
+        Input molecule
+
+    Returns
+    -------
+    set of CanonicalBond
+        Set of bonds identified as rotatable
+    """
+    pattern = Chem.MolFromSmarts("[!$(*#*)&!D1]-&!@[!$(*#*)&!D1]")
+    matches = mol.GetSubstructMatches(pattern, uniquify=1)
+
+    # sanity check
+    assert len(matches) >= rdMolDescriptors.CalcNumRotatableBonds(mol)
+
+    return {mkbond(i, j) for i, j in matches}
