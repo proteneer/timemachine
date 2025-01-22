323b37d577b703442b14ddfb9431d602e84e9abc
Forrest York <forrest.york@gmail.com>
2025-01-22 12:47:01 -0700

    Make black/flake8 match pre-commit

--- setup.py
+++ setup.py
@@ -110,8 +110,8 @@ setup(
     ],
     extras_require={
         "dev": [
-            "black==23.9.1",
-            "flake8==6.1.0",
+            "black==24.10.0",
+            "flake8==7.1.0",
             "isort==5.12.0",
             "mypy==1.5.1",
             "pre-commit==3.4.0",
