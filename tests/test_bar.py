20e0a7bd709798dcfc0eddf86cc7d883b847a405
Joseph Kaus <jkaus@relaytx.com>
2025-02-12 09:17:19 -0500

    Make sure overlap can't be negative

--- tests/test_bar.py
+++ tests/test_bar.py
@@ -195,7 +195,9 @@ def test_pair_overlap_from_ukln():
 
     # non-overlapping
     u_kln, _ = make_gaussian_ukln_example((0, 0.01), (1, 0.01))
+    assert pair_overlap_from_ukln(u_kln) >= 0.0
     assert pair_overlap_from_ukln(u_kln) < 1e-10
+    print(pair_overlap_from_ukln(u_kln))
 
     # overlapping
     u_kln, _ = make_gaussian_ukln_example((0, 0.1), (0.5, 0.2))
