520b609e56bfdd56cf690c8a3c6b4a658c51cba1
Forrest York <forrest.york@gmail.com>
2025-02-12 17:28:15 -0700

    Fix tests

--- timemachine/utils.py
+++ timemachine/utils.py
@@ -1,7 +1,6 @@
 from collections.abc import Iterator, Sequence
-from contextlib import AbstractContextManager
+from contextlib import contextmanager
 from importlib import resources
-from pathlib import Path
 
 
 def batches(n: int, batch_size: int) -> Iterator[int]:
@@ -18,6 +17,7 @@ def not_ragged(xss: Sequence[Sequence]) -> bool:
     return all(len(xs) == len(xss[0]) for xs in xss)
 
 
-def path_to_internal_file(module: str, file_name: str) -> AbstractContextManager[Path]:
+@contextmanager
+def path_to_internal_file(module: str, file_name: str):
     with resources.as_file(resources.files(module).joinpath(file_name)) as path:
         yield path
